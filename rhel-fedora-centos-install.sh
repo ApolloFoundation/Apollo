f02774c857f291fd28489d4fb00e95e1185eea72
Rostyslav Golda <tx_hv@ukr.net>
2018-04-11 14:00:35 +0300

    Some minor fixes...

--- rhel-fedora-centos-install.sh
+++ rhel-fedora-centos-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-java --version
+java -version
 
-if [$? -eq 127]
+if [ $? -eq 127 ]
 then
     wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.rpm"
     rpm -ivh jdk-8u162-linux-x64.rpm
