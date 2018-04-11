67576c7a7e9b4a08b7d54728917efda2161071e2
Rostyslav Golda <tx_hv@ukr.net>
2018-04-11 14:04:02 +0300

    Added java package removal

--- rhel-fedora-centos-install.sh
+++ rhel-fedora-centos-install.sh
@@ -6,6 +6,7 @@ if [ $? -eq 127 ]
 then
     wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.rpm"
     rpm -ivh jdk-8u162-linux-x64.rpm
+    rm jdk-8u162-linux-x64.rpm
 fi
 
 echo "Enter address for this node:"
