4621c80ced5f9f12ab1bc40c976c4beb4e3f5f96
Rostyslav Golda <tx_hv@ukr.net>
2018-04-27 13:52:07 +0300

    Another typo in installer fix

--- rhel-fedora-centos-install.sh
+++ rhel-fedora-centos-install.sh
@@ -5,8 +5,8 @@ java -version
 if [ $? -eq 127 ]
 then
     wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jdk-8u172-linux-x64.rpm"
-    rpm -ivh jdk-8u162-linux-x64.rpm
-    rm jdk-8u162-linux-x64.rpm
+    rpm -ivh jdk-8u172-linux-x64.rpm
+    rm jdk-8u172-linux-x64.rpm
 fi
 
 echo "Enter address for this node:"
