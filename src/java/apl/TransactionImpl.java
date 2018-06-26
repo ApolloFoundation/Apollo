58a5a6fbdf764008e430dfb3a5b44b09b318cc1c
Rostyslav Golda <tx_hv@ukr.net>
2018-06-26 11:26:24 +0300

    Added ATM/NQT backwards compatibility for transactions

--- src/java/apl/TransactionImpl.java
+++ src/java/apl/TransactionImpl.java
@@ -808,8 +808,8 @@ final class TransactionImpl implements Transaction {
             int timestamp = ((Long) transactionData.get("timestamp")).intValue();
             short deadline = ((Long) transactionData.get("deadline")).shortValue();
             byte[] senderPublicKey = Convert.parseHexString((String) transactionData.get("senderPublicKey"));
-            long amountATM = Convert.parseLong(transactionData.get("amountATM"));
-            long feeATM = Convert.parseLong(transactionData.get("feeATM"));
+            long amountATM = transactionData.containsKey("amountATM") ? Convert.parseLong(transactionData.get("amountATM")) : Convert.parseLong(transactionData.get("amountNQT"));
+            long feeATM = transactionData.containsKey("feeATM") ? Convert.parseLong(transactionData.get("feeATM")) : Convert.parseLong(transactionData.get("feeNQT"));
             String referencedTransactionFullHash = (String) transactionData.get("referencedTransactionFullHash");
             byte[] signature = Convert.parseHexString((String) transactionData.get("signature"));
             Long versionValue = (Long) transactionData.get("version");
