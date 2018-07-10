ec03d2ad24267cd72299c0256ac56493da2176fc
Andrew Boyarsky <andrew.boyarski@gmail.com>
2018-07-10 14:11:31 +0300

    Migrated to maven project

---
+++ src/test/java/apl/TestUtil.java
@@ -0,0 +1,87 @@
+package apl;/*
+ * Copyright © 2017-2018 Apollo Foundation
+ *
+ * See the LICENSE.txt file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with Apollo Foundation,
+ * no part of the Apl software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE.txt file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+
+public class TestUtil {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final Random RANDOM = new Random();
+    static {
+        MAPPER.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    private TestUtil() {} //never
+
+    public static URI createURI(String url) {
+        try {
+            return new URI(url);
+        }
+        catch (URISyntaxException e) {
+            throw new RuntimeException(e); //re-throw unchecked
+        }
+    }
+
+    public static Long atm(long amount) {
+        return 100_000_000L * amount;
+    }
+
+    public static Double fromATM(long amount) {
+        return amount / (double) 100_000_000L;
+    }
+
+    public static ObjectMapper getMAPPER() {
+        return MAPPER;
+    }
+
+    public static Map<String, String> loadKeys(String fileName) {
+        Map<String, String> accounts = new HashMap<>();
+        Properties keys = new Properties();
+        try {
+            keys.load(new BufferedReader(new FileReader(new File(fileName))));
+            keys.forEach((rs, pk) -> accounts.put((String) rs, (String) pk));
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return accounts;
+    }
+
+    public static String randomUrl() {
+        return TestData.URLS.get(RANDOM.nextInt(TestData.URLS.size()));
+    }
+
+    public static String getRandomRS(Map<String,String> accounts) {
+        return new ArrayList<>(accounts.keySet()).get(RANDOM.nextInt(accounts.size()));
+    }
+
+    public static String getRandomSecretPhrase(Map<String, String> accounts) {
+        return accounts.get(getRandomRS(accounts));
+    }
+
+    public static String getRandomRecipientRS(Map<String,String> accounts, String senderRS) {
+        return new ArrayList<>(accounts.keySet()).stream().filter(rs -> !senderRS.equalsIgnoreCase(rs)).collect(Collectors.toList()).get(RANDOM.nextInt(accounts.size() - 1));
+    }
+}
---
+++ src/test/java/dto/Block.java
@@ -0,0 +1,240 @@
+package dto;
+/*
+ * Copyright © 2017-2018 Apollo Foundation
+ *
+ * See the LICENSE.txt file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with Apollo Foundation,
+ * no part of the Apl software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE.txt file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+import java.util.List;
+import java.util.Objects;
+
+public class Block {
+    private String previousBlockHash;
+    private Long payloadLength;
+    private Long totalAmountATM;
+    private String generationSignature;
+    private String generator;
+    private String generatorPublicKey;
+    private Long baseTarget;
+    private String payloadHash;
+    private String generatorRS;
+    private Long numberOfTransactions;
+    private String blockSignature;
+    private List<Transaction> transactions;
+    private Long version;
+    private Long totalFeeATM;
+    private String previousBlock;
+    private String cumulativeDifficulty;
+    private String block; //block id
+    private Long height;
+    private Long timestamp; //time in seconds since genesis block
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Block)) return false;
+        Block block1 = (Block) o;
+        return Objects.equals(previousBlockHash, block1.previousBlockHash) &&
+                Objects.equals(payloadLength, block1.payloadLength) &&
+                Objects.equals(totalAmountATM, block1.totalAmountATM) &&
+                Objects.equals(generationSignature, block1.generationSignature) &&
+                Objects.equals(generator, block1.generator) &&
+                Objects.equals(generatorPublicKey, block1.generatorPublicKey) &&
+                Objects.equals(baseTarget, block1.baseTarget) &&
+                Objects.equals(payloadHash, block1.payloadHash) &&
+                Objects.equals(generatorRS, block1.generatorRS) &&
+                Objects.equals(numberOfTransactions, block1.numberOfTransactions) &&
+                Objects.equals(blockSignature, block1.blockSignature) &&
+                Objects.equals(transactions, block1.transactions) &&
+                Objects.equals(version, block1.version) &&
+                Objects.equals(totalFeeATM, block1.totalFeeATM) &&
+                Objects.equals(previousBlock, block1.previousBlock) &&
+                Objects.equals(cumulativeDifficulty, block1.cumulativeDifficulty) &&
+                Objects.equals(block, block1.block) &&
+                Objects.equals(height, block1.height) &&
+                Objects.equals(timestamp, block1.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(previousBlockHash, payloadLength, totalAmountATM, generationSignature, generator, generatorPublicKey, baseTarget,
+                payloadHash, generatorRS, numberOfTransactions, blockSignature, transactions, version, totalFeeATM, previousBlock, cumulativeDifficulty, block, height, timestamp);
+    }
+
+    public String getPreviousBlockHash() {
+        return previousBlockHash;
+    }
+
+    public void setPreviousBlockHash(String previousBlockHash) {
+        this.previousBlockHash = previousBlockHash;
+    }
+
+    public Long getPayloadLength() {
+        return payloadLength;
+    }
+
+    public void setPayloadLength(Long payloadLength) {
+        this.payloadLength = payloadLength;
+    }
+
+    public Long getTotalAmountATM() {
+        return totalAmountATM;
+    }
+
+    public void setTotalAmountATM(Long totalAmountATM) {
+        this.totalAmountATM = totalAmountATM;
+    }
+
+    public String getGenerationSignature() {
+        return generationSignature;
+    }
+
+    public void setGenerationSignature(String generationSignature) {
+        this.generationSignature = generationSignature;
+    }
+
+    public String getGenerator() {
+        return generator;
+    }
+
+    public void setGenerator(String generator) {
+        this.generator = generator;
+    }
+
+    public String getGeneratorPublicKey() {
+        return generatorPublicKey;
+    }
+
+    public void setGeneratorPublicKey(String generatorPublicKey) {
+        this.generatorPublicKey = generatorPublicKey;
+    }
+
+    public Long getBaseTarget() {
+        return baseTarget;
+    }
+
+    public void setBaseTarget(Long baseTarget) {
+        this.baseTarget = baseTarget;
+    }
+
+    public String getPayloadHash() {
+        return payloadHash;
+    }
+
+    public void setPayloadHash(String payloadHash) {
+        this.payloadHash = payloadHash;
+    }
+
+    public String getGeneratorRS() {
+        return generatorRS;
+    }
+
+    public void setGeneratorRS(String generatorRS) {
+        this.generatorRS = generatorRS;
+    }
+
+    public Long getNumberOfTransactions() {
+        return numberOfTransactions;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public void setNumberOfTransactions(Long numberOfTransactions) {
+        this.numberOfTransactions = numberOfTransactions;
+    }
+
+    public String getBlockSignature() {
+        return blockSignature;
+    }
+
+    public void setBlockSignature(String blockSignature) {
+        this.blockSignature = blockSignature;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public Long getTotalFeeATM() {
+        return totalFeeATM;
+    }
+
+
+    public void setTotalFeeATM(Long totalFeeATM) {
+        this.totalFeeATM = totalFeeATM;
+    }
+
+    public String getPreviousBlock() {
+        return previousBlock;
+    }
+
+    public void setPreviousBlock(String previousBlock) {
+        this.previousBlock = previousBlock;
+    }
+
+    public String getCumulativeDifficulty() {
+        return cumulativeDifficulty;
+    }
+
+    public void setCumulativeDifficulty(String cumulativeDifficulty) {
+        this.cumulativeDifficulty = cumulativeDifficulty;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
+
+    public Long getHeight() {
+        return height;
+    }
+
+    public void setHeight(Long height) {
+        this.height = height;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Block{" +
+            "height=" + height +
+            ", previousBlockHash='" + previousBlockHash + '\'' +
+            ", totalAmountATM=" + totalAmountATM +
+            ", generatorPublicKey='" + generatorPublicKey + '\'' +
+            ", blockSignature='" + blockSignature + '\'' +
+            ", numberOfTransactions=" + numberOfTransactions +
+            ", transactions=" + transactions +
+            ", totalFeeATM=" + totalFeeATM +
+            '}';
+    }
+}
