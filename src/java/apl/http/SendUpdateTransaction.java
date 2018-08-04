a2ac7766ce1a2b913a17c93c232106fa97b631cc
Rostyslav Golda <tx_hv@ukr.net>
2018-08-04 09:38:31 +0300

    Fixed bugs

--- src/java/apl/http/SendUpdateTransaction.java
+++ src/java/apl/http/SendUpdateTransaction.java
@@ -1,17 +1,7 @@
 /*
  * Copyright © 2017-2018 Apollo Foundation
- *
- * See the LICENSE.txt file at the top-level directory of this distribution
- * for licensing information.
- *
- * Unless otherwise agreed in a custom licensing agreement with Jelurida IP B.V.,
- * no part of the Nxt software, including this file, may be copied, modified,
- * propagated, or distributed except according to the terms contained in the
- * LICENSE.txt file.
- *
- * Removal or modification of this copyright notice is prohibited.
- *
  */
+ 
 package apl.http;
 
 import apl.*;
