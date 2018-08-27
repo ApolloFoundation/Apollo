56262353387ddf8e20c90e38ef4c1b2743c19b9b
Andrew Boyarsky <andrew.boyarski@gmail.com>
2018-08-27 10:21:05 +0300

    Added missing ThreadFactoryImpl

---
+++ src/main/java/com/apollocurrency/aplwallet/apl/ThreadFactoryImpl.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2018 Apollo Foundation
+ */
+
+package com.apollocurrency.aplwallet.apl;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadFactoryImpl implements ThreadFactory {
+    private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final String namePrefix;
+
+    public ThreadFactoryImpl(String threadPrefix) {
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() :
+                Thread.currentThread().getThreadGroup();
+        namePrefix = threadPrefix +"-";
+    }
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r,
+                namePrefix + threadNumber.getAndIncrement(),
+                0);
+        if (t.isDaemon())
+            t.setDaemon(false);
+        if (t.getPriority() != Thread.NORM_PRIORITY)
+            t.setPriority(Thread.NORM_PRIORITY);
+        return t;
+    }
+}
