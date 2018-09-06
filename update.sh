38562eaf5b49787f9ba6415c56d65167c0d90e9e
Rostyslav Golda <tx_hv@ukr.net>
2018-09-06 19:34:28 +0300

    Corrected scripts and platforms

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 # second parameter is a update directory which contains unpacked jar for update
 # third parameter is a boolean flag, which indicates desktop mode
 
- 
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 #"
--- update1.sh
+++ update1.sh
@@ -18,10 +18,10 @@ then
     echo Copy update files
     cp -vRa $2/* $1
 
-    cd $1 
+    cd $1
     chmod 755 *.sh
 
-    cd $1 
+    cd $1
     chmod 755 *.sh
 
     if [ $3 == true ]
