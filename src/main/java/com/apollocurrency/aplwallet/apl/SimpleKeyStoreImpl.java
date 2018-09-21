fd5d83b7985f6e95e36544084d8f0b13b47f2382
Andrew Boyarsky <andrew.boyarski@gmail.com>
2018-09-21 21:37:54 +0300

    Add keystore tests, add nonce for key generation, keystore refactoring

--- src/main/java/com/apollocurrency/aplwallet/apl/SimpleKeyStoreImpl.java
+++ src/main/java/com/apollocurrency/aplwallet/apl/SimpleKeyStoreImpl.java
@@ -5,19 +5,20 @@
  package com.apollocurrency.aplwallet.apl;
 
  import com.apollocurrency.aplwallet.apl.crypto.Crypto;
-import com.apollocurrency.aplwallet.apl.util.Convert;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+ import com.apollocurrency.aplwallet.apl.util.Convert;
+ import org.json.simple.JSONObject;
+ import org.json.simple.parser.JSONParser;
+ import org.json.simple.parser.ParseException;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.time.Instant;
-import java.util.List;
-import java.util.stream.Collectors;
+ import java.io.IOException;
+ import java.nio.charset.StandardCharsets;
+ import java.nio.file.Files;
+ import java.nio.file.Path;
+ import java.nio.file.StandardOpenOption;
+ import java.time.Instant;
+ import java.util.List;
+ import java.util.Objects;
+ import java.util.stream.Collectors;
 
  public class SimpleKeyStoreImpl implements KeyStore {
      private Path keystoreDirPath;
@@ -37,33 +38,50 @@ import java.util.stream.Collectors;
 
      @Override
      public byte[] getKeySeed(String passphrase, long accountId) throws IOException, SecurityException {
+         Objects.requireNonNull(passphrase);
          Path privateKeyPath = verify(findKeySeedPaths(accountId), accountId);
          byte[] jsonFileBytes = Files.readAllBytes(privateKeyPath);
-
-         byte[] encryptedKeyBytes = readEncryptedKeySeed(jsonFileBytes);
-
-         byte[] key = Crypto.sha256().digest(Convert.toBytes(passphrase));
-         byte[] decryptedKeySeed = Crypto.aesDecrypt(encryptedKeyBytes, key);
-         long actualAccId = Convert.getId(Crypto.getPublicKey(decryptedKeySeed));
-         if (accountId != actualAccId) {
+         JSONObject jsonObject = parseJSON(jsonFileBytes);
+         byte[] encryptedKeyBytes = readBytes(jsonObject, "encryptedKeySeed");
+         byte[] nonce = readBytes(jsonObject, "nonce");
+         byte[] key = Crypto.getKeySeed(passphrase, nonce);
+         try {
+             byte[] decryptedKeySeed = Crypto.aesDecrypt(encryptedKeyBytes, key);
+             long actualAccId = Convert.getId(Crypto.getPublicKey(decryptedKeySeed));
+             if (accountId != actualAccId) {
+                 throw new SecurityException("Passphrase is incorrect");
+             }
+             return decryptedKeySeed;
+         }
+         catch (RuntimeException e) {
              throw new SecurityException("Passphrase is incorrect");
          }
-         return decryptedKeySeed;
+
      }
 
-     protected byte[] readEncryptedKeySeed(byte[] bytes) {
+     private JSONObject parseJSON(byte[] jsonFileBytes) {
          JSONObject jsonObject;
          try {
-             jsonObject = (JSONObject) new JSONParser().parse(new String(bytes));
+             jsonObject = (JSONObject) new JSONParser().parse(new String(jsonFileBytes));
          }
          catch (ParseException e) {
              throw new RuntimeException("Unable to parse pk json", e);
          }
-         String encryptedKey = (String) jsonObject.get("encryptedKeySeed");
-         if (encryptedKey == null) {
-             throw new RuntimeException("'encryptedKey' should not be null ");
+         return jsonObject;
+     }
+
+     private byte[] generateBytes(int size) {
+         byte[] nonce = new byte[size];
+         Crypto.getSecureRandom().nextBytes(nonce);
+         return nonce;
+     }
+
+     protected byte[] readBytes(JSONObject jsonObject, String name) {
+         String bytes = (String) jsonObject.get(name);
+         if (bytes == null) {
+             throw new RuntimeException("'" + name + " should not be null ");
          }
-         return Convert.parseHexString(encryptedKey);
+         return Convert.parseHexString(bytes);
      }
 
      protected List<Path> findKeySeedPaths(long accountId) throws IOException {
@@ -91,6 +109,8 @@ import java.util.stream.Collectors;
 
      @Override
      public void saveKeySeed(String passphrase, byte[] keySeed) throws IOException {
+         Objects.requireNonNull(passphrase);
+         Objects.requireNonNull(keySeed);
          Instant instant = Instant.now();
          long accountId = Convert.getId(Crypto.getPublicKey(keySeed));
          List<Path> keySeedPaths = findKeySeedPaths(accountId);
@@ -98,13 +118,24 @@ import java.util.stream.Collectors;
              throw new RuntimeException("Unable to save keySeed");
          }
          Path keyPath = keystoreDirPath.resolve(instant.toString() + "---" + Convert.rsAccount(accountId));
-         JSONObject jsonContent = new JSONObject();
-         byte[] key = Crypto.sha256().digest(Convert.toBytes(passphrase));
+         byte[] nonce = generateBytes(16);
+         byte[] key = Crypto.getKeySeed(passphrase, nonce);
+         byte[] encryptedKeySeed = Crypto.aesEncrypt(keySeed, key);
+         JSONObject keySeedJSON = getJSONObject(accountId, encryptedKeySeed, keySeed.length, nonce);
+         storeJSONKeySeed(keyPath, keySeedJSON);
+     }
 
-         jsonContent.put("encryptedKeySeed", Convert.toHexString(Crypto.aesEncrypt(keySeed, key)));
-         jsonContent.put("accountRS", Convert.rsAccount(accountId));
-         jsonContent.put("account", accountId);
-         jsonContent.put("bytes", keySeed.length);
-         Files.write(keyPath, jsonContent.toJSONString().getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE_NEW);
+     protected void storeJSONKeySeed(Path keyPath, JSONObject keySeedJSON) throws IOException {
+         Files.write(keyPath, keySeedJSON.toJSONString().getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE_NEW);
+     }
+
+     protected JSONObject getJSONObject(long accountId, byte[] encryptedKeySeed, int keySeedSize, byte[] nonce) {
+         JSONObject keyJson = new JSONObject();
+         keyJson.put("encryptedKeySeed", Convert.toHexString(encryptedKeySeed));
+         keyJson.put("accountRS", Convert.rsAccount(accountId));
+         keyJson.put("account", accountId);
+         keyJson.put("bytes", keySeedSize);
+         keyJson.put("nonce", Convert.toHexString(nonce));
+         return keyJson;
      }
  }
