b7c8bfcf46c56213120a18405dcab528dfd184c9
Rostyslav Golda <tx_hv@ukr.net>
2018-10-31 23:18:24 +0200

    Fixed tor run script

--- run-tor.sh
+++ run-tor.sh
@@ -10,5 +10,6 @@ if [ -x jre/bin/java ]; then
 else
     JAVA=java
 fi
-${JAVA} -DsocksProxyHost=localhost -DsocksProxyPort=9050 -cp target/classes:target/lib/*:conf:addons/classes:addons/lib/* com.apollocurrency.aplwallet.apl.Apl
+${JAVA} -DsocksProxyHost=localhost -DsocksProxyPort=9050 -Dapl.runtime.mode=desktop -jar Apollo.jar
+
 
