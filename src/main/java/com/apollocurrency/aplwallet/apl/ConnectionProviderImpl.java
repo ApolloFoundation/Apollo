f1163782d2820ac0818d3c2785193e5c8fd6fd83
Andrew Boyarsky <andrew.boyarski@gmail.com>
2018-11-13 18:09:16 +0200

    Resolve compile errors

--- src/main/java/com/apollocurrency/aplwallet/apl/ConnectionProviderImpl.java
+++ src/main/java/com/apollocurrency/aplwallet/apl/ConnectionProviderImpl.java
@@ -4,39 +4,39 @@
 
 package com.apollocurrency.aplwallet.apl;
 
-import com.apollocurrency.aplwallet.apl.updater.ConnectionProvider;
-
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import com.apollocurrency.aplwallet.apl.updater.ConnectionProvider;
 
 public class ConnectionProviderImpl implements ConnectionProvider {
     @Override
     public Connection getConnection() throws SQLException {
-        return Db.db.getConnection();
+        return Db.getDb().getConnection();
     }
 
     @Override
     public Connection beginTransaction() {
-        return Db.db.beginTransaction();
+        return Db.getDb().beginTransaction();
     }
 
     @Override
     public void rollbackTransaction(Connection connection) {
-        Db.db.rollbackTransaction();
+        Db.getDb().rollbackTransaction();
     }
 
     @Override
     public void commitTransaction(Connection connection) {
-        Db.db.commitTransaction();
+        Db.getDb().commitTransaction();
     }
 
     @Override
     public void endTransaction(Connection connection) {
-        Db.db.endTransaction();
+        Db.getDb().endTransaction();
     }
 
     @Override
     public boolean isInTransaction(Connection connection) {
-        return Db.db.isInTransaction();
+        return Db.getDb().isInTransaction();
     }
 }
