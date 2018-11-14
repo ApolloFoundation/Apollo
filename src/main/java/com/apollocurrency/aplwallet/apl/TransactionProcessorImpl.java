071baf013f96d2744dec61817520c13845f480bf
Rostyslav Golda <tx_hv@ukr.net>
2018-11-14 14:49:13 +0200

    TransactionImpl import hotfix

--- src/main/java/com/apollocurrency/aplwallet/apl/TransactionProcessorImpl.java
+++ src/main/java/com/apollocurrency/aplwallet/apl/TransactionProcessorImpl.java
@@ -54,6 +54,7 @@ import com.apollocurrency.aplwallet.apl.util.JSON;
 import com.apollocurrency.aplwallet.apl.util.Listener;
 import com.apollocurrency.aplwallet.apl.util.Listeners;
 import com.apollocurrency.aplwallet.apl.util.ThreadPool;
+import com.apollocurrency.aplwallet.apl.util.NtpTime;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
