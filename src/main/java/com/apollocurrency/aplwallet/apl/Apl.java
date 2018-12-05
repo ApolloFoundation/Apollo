40b3cc5edfd139f340b14a127777d2c636cb128a
Rostyslav Golda <tx_hv@ukr.net>
2018-12-05 12:31:12 +0200

    1.23.2

--- src/main/java/com/apollocurrency/aplwallet/apl/Apl.java
+++ src/main/java/com/apollocurrency/aplwallet/apl/Apl.java
@@ -73,7 +73,7 @@ public final class Apl {
     private static Logger LOG;
 
 
-    public static final Version VERSION = Version.from("1.23.1");
+    public static final Version VERSION = Version.from("1.23.2");
 
     public static final String APPLICATION = "Apollo";
     private static Thread shutdownHook;
