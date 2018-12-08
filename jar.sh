075207c381900c78562ca89311def7d4fd45c6e0
Oleksiy Lukin <alukin@gmail.com>
2018-12-08 11:41:57 +0200

    cleanup

--- jar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-APPLICATION="Apollo"
-java -cp classes com.apollocurrency.aplwallet.apl.tools.ManifestGenerator
-/bin/rm -f ${APPLICATION}.jar
-jar cfm ${APPLICATION}.jar resource/apl.manifest.mf -C classes . || exit 1
-/bin/rm -f ${APPLICATION}service.jar
-jar cfm ${APPLICATION}service.jar resource/aplservice.manifest.mf -C classes . || exit 1
-
-echo "jar files generated successfully"
