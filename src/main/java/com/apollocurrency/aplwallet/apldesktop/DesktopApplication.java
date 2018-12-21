fcc1b7bbaea4ce3da119914a2d30918be5cc6294
Rostyslav Golda <tx_hv@ukr.net>
2018-12-22 00:59:03 +0200

    Disabled firebug

--- src/main/java/com/apollocurrency/aplwallet/apldesktop/DesktopApplication.java
+++ src/main/java/com/apollocurrency/aplwallet/apldesktop/DesktopApplication.java
@@ -368,7 +368,8 @@ public class DesktopApplication extends Application {
         private static WebView invisible;
         
         private static void enableFirebug(final WebEngine engine) {
-            engine.executeScript("if (!document.getElementById('FirebugLite')){E = document['createElement' + 'NS'] && document.documentElement.namespaceURI;E = E ? document['createElement' + 'NS'](E, 'script') : document['createElement']('script');E['setAttribute']('id', 'FirebugLite');E['setAttribute']('src', 'https://getfirebug.com/' + 'firebug-lite.js' + '#startOpened');E['setAttribute']('FirebugLite', '4');(document['getElementsByTagName']('head')[0] || document['getElementsByTagName']('body')[0]).appendChild(E);E = new Image;E['setAttribute']('src', 'https://getfirebug.com/' + '#startOpened');}"); 
+            //uncomment it to enable firebug
+            //engine.executeScript("if (!document.getElementById('FirebugLite')){E = document['createElement' + 'NS'] && document.documentElement.namespaceURI;E = E ? document['createElement' + 'NS'](E, 'script') : document['createElement']('script');E['setAttribute']('id', 'FirebugLite');E['setAttribute']('src', 'https://getfirebug.com/' + 'firebug-lite.js' + '#startOpened');E['setAttribute']('FirebugLite', '4');(document['getElementsByTagName']('head')[0] || document['getElementsByTagName']('body')[0]).appendChild(E);E = new Image;E['setAttribute']('src', 'https://getfirebug.com/' + '#startOpened');}"); 
         }
         
         public void startDesktopApplication() {
