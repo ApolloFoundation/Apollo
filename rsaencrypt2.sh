9e26b2ed85eae5df05b10fefd7cbcefbf4215226
Rostyslav Golda <tx_hv@ukr.net>
2019-01-08 12:09:13 +0200

    Removed unnecesary files

--- rsaencrypt2.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for i in $1 $2 $3
-do
-    java -cp "target/classes:target/lib/*" com.apollocurrency.aplwallet.apl.tools.RSAEncryption false $i ../rost-privkey4.pem
-    echo
-done
