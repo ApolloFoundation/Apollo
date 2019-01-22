e72a7f10ac05f8ced7346132bda606df19970424
Oleksiy Lukin <alukin@gmail.com>
2019-01-22 17:23:36 +0200

    part of old html required by APITestServlet is renamed to apl-html and now builds with Maven. WebUI and APITestServlet are OK. TODO: replace zip downloading URL to real one

--- apl-exec/src/main/java/com/apollocurrency/aplwallet/apl/exec/webui/WebUiExtractor.java
+++ apl-exec/src/main/java/com/apollocurrency/aplwallet/apl/exec/webui/WebUiExtractor.java
@@ -18,6 +18,7 @@ import java.util.concurrent.Callable;
  */
 public class WebUiExtractor implements Callable<Boolean>{
     private static String webUiZipFile = "web-wallet.zip";
+    private static String testUiZipFile = "apl-html";
     private DirProvider dirProvider;
     public final static String WEB_UI_DIR="webui";
 
@@ -51,6 +52,32 @@ public class WebUiExtractor implements Callable<Boolean>{
       return res;
     }
     
+    private File findTestUiZip() throws FileNotFoundException{
+      File res = null;
+      File dir = new File(dirProvider.getBinDirectory().getAbsolutePath()+File.separator+"lib");
+      if(!dir.exists()){//we are in dev. env or tests
+          dir = new File(dirProvider.getBinDirectory().getAbsolutePath()+"/apl-exec/target/lib");
+      }
+      if(!dir.exists()){
+          throw new FileNotFoundException(dir.getAbsolutePath());
+      }
+      File[] filesList = dir.listFiles(new FileFilter(){
+          @Override
+          public boolean accept(File pathname) {
+              return pathname.getName().endsWith("zip");
+          }
+      });
+        for(File f : filesList){
+            if(f.isFile()){
+                if(f.getName().startsWith(testUiZipFile)){
+                  res = new File(dir.getAbsolutePath()+File.separator+f.getName());
+                  break;
+                }
+            }
+        }  
+      return res;
+    }
+    
     private File findDest(){
         File res = new File(dirProvider.getAppHomeDir()+File.separator+WEB_UI_DIR);
         return res;
@@ -63,7 +90,7 @@ public class WebUiExtractor implements Callable<Boolean>{
         if(!dest.exists()){
             res=false;
         }else{
-            //TODO: check version of UI
+            //TODO: check version of UI somehow, depends on zip name in our Artifactory
             long lmd = dest.lastModified();
             long lmz =zip.lastModified();
             res=lmd<lmz;
@@ -89,6 +116,7 @@ public class WebUiExtractor implements Callable<Boolean>{
               removeDir(dest.getAbsolutePath());
             }
             res=Zip.extract(findWebUiZip().getAbsolutePath(), dest.getAbsolutePath());
+            res=res&Zip.extract(findTestUiZip().getAbsolutePath(), dest.getAbsolutePath());
         }
         return res;
     }
