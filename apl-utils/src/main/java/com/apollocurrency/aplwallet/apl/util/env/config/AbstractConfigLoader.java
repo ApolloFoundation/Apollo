25d1824d8c6507033f1d8b2b734aa39249456a18
Andrew Boyarsky <zandrey322@gmail.com>
2019-02-06 15:48:49 +0200

    Fix loading of resources on windows

--- apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/env/config/AbstractConfigLoader.java
+++ apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/env/config/AbstractConfigLoader.java
@@ -42,7 +42,8 @@ public abstract class AbstractConfigLoader<T> implements ConfigLoader<T> {
 
 
     private void loadFromResources() {
-        String fn = DEFAULT_CONFIG_DIR + File.separator + resourceName;
+        // using '/' as separator instead of platform dependent File.separator
+        String fn = DEFAULT_CONFIG_DIR + "/" + resourceName;
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         try (InputStream is = classloader.getResourceAsStream(fn)) {
             T defaultConfig = read(is);
