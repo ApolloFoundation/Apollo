8b470230ee9df9471d807d7e29d8588e704d662d
Yuriy Larin <yuriy.larin@firstbridge.io>
2019-02-21 14:27:35 +0200

    added shard file name formatter by using Id + unit test

---
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/app/ShardNameHelper.java
@@ -0,0 +1,21 @@
+package com.apollocurrency.aplwallet.apl.core.app;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.slf4j.Logger;
+
+/**
+ * Class for mapping shard_id into shard file name as String.
+ */
+public class ShardNameHelper {
+    private static final Logger log = getLogger(ShardNameHelper.class);
+
+    private final static String SHARD_NAME_PATTERN = "apl-shard-%07d";
+
+    public static String getShardNameByShardId(Long shardId) {
+        String result = String.format(SHARD_NAME_PATTERN, shardId);
+        log.debug(result);
+        return result;
+    }
+
+}
---
+++ apl-core/src/test/java/com/apollocurrency/aplwallet/apl/core/app/ShardNameHelperTest.java
@@ -0,0 +1,18 @@
+package com.apollocurrency.aplwallet.apl.core.app;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ShardNameHelperTest {
+
+    @Test
+    void getShardName() {
+        String result = ShardNameHelper.getShardNameByShardId(001L);
+        assertEquals("apl-shard-0000001", result);
+
+        result = ShardNameHelper.getShardNameByShardId(2001L);
+        assertEquals("apl-shard-0002001", result);
+    }
+}
