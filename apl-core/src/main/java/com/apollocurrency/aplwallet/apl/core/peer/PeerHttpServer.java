746d904ca4ab34569c7a856f83d9eb704de7db0b
Oleksiy Lukin <alukin@gmail.com>
2019-03-22 13:07:23 +0200

    Peers class refactoring work in progress

---
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/peer/PeerHttpServer.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apollocurrency.aplwallet.apl.core.peer;
+
+import com.apollocurrency.aplwallet.apl.crypto.Convert;
+import com.apollocurrency.aplwallet.apl.util.ThreadPool;
+import com.apollocurrency.aplwallet.apl.util.UPnP;
+import com.apollocurrency.aplwallet.apl.util.injectable.PropertiesHolder;
+import java.util.EnumSet;
+import javax.enterprise.inject.spi.CDI;
+import javax.servlet.DispatcherType;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.gzip.GzipHandler;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlets.DoSFilter;
+import org.slf4j.Logger;
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ *
+ * @author al
+ */
+public class PeerHttpServer {
+    
+     private static final Logger LOG = getLogger(PeerHttpServer.class);
+     
+     static final int MAX_PLATFORM_LENGTH = 30;
+    
+     static PropertiesHolder propertiesHolder = CDI.current().select(PropertiesHolder.class).get();   
+     static boolean shareMyAddress;
+     static int myPeerServerPort;
+     static boolean enablePeerUPnP;    
+     static String myPlatform;
+     static String myAddress;
+     static Server peerServer;
+    
+    //TODO: remove static context
+    static final UPnP upnp = UPnP.getInstance();
+    
+    private PeerHttpServer() {
+    }
+    
+    public static void init()
+    {
+        shareMyAddress = propertiesHolder.getBooleanProperty("apl.shareMyAddress") && ! propertiesHolder.isOffline();  
+        myPeerServerPort = propertiesHolder.getIntProperty("apl.myPeerServerPort");
+        String platform = propertiesHolder.getStringProperty("apl.myPlatform", System.getProperty("os.name") + " " + System.getProperty("os.arch"));
+        if (platform.length() > MAX_PLATFORM_LENGTH) {
+            platform = platform.substring(0, MAX_PLATFORM_LENGTH);
+        }
+        enablePeerUPnP = propertiesHolder.getBooleanProperty("apl.enablePeerUPnP");
+        
+        myPlatform = platform;
+        myAddress = Convert.emptyToNull(propertiesHolder.getStringProperty("apl.myAddress", "").trim());      
+        Peers.shutdown = false;
+        if (shareMyAddress) {
+            peerServer = new Server();
+            ServerConnector connector = new ServerConnector(peerServer);
+            final int port = myPeerServerPort;
+            connector.setPort(port);
+            final String host = propertiesHolder.getStringProperty("apl.peerServerHost");
+            connector.setHost(host);
+            connector.setIdleTimeout(propertiesHolder.getIntProperty("apl.peerServerIdleTimeout"));
+            connector.setReuseAddress(true);
+            peerServer.addConnector(connector);
+            ServletContextHandler ctxHandler = new ServletContextHandler();
+            ctxHandler.setContextPath("/");
+            ServletHolder peerServletHolder = new ServletHolder(new PeerServlet());
+            ctxHandler.addServlet(peerServletHolder, "/*");
+            if (propertiesHolder.getBooleanProperty("apl.enablePeerServerDoSFilter")) {
+                FilterHolder dosFilterHolder = ctxHandler.addFilter(DoSFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
+                dosFilterHolder.setInitParameter("maxRequestsPerSec", propertiesHolder.getStringProperty("apl.peerServerDoSFilter.maxRequestsPerSec"));
+                dosFilterHolder.setInitParameter("delayMs", propertiesHolder.getStringProperty("apl.peerServerDoSFilter.delayMs"));
+                dosFilterHolder.setInitParameter("maxRequestMs", propertiesHolder.getStringProperty("apl.peerServerDoSFilter.maxRequestMs"));
+                dosFilterHolder.setInitParameter("trackSessions", "false");
+                dosFilterHolder.setAsyncSupported(true);
+            }
+            if (Peers.isGzipEnabled) {
+                GzipHandler gzipHandler = new GzipHandler();
+                gzipHandler.setIncludedMethods("GET", "POST");
+                gzipHandler.setIncludedPaths("/*");
+                gzipHandler.setMinGzipSize(Peers.MIN_COMPRESS_SIZE);
+                ctxHandler.setGzipHandler(gzipHandler);
+            }
+            peerServer.setHandler(ctxHandler);
+            peerServer.setStopAtShutdown(true);
+            ThreadPool.runBeforeStart("PeerUPnPInit", () -> {
+                try {
+                    if (enablePeerUPnP) {
+                        Connector[] peerConnectors = peerServer.getConnectors();
+                        for (Connector peerConnector : peerConnectors) {
+                            if (peerConnector instanceof ServerConnector) {
+                                upnp.addPort(((ServerConnector) peerConnector).getPort());
+                            }
+                        }
+                    }
+                    peerServer.start();
+                    LOG.info("Started peer networking server at " + host + ":" + port);
+                } catch (Exception e) {
+                    LOG.error("Failed to start peer networking server", e);
+                    throw new RuntimeException(e.toString(), e);
+                }
+            }, true);
+        } else {
+            peerServer = null;
+            LOG.info("shareMyAddress is disabled, will not start peer networking server");
+        }
+    }
+    
+}
