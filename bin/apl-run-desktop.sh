61ae62aba419dc7bdace36cad3eebb397e775005
Rostyslav Golda <tx_hv@ukr.net>
2019-03-29 19:23:11 +0200

    Fixed icon location in desktop script

--- bin/apl-run-desktop.sh
+++ bin/apl-run-desktop.sh
@@ -11,7 +11,7 @@ unamestr=`uname`
 xdock=''
 
 if [[ "$unamestr" == 'Darwin' ]]; then
-  xdock=-Xdock:icon=./favicon.ico
+  xdock=-Xdock:icon=../favicon.ico
 fi
 # uncomment when GUI will start standalone
 # ${JAVA_CMD} $xdock  -jar ${MAIN_GUI_JAR}
