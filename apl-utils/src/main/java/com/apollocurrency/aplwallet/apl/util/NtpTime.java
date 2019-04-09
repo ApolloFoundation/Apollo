2a05944b4de587277003a40d558abdb6399b9884
Andrew Boyarsky <andrew.boyarski@gmail.com>
2019-04-09 18:13:11 +0300

    Optimize ntpTime

--- apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/NtpTime.java
+++ apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/NtpTime.java
@@ -12,8 +12,10 @@ import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.inject.Singleton;
 
 @Singleton
@@ -23,14 +25,12 @@ public class NtpTime {
 
     private static final int REFRESH_FREQUENCY = 60;
     private static final String TIME_SERVICE = "pool.ntp.org";
-
+    private static final int DEFAULT_TIMEOUT = 3000; // in millis
     private volatile long timeOffset = 0;
+    private NTPUDPClient client;
 
     private void setTimeDrift() {
-        NTPUDPClient client = new NTPUDPClient();
-
         try {
-            client.open();
             InetAddress hostAddr = InetAddress.getByName(TIME_SERVICE);
             TimeInfo info = client.getTime(hostAddr);
             info.computeDetails(); // compute offset/delay if not already done
@@ -48,9 +48,6 @@ public class NtpTime {
             LOG.error(e.getMessage(), e);
             timeOffset = 0;
         }
-        finally {
-             client.close();
-        }
     }
     
     public long getTime() {
@@ -63,7 +60,22 @@ public class NtpTime {
     public void start() {
         Runnable timeUpdate = this::setTimeDrift;
         ThreadPool.scheduleThread("NTP Update", timeUpdate, REFRESH_FREQUENCY, TimeUnit.SECONDS);
+        setUpClient();
     }
-    
-            
+
+    private void setUpClient() {
+        try {
+            client = new NTPUDPClient();
+            client.setDefaultTimeout(DEFAULT_TIMEOUT);
+            client.open();
+        }
+        catch (SocketException e) {
+            throw new RuntimeException(e.toString(), e);
+        }
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        client.close();
+    }
 }
