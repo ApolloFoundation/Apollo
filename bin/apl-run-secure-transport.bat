152f803f22aae5f3fff2da353a55f8e6d23db80d
Rostyslav Golda <tx_hv@ukr.net>
2019-04-12 16:15:40 +0300

    Windows startup scropt correction

--- bin/apl-run-secure-transport.bat
+++ bin/apl-run-secure-transport.bat
@@ -3,8 +3,7 @@
 set DIRP=%~dp0
 call "%DIRP%\apl-common.bat"
 @REM start Apollo
-
-cd %APL_TOP%\secureTransport
+cd "%APL_TOP%\secureTransport"
 start runClient.bat 
 cd ..
-%JAVA_CMD%  -DsocksProxyHost=10.75.110.1 -DsocksProxyPort=1088 -Dapl.runtime.mode=desktop -Dapl.enablePeerUPnP=false -jar "%APL_MAIN%"
+%JAVA_CMD% -DsocksProxyHost=10.75.110.1 -DsocksProxyPort=1088 -Dapl.runtime.mode=desktop -Dapl.enablePeerUPnP=false -jar %APL_MAIN%
