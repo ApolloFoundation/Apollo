369486cb4ffe72919c5500ac7c51fe58411e4ff6
Oleksiy Lukin <alukin@gmail.com>
2019-04-17 20:13:42 +0300

    db download shell script added

---
+++ bin/apl-download-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# (C) 2019 Apollo Foundation 
+# Downloads Apollo blockchain database and extracts it to user's home location
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+ . ${DIR}/apl-common.sh 
+
+${DIR}/apl-stop.sh
+
+DB_LOCATION= ~/.apl-blockchain/apl-blockchain-db
+DB_FILE=b5d7b6.jar
+DB_URL="https://s3.amazonaws.com/updates.apollowallet.org/database/${DB_FILE}"
+cd $DB_LOCATION
+curl --output $DB_FILE $DB_URL
+${JAR_CMD} -xvf $DB_FILE
+rm -rf META-INF
