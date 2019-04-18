8180a7947b352787d301c31a72c704b2200bd932
Andrew Boyarsky <andrew.boyarski@gmail.com>
2019-04-18 13:51:59 +0300

    Add retrieving stats for one peer

--- apl-tools/src/main/java/com/apollocurrency/aplwallet/apl/tools/impl/heightmon/web/NetStatController.java
+++ apl-tools/src/main/java/com/apollocurrency/aplwallet/apl/tools/impl/heightmon/web/NetStatController.java
@@ -6,15 +6,19 @@ package com.apollocurrency.aplwallet.apl.tools.impl.heightmon.web;
 
 import com.apollocurrency.aplwallet.apl.tools.impl.heightmon.HeightMonitorService;
 import com.apollocurrency.aplwallet.apl.tools.impl.heightmon.model.NetworkStats;
+import com.apollocurrency.aplwallet.apl.tools.impl.heightmon.model.PeerDiffStat;
 import com.apollocurrency.aplwallet.apl.tools.impl.heightmon.model.PeerInfo;
 
 import java.net.UnknownHostException;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -39,6 +43,7 @@ public class NetStatController {
 
 
     @POST
+    @Path("/peers")
     @Produces(MediaType.APPLICATION_JSON)
     public Response addPeer(@NotNull @QueryParam("ip") String ip, @QueryParam("port") Integer port, @QueryParam("schema") String schema) {
         try {
@@ -52,7 +57,24 @@ public class NetStatController {
             return Response.ok(heightMonitorService.addPeer(peerInfo)).build();
         }
         catch (UnknownHostException e) {
-            return Response.status(422).build();
+            return Response.status(422, e.getLocalizedMessage()).build();
+        }
+    }
+
+    @GET
+    @Path("/peers/{ip}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPeerStats(@NotNull @PathParam("ip") String ip) {
+        List<PeerDiffStat> diffStats = heightMonitorService.getLastStats()
+                .getPeerDiffStats()
+                .stream()
+                .filter(peerDiffStat -> peerDiffStat.getPeer1().equalsIgnoreCase(ip)
+                        || peerDiffStat.getPeer2().equalsIgnoreCase(ip))
+                .collect(Collectors.toList());
+        if (diffStats.isEmpty()) {
+            return Response.status(422, "No monitoring data found for peer " + ip).build();
+        } else {
+            return Response.ok(diffStats).build();
         }
     }
 }
