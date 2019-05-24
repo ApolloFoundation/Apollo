6bd8980ffff662b6057dd743ec509e0b9060dd5a
Yuriy Larin <yuriy.larin@firstbridge.io>
2019-05-24 12:19:21 +0300

    minor clean up

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/shard/helper/CsvImporterImpl.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/shard/helper/CsvImporterImpl.java
@@ -21,7 +21,6 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.Base64;
-import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
