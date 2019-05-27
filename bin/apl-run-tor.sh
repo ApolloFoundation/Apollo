b99ac9e96503f0b4929ede112bc8ebda108247b2
Rostyslav Golda <tx_hv@ukr.net>
2019-05-27 13:20:26 +0300

    Sent tor into backgroung

--- bin/apl-run-tor.sh
+++ bin/apl-run-tor.sh
@@ -16,7 +16,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 TOR_DIR=${APL_TOP_DIR}/${TOR_DIST_DIR}
 echo Tor dir  = ${TOR_DIR}
 if [ -x ${TOR_DIR} ];  then
-    TOR_CMD="${TOR_DIR}/tor"
+    TOR_CMD="${TOR_DIR}/tor &"
 else
   if [[ -n $(type -p tor) ]]
   then
