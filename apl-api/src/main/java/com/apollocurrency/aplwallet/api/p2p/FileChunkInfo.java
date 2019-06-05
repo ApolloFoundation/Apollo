1f035e1207182ea8e892af39611efb80b697f0d2
Yuriy Larin <yuriy.larin@firstbridge.io>
2019-06-05 17:17:15 +0300

    added hash/equals/toString methods for keeping in hash map

--- apl-api/src/main/java/com/apollocurrency/aplwallet/api/p2p/FileChunkInfo.java
+++ apl-api/src/main/java/com/apollocurrency/aplwallet/api/p2p/FileChunkInfo.java
@@ -3,6 +3,8 @@
  */
 package com.apollocurrency.aplwallet.api.p2p;
 
+import java.util.Objects;
+
 /**
  *
  * @author alukin@gmail.com
@@ -20,4 +22,35 @@ public class FileChunkInfo {
      */
     public int present;
     public long crc;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FileChunkInfo that = (FileChunkInfo) o;
+        return chunkId == that.chunkId &&
+                offset == that.offset &&
+                size == that.size &&
+                present == that.present &&
+                crc == that.crc &&
+                Objects.equals(fileId, that.fileId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fileId, chunkId, offset, size, present, crc);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("FileChunkInfo{");
+        sb.append("fileId='").append(fileId).append('\'');
+        sb.append(", chunkId=").append(chunkId);
+        sb.append(", offset=").append(offset);
+        sb.append(", size=").append(size);
+        sb.append(", present=").append(present);
+        sb.append(", crc=").append(crc);
+        sb.append('}');
+        return sb.toString();
+    }
 }
--- apl-api/src/main/java/com/apollocurrency/aplwallet/api/p2p/FileDownloadInfo.java
+++ apl-api/src/main/java/com/apollocurrency/aplwallet/api/p2p/FileDownloadInfo.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 /**
  *
@@ -18,4 +19,29 @@ public class FileDownloadInfo {
     @JsonIgnore
     /** record creation date, needed by cache */
     public Date created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FileDownloadInfo that = (FileDownloadInfo) o;
+        return Objects.equals(fileInfo, that.fileInfo) &&
+                Objects.equals(chunks, that.chunks) &&
+                Objects.equals(created, that.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fileInfo, chunks, created);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("FileDownloadInfo{");
+        sb.append("fileInfo=").append(fileInfo);
+        sb.append(", chunks=").append(chunks);
+        sb.append(", created=").append(created);
+        sb.append('}');
+        return sb.toString();
+    }
 }
