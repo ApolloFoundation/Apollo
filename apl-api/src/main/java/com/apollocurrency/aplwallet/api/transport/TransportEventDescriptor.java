38d1be27e106e04d1465ee38fa3ef1358b093e01
Serhiy Lymar <limar.sergey@gmail.com>
2019-06-10 21:24:50 +0300

    License headers. Now they are of correct version

--- apl-api/src/main/java/com/apollocurrency/aplwallet/api/transport/TransportEventDescriptor.java
+++ apl-api/src/main/java/com/apollocurrency/aplwallet/api/transport/TransportEventDescriptor.java
@@ -1,15 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright © 2018-2019 Apollo Foundation
  */
+
 package com.apollocurrency.aplwallet.api.transport;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  *
- * @author nemez
+ * @author Serhiy Lymar
  */
 public class TransportEventDescriptor {
     @JsonProperty("event")
