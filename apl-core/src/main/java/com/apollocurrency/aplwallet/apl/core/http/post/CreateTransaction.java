727860b8f37ae72fdfeca36f720ad84491baa973
leonidstr <leonid.stryzhevskyi@firstbridge.io>
2019-07-03 13:08:42 +0300

    Refactoring Create transaction class

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/http/post/CreateTransaction.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/http/post/CreateTransaction.java
@@ -30,14 +30,13 @@ import com.apollocurrency.aplwallet.apl.core.http.JSONData;
 import com.apollocurrency.aplwallet.apl.core.http.JSONResponses;
 import com.apollocurrency.aplwallet.apl.core.http.ParameterException;
 import com.apollocurrency.aplwallet.apl.core.http.ParameterParser;
+import com.apollocurrency.aplwallet.apl.core.model.CreateTransactionRequest;
 import com.apollocurrency.aplwallet.apl.core.phasing.model.PhasingParams;
 import com.apollocurrency.aplwallet.apl.core.transaction.FeeCalculator;
 import com.apollocurrency.aplwallet.apl.core.transaction.TransactionValidator;
 import com.apollocurrency.aplwallet.apl.core.transaction.messages.Attachment;
-import com.apollocurrency.aplwallet.apl.core.transaction.messages.EncryptToSelfMessageAppendix;
 import com.apollocurrency.aplwallet.apl.core.transaction.messages.EncryptedMessageAppendix;
 import com.apollocurrency.aplwallet.apl.core.transaction.messages.MessageAppendix;
-import com.apollocurrency.aplwallet.apl.core.transaction.messages.PhasingAppendix;
 import com.apollocurrency.aplwallet.apl.core.transaction.messages.PhasingAppendixV2;
 import com.apollocurrency.aplwallet.apl.core.transaction.messages.PrunableEncryptedMessageAppendix;
 import com.apollocurrency.aplwallet.apl.core.transaction.messages.PrunablePlainMessageAppendix;
@@ -181,142 +180,171 @@ public abstract class CreateTransaction extends AbstractAPIRequestHandler {
         return new PhasingParams(votingModel, holdingId, quorum, minBalance, minBalanceModel, whitelist);
     }
 
-    public JSONStreamAware createTransaction(HttpServletRequest req, Account senderAccount, long recipientId,
-                                            long amountATM, Attachment attachment) throws AplException.ValidationException, ParameterException {
-        String deadlineValue = req.getParameter("deadline");
-        String referencedTransactionFullHash = Convert.emptyToNull(req.getParameter("referencedTransactionFullHash"));
-        String secretPhrase = ParameterParser.getSecretPhrase(req, false);
-        String publicKeyValue = Convert.emptyToNull(req.getParameter("publicKey"));
+    public JSONStreamAware createTransaction(HttpServletRequest req, Account senderAccount, long recipientId, long amountATM, Attachment attachment) throws AplException.ValidationException, ParameterException {
         String passphrase = Convert.emptyToNull(ParameterParser.getPassphrase(req, false));
-        boolean broadcast = !"false".equalsIgnoreCase(req.getParameter("broadcast")) && (secretPhrase != null || passphrase != null);
+        String secretPhrase = ParameterParser.getSecretPhrase(req, false);
+        Boolean encryptedMessageIsPrunable = Boolean.valueOf(req.getParameter("encryptedMessageIsPrunable"));
+        Boolean messageIsPrunable = Boolean.valueOf(req.getParameter("messageIsPrunable"));
+        Boolean isPhased = Boolean.valueOf(req.getParameter("phased"));
+        Account recipient = null;
+
+        if(recipientId != 0) {
+            recipient = Account.getAccount(recipientId);
+        }
+
+        CreateTransactionRequest createTransactionRequest = CreateTransactionRequest.builder()
+                .broadcast(!"false".equalsIgnoreCase(req.getParameter("broadcast")) && (secretPhrase != null || passphrase != null))
+                .deadlineValue(req.getParameter("deadline"))
+                .referencedTransactionFullHash(Convert.emptyToNull(req.getParameter("referencedTransactionFullHash")))
+                .publicKeyValue(Convert.emptyToNull(req.getParameter("publicKey")))
+                .publicKey(ParameterParser.getPublicKey(req, senderAccount.getId()))
+                .keySeed(ParameterParser.getKeySeed(req, senderAccount.getId(), false))
+
+                .amountATM(amountATM)
+                .feeATM(ParameterParser.getFeeATM(req))
+                .senderAccount(senderAccount)
+                .recipientId(recipientId)
+                .recipientPublicKey(Convert.emptyToNull(req.getParameter("recipientPublicKey")))
+                .phased(isPhased)
+                .phasing(isPhased ? parsePhasing(req) : null)
+
+                .attachment(attachment)
+                .encryptToSelfMessage(ParameterParser.getEncryptToSelfMessage(req, senderAccount.getId()))
+                .encryptedMessageIsPrunable(encryptedMessageIsPrunable)
+                .appendix(ParameterParser.getEncryptedMessage(req, recipient, senderAccount.getId(),encryptedMessageIsPrunable))
+                .messageIsPrunable(messageIsPrunable)
+                .message(ParameterParser.getPlainMessage(req, messageIsPrunable))
+
+                .ecBlockHeight(ParameterParser.getInt(req, "ecBlockHeight", 0, Integer.MAX_VALUE, false))
+                .ecBlockId(Convert.parseUnsignedLong(req.getParameter("ecBlockId")))
+
+                .build();
+
+
+        Transaction transaction;
+        JSONObject response = new JSONObject();
+        try {
+            transaction = createTransaction(createTransactionRequest);
+        } catch (AplException.ValidationException e) {
+            return e.getJsonResponce();
+        }
+
+        JSONObject transactionJSON = JSONData.unconfirmedTransaction(transaction);
+        response.put("transactionJSON", transactionJSON);
+        try {
+            response.put("unsignedTransactionBytes", Convert.toHexString(transaction.getUnsignedBytes()));
+        } catch (AplException.NotYetEncryptedException ignore) {}
+        if (createTransactionRequest.getKeySeed() != null) {
+            response.put("transaction", transaction.getStringId());
+            response.put("fullHash", transactionJSON.get("fullHash"));
+            response.put("transactionBytes", Convert.toHexString(transaction.getBytes()));
+            response.put("signatureHash", transactionJSON.get("signatureHash"));
+        }
+        if (createTransactionRequest.isBroadcast()) {
+            response.put("broadcasted", true);
+        } else {
+            response.put("broadcasted", false);
+        }
+
+        return response;
+    }
+
+    public Transaction createTransaction(CreateTransactionRequest txRequest) throws AplException.ValidationException, ParameterException {
         EncryptedMessageAppendix encryptedMessage = null;
         PrunableEncryptedMessageAppendix prunableEncryptedMessage = null;
-        if (attachment.getTransactionType().canHaveRecipient() && recipientId != 0) {
-            Account recipient = Account.getAccount(recipientId);
-            if ("true".equalsIgnoreCase(req.getParameter("encryptedMessageIsPrunable"))) {
-                prunableEncryptedMessage = (PrunableEncryptedMessageAppendix) ParameterParser.getEncryptedMessage(req, recipient,
-                        senderAccount.getId(),true);
+
+        if (txRequest.getAttachment().getTransactionType().canHaveRecipient() && txRequest.getRecipientId() != 0) {
+            if (txRequest.isEncryptedMessageIsPrunable()) {
+                prunableEncryptedMessage = (PrunableEncryptedMessageAppendix) txRequest.getAppendix();
             } else {
-                encryptedMessage = (EncryptedMessageAppendix) ParameterParser.getEncryptedMessage(req, recipient, senderAccount.getId(), false);
+                encryptedMessage = (EncryptedMessageAppendix) txRequest.getAppendix();
             }
         }
-        EncryptToSelfMessageAppendix encryptToSelfMessage = ParameterParser.getEncryptToSelfMessage(req, senderAccount.getId());
-        MessageAppendix message = null;
-        PrunablePlainMessageAppendix prunablePlainMessage = null;
-        if ("true".equalsIgnoreCase(req.getParameter("messageIsPrunable"))) {
-            prunablePlainMessage = (PrunablePlainMessageAppendix) ParameterParser.getPlainMessage(req, true);
-        } else {
-            message = (MessageAppendix) ParameterParser.getPlainMessage(req, false);
-        }
+
+        MessageAppendix message = txRequest.isMessageIsPrunable() ? null : (MessageAppendix) txRequest.getMessage();
+        PrunablePlainMessageAppendix prunablePlainMessage = txRequest.isMessageIsPrunable() ? (PrunablePlainMessageAppendix) txRequest.getMessage() : null;
+
         PublicKeyAnnouncementAppendix publicKeyAnnouncement = null;
-        String recipientPublicKey = Convert.emptyToNull(req.getParameter("recipientPublicKey"));
-        if (recipientPublicKey != null) {
-            publicKeyAnnouncement = new PublicKeyAnnouncementAppendix(Convert.parseHexString(recipientPublicKey));
+        if (txRequest.getRecipientPublicKey() != null) {
+            publicKeyAnnouncement = new PublicKeyAnnouncementAppendix(Convert.parseHexString(txRequest.getRecipientPublicKey()));
         }
 
-        PhasingAppendix phasing = null;
-        boolean phased = "true".equalsIgnoreCase(req.getParameter("phased"));
-        if (phased) {
-            phasing = parsePhasing(req);
-        }
-
-        if (secretPhrase == null && publicKeyValue == null && passphrase == null) {
-            return MISSING_SECRET_PHRASE;
-        } else if (deadlineValue == null) {
-            return MISSING_DEADLINE;
+        if (txRequest.getKeySeed() == null && txRequest.getPublicKeyValue() == null) {
+            throw new AplException.NotValidException(MISSING_SECRET_PHRASE);
+        } else if (txRequest.getDeadlineValue() == null) {
+            throw new AplException.NotValidException(MISSING_DEADLINE);
         }
 
         short deadline;
         try {
-            deadline = Short.parseShort(deadlineValue);
+            deadline = Short.parseShort(txRequest.getDeadlineValue());
             if (deadline < 1) {
-                return INCORRECT_DEADLINE;
+                throw new AplException.NotValidException(INCORRECT_DEADLINE);
             }
         } catch (NumberFormatException e) {
-            return INCORRECT_DEADLINE;
+            throw new AplException.NotValidException(INCORRECT_DEADLINE);
         }
 
-        long feeATM = ParameterParser.getFeeATM(req);
-        int ecBlockHeight = ParameterParser.getInt(req, "ecBlockHeight", 0, Integer.MAX_VALUE, false);
-        long ecBlockId = Convert.parseUnsignedLong(req.getParameter("ecBlockId"));
         Blockchain blockchain = lookupBlockchain();
-        if (ecBlockId != 0 && ecBlockId != blockchain.getBlockIdAtHeight(ecBlockHeight)) {
-            return INCORRECT_EC_BLOCK;
+        if (txRequest.getEcBlockId() != 0 && txRequest.getEcBlockId() != blockchain.getBlockIdAtHeight(txRequest.getEcBlockHeight())) {
+            throw new AplException.NotValidException(INCORRECT_EC_BLOCK);
         }
-        if (ecBlockId == 0 && ecBlockHeight > 0) {
-            ecBlockId = blockchain.getBlockIdAtHeight(ecBlockHeight);
+        if (txRequest.getEcBlockId() == 0 && txRequest.getEcBlockHeight() > 0) {
+            txRequest.setEcBlockId(blockchain.getBlockIdAtHeight(txRequest.getEcBlockHeight()));
         }
-
-        JSONObject response = new JSONObject();
 
         // shouldn't try to get publicKey from senderAccount as it may have not been set yet
-        byte[] publicKey = ParameterParser.getPublicKey(req, senderAccount.getId());
         int timestamp = timeService.getEpochTime();
+        Transaction transaction;
         try {
-            Transaction.Builder builder = Transaction.newTransactionBuilder(publicKey, amountATM, feeATM,
-                    deadline, attachment, timestamp).referencedTransactionFullHash(referencedTransactionFullHash);
-            if (attachment.getTransactionType().canHaveRecipient()) {
-                builder.recipientId(recipientId);
+            Transaction.Builder builder = Transaction.newTransactionBuilder(txRequest.getPublicKey(), txRequest.getAmountATM(), txRequest.getFeeATM(),
+                    deadline, txRequest.getAttachment(), timestamp).referencedTransactionFullHash(txRequest.getReferencedTransactionFullHash());
+            if (txRequest.getAttachment().getTransactionType().canHaveRecipient()) {
+                builder.recipientId(txRequest.getRecipientId());
             }
             builder.appendix(encryptedMessage);
             builder.appendix(message);
             builder.appendix(publicKeyAnnouncement);
-            builder.appendix(encryptToSelfMessage);
-            builder.appendix(phasing);
+            builder.appendix(txRequest.getEncryptToSelfMessage());
+            builder.appendix(txRequest.getPhasing());
             builder.appendix(prunablePlainMessage);
             builder.appendix(prunableEncryptedMessage);
-            if (ecBlockId != 0) {
-                builder.ecBlockId(ecBlockId);
-                builder.ecBlockHeight(ecBlockHeight);
+            if (txRequest.getEcBlockId() != 0) {
+                builder.ecBlockId(txRequest.getEcBlockId());
+                builder.ecBlockHeight(txRequest.getEcBlockHeight());
             }
-            byte[] keySeed = ParameterParser.getKeySeed(req, senderAccount.getId(), false);
-            Transaction transaction = builder.build(keySeed);
-            if (feeATM <= 0 || (propertiesHolder.correctInvalidFees() && keySeed == null)) {
+            transaction = builder.build(txRequest.getKeySeed());
+            if (txRequest.getFeeATM() <= 0 || (propertiesHolder.correctInvalidFees() && txRequest.getKeySeed() == null)) {
                 int effectiveHeight = blockchain.getHeight();
                 long minFee = feeCalculator.getMinimumFeeATM(transaction, effectiveHeight);
-                feeATM = Math.max(minFee, feeATM);
-                transaction.setFeeATM(feeATM);
-            }
-            try {
-                if (Math.addExact(amountATM, transaction.getFeeATM()) > senderAccount.getUnconfirmedBalanceATM()) {
-                    return NOT_ENOUGH_FUNDS;
-                }
-            } catch (ArithmeticException e) {
-                return NOT_ENOUGH_FUNDS;
+                txRequest.setFeeATM(Math.max(minFee, txRequest.getFeeATM()));
+                transaction.setFeeATM(txRequest.getFeeATM());
             }
 
-            JSONObject transactionJSON = JSONData.unconfirmedTransaction(transaction);
-            response.put("transactionJSON", transactionJSON);
             try {
-                response.put("unsignedTransactionBytes", Convert.toHexString(transaction.getUnsignedBytes()));
-            } catch (AplException.NotYetEncryptedException ignore) {}
-            if (keySeed != null) {
-                response.put("transaction", transaction.getStringId());
-                response.put("fullHash", transactionJSON.get("fullHash"));
-                response.put("transactionBytes", Convert.toHexString(transaction.getBytes()));
-                response.put("signatureHash", transactionJSON.get("signatureHash"));
+                if (Math.addExact(txRequest.getAmountATM(), transaction.getFeeATM()) > txRequest.getSenderAccount().getUnconfirmedBalanceATM()) {
+                    throw new AplException.NotValidException(NOT_ENOUGH_FUNDS);
+                }
+            } catch (ArithmeticException e) {
+                throw new AplException.NotValidException(NOT_ENOUGH_FUNDS);
             }
-            if (broadcast) {
+
+            if (txRequest.isBroadcast()) {
                 lookupTransactionProcessor().broadcast(transaction);
-                response.put("broadcasted", true);
             } else {
                 validator.validate(transaction);
-                response.put("broadcasted", false);
             }
         } catch (AplException.NotYetEnabledException e) {
-            return FEATURE_NOT_AVAILABLE;
+            throw new AplException.NotValidException(FEATURE_NOT_AVAILABLE);
         } catch (AplException.InsufficientBalanceException e) {
             throw e;
         } catch (AplException.ValidationException e) {
-            if (broadcast) {
-                response.clear();
-            }
-            response.put("broadcasted", false);
-            JSONData.putException(response, e);
+            throw e;
         }
-        return response;
 
+        return transaction;
     }
+
     @Override
     protected final boolean requirePost() {
         return true;
