5aaeb8df36307c2057ba0432500db0453ffbbe8f
Serhiy Lymar <limar.sergey@gmail.com>
2019-07-05 22:39:40 +0300

    Database initialization - trade-related table creation

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/db/AplDbVersion.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/db/AplDbVersion.java
@@ -784,8 +784,15 @@ public class AplDbVersion extends DbVersion {
             case 297:
                 apply("CREATE TABLE IF NOT EXISTS dex_contract (db_id IDENTITY NOT NULL, offer_id BIGINT NOT NULL, " +
                         "counter_offer_id BIGINT NOT NULL, secret_hash CHAR(64) NOT NULL, height INT NOT NULL, latest BOOLEAN NOT NULL DEFAULT TRUE)");
-            case 298:
-                return 298;
+            case 299: 
+                apply(  "CREATE TABLE IF NOT EXISTS dex_trade (db_id IDENTITY NOT NULL, transaction_id BIGINT not null, sender_offer_id BIGINT not null, " +
+                        "RECEIVER_OFFER_ID BIGINT not null, SENDER_OFFER_TYPE TINYINT not null, SENDER_OFFER_CURRENCY TINYINT not null, " +
+                        "SENDER_OFFER_AMOUNT BIGINT not null, PAIR_CURRENCY TINYINT not null, PAIR_RATE DECIMAL not null, FINISH_TIME INT not null, " +
+                        "HEIGHT INT not null )" );                   
+            case 300:
+                return 300;
+                
+                
             default:
                 throw new RuntimeException("Blockchain database inconsistent with code, at update " + nextUpdate
                         + ", probably trying to run older code on newer database");
