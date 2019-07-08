1b93c3ce1c9abd79851332b0c76710538d5ad84a
Serhiy Lymar <limar.sergey@gmail.com>
2019-07-08 22:12:49 +0300

    DTO correction + prepare to imbibe into controller

--- apl-api/src/main/java/com/apollocurrency/aplwallet/api/dto/DexTradeInfoDto.java
+++ apl-api/src/main/java/com/apollocurrency/aplwallet/api/dto/DexTradeInfoDto.java
@@ -21,23 +21,23 @@ public class DexTradeInfoDto {
     @Schema(name="database id", description="Identificator of the entry insidt the database")
     public long dbId;
     @Schema(name="transaction ID", description="Identificator of exchange transaction")
-    private long transactionID;
+    public long transactionID;
     @Schema(name="sender's offer ID", description="Identificator of exchange offer from the side of sender")
-    private long senderOfferID; 
+    public long senderOfferID; 
     @Schema(name="receiver's offer ID", description="Identificator of exchange offer from the side of receiver")
-    private long receiverOfferID;   
+    public long receiverOfferID;   
     @Schema(name="Exchange type", description="buy/sell = 0/1")
-    private byte senderOfferType; 
+    public byte senderOfferType; 
     @Schema(name="Type of currency", description="eth/pax/apl = 0/1/2")
-    private byte senderOfferCurrency; 
+    public byte senderOfferCurrency; 
     @Schema(name="Amount of currency for exchagne", description="parameter in gwei units")
-    private long senderOfferAmount;
+    public long senderOfferAmount;
     @Schema(name="Type of pair currency", description="eth/pax/apl = 0/1/2")
-    private byte pairCurrency;
+    public byte pairCurrency;
     @Schema(name="Exchange rate", description="parameter in gwei units")    
-    private BigDecimal pairRate;
+    public BigDecimal pairRate;
     @Schema(name="Finish time", description="Order finish time")
-    private Integer finishTime;
+    public Integer finishTime;
     @Schema(name="Order height", description="Blockchain-related height value")
-    private Integer height; 
+    public Integer height; 
 }
--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/exchange/model/DexTradeEntry.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/exchange/model/DexTradeEntry.java
@@ -4,6 +4,7 @@
 
 package com.apollocurrency.aplwallet.apl.exchange.model;
 
+import com.apollocurrency.aplwallet.api.dto.DexTradeInfoDto;
 import java.math.BigDecimal;
 
 /**
@@ -110,6 +111,22 @@ public class DexTradeEntry {
 
     public void setHeight(Integer height) {
         this.height = height;
+    }
+        
+    public DexTradeInfoDto toDto(){       
+        DexTradeInfoDto dexTradeInfoDto = new DexTradeInfoDto();        
+        dexTradeInfoDto.dbId = this.dbId;
+        dexTradeInfoDto.transactionID = this.transactionID;
+        dexTradeInfoDto.senderOfferID = this.senderOfferID; 
+        dexTradeInfoDto.receiverOfferID = this.receiverOfferID;   
+        dexTradeInfoDto.senderOfferType = this.senderOfferType; 
+        dexTradeInfoDto.senderOfferCurrency = this.senderOfferCurrency; 
+        dexTradeInfoDto.senderOfferAmount = this.senderOfferAmount;
+        dexTradeInfoDto.pairCurrency = this.pairCurrency;
+        dexTradeInfoDto.pairRate = this.pairRate;
+        dexTradeInfoDto.finishTime = this.finishTime;
+        dexTradeInfoDto.height = this.height;         
+        return dexTradeInfoDto;
     }
    
 }
