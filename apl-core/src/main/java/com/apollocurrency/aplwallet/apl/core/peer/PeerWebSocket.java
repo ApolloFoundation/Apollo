953d0c788c579c6989b74323fae13318bf5b3fa4
al <al@al2w.alhome>
2019-07-08 06:40:49 +0300

    creating new PeerWebSocket if closed

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/peer/PeerWebSocket.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/peer/PeerWebSocket.java
@@ -414,8 +414,7 @@ public class PeerWebSocket {
             requests.forEach((entry) -> entry.getValue().complete(exc));
             requestMap.clear();
             if(clientPeer!=null){
-                LOG.debug("Client socket is closed for peer: {} Reason: . Deactivating it",clientPeer.getHostWithPort(),reason);
-                clientPeer.deactivate();
+                LOG.debug("Client socket is closed for peer: {} Reason: ",clientPeer.getHostWithPort(),reason);
             }
         } finally {
             lock.unlock();
