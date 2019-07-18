2221e843a97f73dfae3f2a3569d801c1a4f1dea2
Serhiy Lymar <serhiy.lymar@gmail.com>
2019-07-18 15:37:01 +0300

    API update: first steps

---
+++ apl-api/src/main/java/com/apollocurrency/aplwallet/api/dto/DexTradeInfoMinDto.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2018-2019 Apollo Foundation
+ */ 
+package com.apollocurrency.aplwallet.api.dto;
+
+/**
+ *
+ * @author Serhiy Lymar
+ */
+public class DexTradeInfoMinDto {
+    
+}
+
