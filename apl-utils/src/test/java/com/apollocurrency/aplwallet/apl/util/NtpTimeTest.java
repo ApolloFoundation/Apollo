b47785429069afe02c5cc0c94fed19e8a15ec221
Yuriy Larin <yuriy.larin@firstbridge.io>
2019-08-13 16:24:43 +0300

    added unit tests, minor updates

---
+++ apl-utils/src/test/java/com/apollocurrency/aplwallet/apl/util/NtpTimeTest.java
@@ -0,0 +1,42 @@
+package com.apollocurrency.aplwallet.apl.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.inject.Inject;
+
+import java.text.SimpleDateFormat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jboss.weld.junit5.EnableWeld;
+import org.jboss.weld.junit5.WeldInitiator;
+import org.jboss.weld.junit5.WeldSetup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+@EnableWeld
+class NtpTimeTest {
+
+    @WeldSetup
+    public WeldInitiator weld = WeldInitiator.from(NtpTime.class)
+            .build();
+
+    private NtpTime ntpTime;
+    private long currentTime;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+    @BeforeEach
+    void setUp() {
+        currentTime = System.currentTimeMillis();
+    }
+
+    @Test
+    void getTimeTest() {
+        ntpTime = new NtpTime();
+        ntpTime.start(); // emulate @PostConstrust
+        long freshTime = ntpTime.getTime();
+        assertTrue(freshTime > currentTime);
+        log.info("now : long = {} / formatted = {}", freshTime, dateFormat.format(freshTime));
+    }
+
+}
