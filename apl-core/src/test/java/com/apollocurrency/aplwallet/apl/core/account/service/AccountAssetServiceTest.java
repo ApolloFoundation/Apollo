ca7f391f749723e244b64246114c3a909ad243ba
Andrii Zinchenko <andrew.zinchenko@gmail.com>
2019-09-02 11:04:36 +0300

    APL-724 added addToAssetBalanceATU test;

--- apl-core/src/test/java/com/apollocurrency/aplwallet/apl/core/account/service/AccountAssetServiceTest.java
+++ apl-core/src/test/java/com/apollocurrency/aplwallet/apl/core/account/service/AccountAssetServiceTest.java
@@ -4,7 +4,9 @@
 
 package com.apollocurrency.aplwallet.apl.core.account.service;
 
+import com.apollocurrency.aplwallet.apl.core.account.LedgerEvent;
 import com.apollocurrency.aplwallet.apl.core.account.dao.AccountAssetTable;
+import com.apollocurrency.aplwallet.apl.core.account.model.Account;
 import com.apollocurrency.aplwallet.apl.core.account.model.AccountAsset;
 import com.apollocurrency.aplwallet.apl.core.app.Blockchain;
 import com.apollocurrency.aplwallet.apl.core.app.BlockchainImpl;
@@ -13,30 +15,25 @@ import com.apollocurrency.aplwallet.apl.core.app.BlockchainProcessorImpl;
 import com.apollocurrency.aplwallet.apl.core.chainid.BlockchainConfig;
 import com.apollocurrency.aplwallet.apl.core.db.DatabaseManager;
 import com.apollocurrency.aplwallet.apl.core.db.DatabaseManagerImpl;
-import com.apollocurrency.aplwallet.apl.core.db.DerivedDbTablesRegistryImpl;
-import com.apollocurrency.aplwallet.apl.core.db.DerivedTablesRegistry;
-import com.apollocurrency.aplwallet.apl.core.db.fulltext.FullTextConfig;
-import com.apollocurrency.aplwallet.apl.core.db.fulltext.FullTextConfigImpl;
 import com.apollocurrency.aplwallet.apl.data.AccountTestData;
-import com.apollocurrency.aplwallet.apl.testutil.DbUtils;
-import com.apollocurrency.aplwallet.apl.util.Listener;
 import com.apollocurrency.aplwallet.apl.util.injectable.PropertiesHolder;
 import org.jboss.weld.junit.MockBean;
 import org.jboss.weld.junit5.EnableWeld;
 import org.jboss.weld.junit5.WeldInitiator;
 import org.jboss.weld.junit5.WeldSetup;
-import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -46,8 +43,10 @@ class AccountAssetServiceTest {
     private Blockchain blockchain = mock(BlockchainImpl.class);
     private BlockchainConfig blockchainConfig = mock(BlockchainConfig.class);
     private BlockchainProcessor blockchainProcessor = mock(BlockchainProcessor.class);
-    private DatabaseManager databaseManager = mock(DatabaseManagerImpl.class);
+    //private DatabaseManager databaseManager = mock(DatabaseManagerImpl.class);
     private AccountAssetTable accountAssetTable = mock(AccountAssetTable.class);
+    private Event<Account> accountEvent = mock(Event.class);
+    private Event<AccountAsset> accountAssetEvent = mock(Event.class);
 
 
     @WeldSetup
@@ -59,7 +58,7 @@ class AccountAssetServiceTest {
             .addBeans(MockBean.of(blockchain, Blockchain.class, BlockchainImpl.class))
             .addBeans(MockBean.of(blockchainProcessor, BlockchainProcessor.class, BlockchainProcessorImpl.class))
             .addBeans(MockBean.of(accountAssetTable, AccountAssetTable.class))
-            .addBeans(MockBean.of(databaseManager, DatabaseManager.class, DatabaseManagerImpl.class))
+            //.addBeans(MockBean.of(databaseManager, DatabaseManager.class, DatabaseManagerImpl.class))
             .addBeans(MockBean.of(mock(AccountService.class), AccountService.class, AccountServiceImpl.class))
             .addBeans(MockBean.of(mock(AccountLedgerService.class), AccountLedgerService.class, AccountLedgerServiceImpl.class))
             .build();
@@ -79,71 +78,27 @@ class AccountAssetServiceTest {
     }
 
     @Test
-    void getAssetAccounts() {
-    }
-
-    @Test
-    void testGetAssetAccounts() {
-    }
-
-    @Test
-    void testGetAssetAccounts1() {
-    }
-
-    @Test
-    void getAssetCount() {
-    }
-
-    @Test
-    void testGetAssetCount() {
-    }
-
-    @Test
-    void getAssets() {
-    }
-
-    @Test
-    void testGetAssets() {
-    }
-
-    @Test
-    void getAccountAssetCount() {
-    }
-
-    @Test
-    void testGetAccountAssetCount() {
-    }
-
-    @Test
-    void getAsset() {
-    }
-
-    @Test
-    void testGetAsset() {
-    }
-
-    @Test
-    void testGetAsset1() {
-    }
-
-    @Test
-    void getAssetBalanceATU() {
-    }
-
-    @Test
-    void testGetAssetBalanceATU() {
-    }
-
-    @Test
-    void testGetAssetBalanceATU1() {
-    }
-
-    @Test
-    void getUnconfirmedAssetBalanceATU() {
-    }
-
-    @Test
     void addToAssetBalanceATU() {
+        long quantity = 50000L;
+        long assetId = 50L;
+        LedgerEvent event = LedgerEvent.ASSET_DIVIDEND_PAYMENT;
+        long eventId = 10L;
+        long balance = Math.addExact(testData.ACC_ASS_0.getQuantityATU(), quantity);
+
+        AccountAssetService spyService = spy(accountAssetService);
+
+        doReturn(testData.ACC_ASS_0).when(accountAssetTable).get(any());
+        spyService.addToAssetBalanceATU(testData.ACC_1, event, eventId, assetId, quantity);
+
+        assertEquals(balance, testData.ACC_ASS_0.getQuantityATU());
+        verify(spyService).update(testData.ACC_ASS_0);
+        verify(accountEvent).fire(testData.ACC_1);
+        verify(accountAssetEvent).fire(testData.ACC_ASS_0);
+    }
+
+    @Test
+    void addToAssetBalanceATU_newAsset() {
+        //ACC0
     }
 
     @Test
