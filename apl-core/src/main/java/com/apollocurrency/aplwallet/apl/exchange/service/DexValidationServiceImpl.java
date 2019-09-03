121249d2b267f6e8bfe11e3fe41e2218d66e18fc
Serhiy Lymar <limar.sergey@gmail.com>
2019-09-03 15:05:48 +0300

    Core routines

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/exchange/service/DexValidationServiceImpl.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/exchange/service/DexValidationServiceImpl.java
@@ -8,12 +8,21 @@ import com.apollocurrency.aplwallet.apl.core.account.Account;
 import com.apollocurrency.aplwallet.apl.core.account.AccountTable;
 import com.apollocurrency.aplwallet.apl.core.app.TimeService;
 import com.apollocurrency.aplwallet.apl.core.db.DbKey;
+import com.apollocurrency.aplwallet.apl.eth.service.EthereumWalletService;
+import com.apollocurrency.aplwallet.apl.eth.utils.EthUtil;
 import com.apollocurrency.aplwallet.apl.exchange.model.DexContractDBRequest;
+import com.apollocurrency.aplwallet.apl.exchange.model.DexCurrencies;
 import com.apollocurrency.aplwallet.apl.exchange.model.DexOffer;
 import com.apollocurrency.aplwallet.apl.exchange.model.ExchangeContract;
+import com.apollocurrency.aplwallet.apl.exchange.model.UserEthDepositInfo;
+import com.apollocurrency.aplwallet.apl.util.AplException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Objects;
+import java.util.logging.Level;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.slf4j.Logger;
@@ -29,10 +38,15 @@ public class DexValidationServiceImpl implements IDexBasicServiceInterface, IDex
     
     private static final Logger log = LoggerFactory.getLogger(DexMatchingService.class);
     private DexService dexService;
+    private DexSmartContractService dexSmartContractService;
+    private EthereumWalletService ethereumWalletService;
     
     @Inject
-    DexValidationServiceImpl( DexService dexService ) {
-        this.dexService =  Objects.requireNonNull( dexService,"dexService is null");        
+    DexValidationServiceImpl( DexService dexService, DexSmartContractService dexSmartContractService, EthereumWalletService ethereumWalletService ) {
+        this.dexService =  Objects.requireNonNull( dexService,"dexService is null");   
+        this.dexSmartContractService =  Objects.requireNonNull( dexSmartContractService,"dexSmartContractService is null");           
+        this.ethereumWalletService =  Objects.requireNonNull( ethereumWalletService,"dexSmartContractService is null");           
+
     }
 
     @Override
@@ -59,11 +73,32 @@ public class DexValidationServiceImpl implements IDexBasicServiceInterface, IDex
         return hisUnconfirmedAplBalance;
     }
     
-    // Long getEthDeposit()
-
+    private List<UserEthDepositInfo> getUserEthDeposits(String user) {
+        try {
+            return dexSmartContractService.getUserFilledDeposits(user);
+        } catch (AplException.ExecutiveProcessException ex) {
+            java.util.logging.Logger.getLogger(DexValidationServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    BigInteger getUserEthDeposit(String user, DexCurrencies currencyType) {
+        return  ethereumWalletService.getBalanceWei(user, currencyType);
+    }
+    
+    BigInteger getEthBalanceWei(String user, DexCurrencies currencyType) {
+        return ethereumWalletService.getBalanceWei(user, currencyType );
+    }
+    
+    boolean validateEthDeposit(DexOffer myOffer, DexOffer counterOffer) {
+        // right now it is checking the balance. TODO: switch to checking out deposits
+        BigInteger amountOnHisWallet = getEthBalanceWei(counterOffer.getFromAddress(), counterOffer.getPairCurrency());
+        BigDecimal haveToPay = EthUtil.atmToEth(counterOffer.getOfferAmount()).multiply(counterOffer.getPairRate());
+        return amountOnHisWallet.compareTo(EthUtil.etherToWei(haveToPay)) < 0;        
+    }
+       
     @Override
     public boolean validateAplSell(DexOffer myOffer, DexOffer counterOffer) {
-        
        
         
         // placeholder
@@ -72,8 +107,6 @@ public class DexValidationServiceImpl implements IDexBasicServiceInterface, IDex
 
     @Override
     public boolean validateAplBuy(DexOffer myOffer, DexOffer counterOffer) {
-
-        // ExchangeContract  exchangeContract = dexService.getDexContract(DexContractDBRequest.builder().counterOfferId(counterOffer.getTransactionId()).build());        
         // placeholder
         return true;
     }
