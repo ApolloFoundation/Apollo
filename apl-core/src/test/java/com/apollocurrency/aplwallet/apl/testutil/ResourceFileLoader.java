e2dbda6069b8cba9c878be2d58a98f72f6ed948f
Andrew Boyarsky <andrew.boyarski@gmail.com>
2019-09-13 16:03:43 +0300

    Fix space encoding in resource names for apl-core

--- apl-core/src/test/java/com/apollocurrency/aplwallet/apl/testutil/ResourceFileLoader.java
+++ apl-core/src/test/java/com/apollocurrency/aplwallet/apl/testutil/ResourceFileLoader.java
@@ -3,7 +3,11 @@ package com.apollocurrency.aplwallet.apl.testutil;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 
@@ -23,10 +27,13 @@ public class ResourceFileLoader {
     public Path getResourcePath(){
         ClassLoader classLoader = getClass().getClassLoader();
         // "logback-test.xml" should be present in resource folder, otherwise it fails
-        File file = new File(classLoader.getResource("logback-test.xml").getFile()); // usually we have it there
-//        File file = new File("/media/ylarin/PHOTO/java_projects/Apollo/tmp/public_key.csv"); // usually we have it there
-        log.trace(file.getAbsolutePath());
-        return new File(file.getAbsolutePath().substring(0, file.getAbsolutePath().lastIndexOf(File.separator))).toPath();
+        try {
+            URI uri = Objects.requireNonNull(classLoader.getResource("logback-test.xml")).toURI();// usually we have it there
+            return Paths.get(uri).getParent().toAbsolutePath();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 
 }
