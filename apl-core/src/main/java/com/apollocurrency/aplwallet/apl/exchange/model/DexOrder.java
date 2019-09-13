7cb8d660b1faa1c3fc3103ddf2c2f3ae6de5a791
Serhiy Lymar <limar.sergey@gmail.com>
2019-09-13 11:19:36 +0300

    Cleaning up (minor update)

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/exchange/model/DexOrder.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/exchange/model/DexOrder.java
@@ -14,7 +14,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
-import lombok.Getter;
 
 
 @Data
--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/exchange/model/UserEthDepositInfo.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/exchange/model/UserEthDepositInfo.java
@@ -5,8 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @AllArgsConstructor
