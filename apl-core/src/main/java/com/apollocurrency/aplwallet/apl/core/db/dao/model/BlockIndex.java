8e4c5b3880f3129c7eb2f4f844fac0bc5e0bd889
Andrii Zinchenko <andrew.zinchenko@gmail.com>
2019-09-25 11:28:52 +0300

    APL-834 removed custom builder and added Lombok annotation;

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/db/dao/model/BlockIndex.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/db/dao/model/BlockIndex.java
@@ -1,6 +1,7 @@
 package com.apollocurrency.aplwallet.apl.core.db.dao.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,37 +14,12 @@ import lombok.Setter;
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class BlockIndex {
     private long blockId;
     private int blockHeight;
 
     public BlockIndex copy() {
         return new BlockIndex(blockId, blockHeight);
-    }
-
-    public static ShardBuilder builder() {
-        return new ShardBuilder();
-    }
-
-    public static final class ShardBuilder {
-        private long blockId;
-        private int blockHeight;
-
-        private ShardBuilder() {
-        }
-
-        public ShardBuilder blockId(long blockId) {
-            this.blockId = blockId;
-            return this;
-        }
-
-        public ShardBuilder blockHeight(int blockHeight) {
-            this.blockHeight = blockHeight;
-            return this;
-        }
-
-        public BlockIndex build() {
-            return new BlockIndex(blockId, blockHeight);
-        }
     }
 }
