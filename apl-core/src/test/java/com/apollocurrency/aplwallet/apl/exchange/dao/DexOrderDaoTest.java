f4cd4acb1d06aebece6e4f8909312e3efeae36dd
Yuriy Larin <yuriy.larin@firstbridge.io>
2019-09-26 13:00:38 +0300

    added copyright

--- apl-core/src/test/java/com/apollocurrency/aplwallet/apl/exchange/dao/DexOrderDaoTest.java
+++ apl-core/src/test/java/com/apollocurrency/aplwallet/apl/exchange/dao/DexOrderDaoTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright © 2018-2019 Apollo Foundation
+ */
+
 package com.apollocurrency.aplwallet.apl.exchange.dao;
 
 import com.apollocurrency.aplwallet.apl.core.db.cdi.transaction.JdbiHandleFactory;
--- apl-core/src/test/java/com/apollocurrency/aplwallet/apl/exchange/dao/EthGasStationInfoDaoTest.java
+++ apl-core/src/test/java/com/apollocurrency/aplwallet/apl/exchange/dao/EthGasStationInfoDaoTest.java
@@ -1,8 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright © 2018-2019 Apollo Foundation
  */
+
 package com.apollocurrency.aplwallet.apl.exchange.dao;
 
 import com.apollocurrency.aplwallet.apl.exchange.model.EthGasInfo;
