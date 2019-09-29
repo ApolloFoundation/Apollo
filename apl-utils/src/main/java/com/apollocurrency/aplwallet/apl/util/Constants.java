7e2c0286976691d8c0fa3215a494f58684b36880
leonidstr <leonid.stryzhevskyi@firstbridge.io>
2019-09-29 15:59:44 +0300

    Version dump for the exchange dev 1.38.1

--- apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/Constants.java
+++ apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/Constants.java
@@ -24,7 +24,7 @@ import java.math.BigInteger;
 
 public final class Constants {
 
-    public static final Version VERSION = new Version("1.37.6");
+    public static final Version VERSION = new Version("1.38.1");
 
 
     public static final String APPLICATION = "Apollo";
