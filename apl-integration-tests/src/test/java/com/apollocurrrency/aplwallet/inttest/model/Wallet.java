afe0823b3762a98d58e5af2e637161812924c246
MKhabenko <m.habenko@ukr.net>
2019-10-22 12:47:25 +0300

    Added lombook annotations

--- apl-integration-tests/src/test/java/com/apollocurrrency/aplwallet/inttest/model/Wallet.java
+++ apl-integration-tests/src/test/java/com/apollocurrrency/aplwallet/inttest/model/Wallet.java
@@ -1,47 +1,22 @@
 package com.apollocurrrency.aplwallet.inttest.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Wallet {
     private String user;
     private String pass;
     private String publicKey;
     private boolean vault;
 
-
-    public Wallet() {
-    }
-
-    public Wallet(String user, String pass, String publicKey, boolean vault) {
-        this.user = user;
-        this.pass = pass;
-        this.publicKey = publicKey;
-        this.vault = vault;
-    }
     public Wallet(String user, String pass) {
         this.user = user;
-        this.pass = pass;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public String getPass() {
-        return pass;
-    }
-
-    public String getPublicKey() {
-        return publicKey;
-    }
-
-    public boolean isVault() {
-        return vault;
-    }
-
-    public void setVault(boolean vault) {
-        this.vault = vault;
-    }
-
-    public void setPass(String pass) {
         this.pass = pass;
     }
 
