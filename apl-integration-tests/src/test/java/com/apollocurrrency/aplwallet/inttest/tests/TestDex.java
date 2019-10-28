6022cbc8e87b38c105d53e15cf008b6e330b3deb
MKhabenko <m.habenko@ukr.net>
2019-10-28 17:30:34 +0200

    Update TestDex.java

--- apl-integration-tests/src/test/java/com/apollocurrrency/aplwallet/inttest/tests/TestDex.java
+++ apl-integration-tests/src/test/java/com/apollocurrrency/aplwallet/inttest/tests/TestDex.java
@@ -1,19 +1,11 @@
 package com.apollocurrrency.aplwallet.inttest.tests;
-import com.apollocurrency.aplwallet.api.dto.BlockDTO;
-import com.apollocurrency.aplwallet.api.dto.BlockchainInfoDTO;
-import com.apollocurrency.aplwallet.api.dto.BlockchainState;
+
 import com.apollocurrency.aplwallet.api.dto.DexOrderDto;
-import com.apollocurrency.aplwallet.api.dto.ECBlockDTO;
-import com.apollocurrency.aplwallet.api.response.AccountBlocksResponse;
-import com.apollocurrency.aplwallet.api.response.DexOrderResponse;
-import com.apollocurrency.aplwallet.api.response.GetBlockIdResponse;
 import com.apollocurrrency.aplwallet.inttest.model.TestBaseNew;
-import com.apollocurrrency.aplwallet.inttest.model.TestBaseOld;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
