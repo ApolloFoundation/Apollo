027cc76d31acee555667c33b202798939fd36bc3
Serhiy Lymar <limar.sergey@gmail.com>
2019-10-29 15:42:08 +0200

    dbrequest for getting trading information

---
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/exchange/model/DexOrderDBRequestForTrading.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2018-2019 Apollo Foundation
+ */
+package com.apollocurrency.aplwallet.apl.exchange.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Serhiy Lymar
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DexOrderDBRequestForTrading {
+    private Integer startInterval;
+    private Integer endInterval;
+    private Integer pairCur;
+    private Integer offset;
+    private Integer limit;
+}
