c280a290f4d7751244c51b163e485436a423a534
MKhabenko <m.habenko@ukr.net>
2019-10-29 15:20:58 +0200

    Added before hook

--- apl-performance-tests/src/test/scala/com/apollocurrency/RecordedSimulation.scala
+++ apl-performance-tests/src/test/scala/com/apollocurrency/RecordedSimulation.scala
@@ -1,39 +1,64 @@
 package com.apollocurrency
 
 import com.typesafe.config.ConfigFactory
-
+import collection.JavaConverters._
 import scala.concurrent.duration._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
 import scala.util.Random
+import scalaj.http._
 
 class RecordedSimulation extends Simulation {
 
-	var peers = ConfigFactory.load("application.conf").getStringList("peers")
-	println(peers.get(0).getClass) // prints my-app
+	var peers = ConfigFactory.load("application.conf").getStringList("peers").asScala.toList
 	val random = new Random
-	println(peers.get(random.nextInt(peers.size())))
-
 	var httpProtocol = http.baseUrls(peers)
 		.inferHtmlResources()
 		.acceptHeader("*/*")
-	//"http://localhost:7876"
 
-	foreach("peers", "peer") {
-		println("++++++++++++++++++++++++++++++++++++++++++++++++")
-		println("http://${peer}:7876")
-		exec(http("get google inside during")
-			.get("http://${peer}:7876")
-			.check(status.is(200)))
+	before {
+		println("Stop/Start forging!")
+		for (peer <- peers) {
+			try {
+			println(peer)
+			val response = Http(peer+"/apl")
+				.postForm
+				.param("requestType","stopForging")
+  			.param("adminPassword","1").asString
+		   	 println(response.body)
+			} catch { case e: Exception =>
+				println(e.getMessage)
+			}
+		 }
+
+		for( i <- 1 to 10) {
+				try {
+					val peer = peers(
+						random.nextInt(peers.length)
+					)
+					println(peer)
+					val response = Http(peer+"/apl")
+						.postForm
+						.param("requestType","startForging")
+						.param("secretPhrase",i.toString).asString
+					println(response.body)
+					} catch { case e: Exception =>
+						println(e.getMessage)
+					}
+				}
 	}
 
 
 
 	val scn = scenario("RecordedSimulation")
 		.exec(http("StopForging")
-		.post("/apl?requestType=stopForging&adminPassword=1"))
+		.post("/apl?requestType=sendmoney&adminPassword=1"))
 		.pause(1)
 
+		.exec { session =>
+			println(session)
+			session
+		}
 	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
