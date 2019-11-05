4bab2f263fec2f5caa8fa1213e5c9cd51df4a1b4
Yuriy Larin <yuriy.larin@firstbridge.io>
2019-11-05 11:16:13 +0200

    UI is upgraded to 2.5.29, backend is upgraded to 1.39.0

--- apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/Constants.java
+++ apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/Constants.java
@@ -24,7 +24,7 @@ import java.math.BigInteger;
 
 public final class Constants {
 
-    public static final Version VERSION = new Version("1.38.8");
+    public static final Version VERSION = new Version("1.39.0");
 
 
     public static final String APPLICATION = "Apollo";
