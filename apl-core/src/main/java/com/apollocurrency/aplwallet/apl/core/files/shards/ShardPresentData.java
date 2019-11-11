3c7e53848912b66a41eb59c326c521eb25d3b90c
Yuriy Larin <yuriy.larin@firstbridge.io>
2019-11-11 12:06:15 +0200

    reverted back to @Data annotation

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/files/shards/ShardPresentData.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/files/shards/ShardPresentData.java
@@ -6,26 +6,16 @@ package com.apollocurrency.aplwallet.apl.core.files.shards;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Setter
-@Getter
+@Data
 public class ShardPresentData {
     public Long shardId;
     public String shardFileId; // contains shardId + chainId in special format
     public List<String> additionalFileIDs;
-
-    @Override
-    public String toString() {
-        final StringBuffer sb = new StringBuffer("ShardPresentData{");
-        sb.append("shardId=").append(shardId);
-        sb.append(", shardFileId='").append(shardFileId).append('\'');
-        sb.append(", additionalFileIDs=").append(additionalFileIDs);
-        sb.append('}');
-        return sb.toString();
-    }
 }
