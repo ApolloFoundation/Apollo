c05208c9fa5b11c851bb219d114378c42ee865df
Oleksiy Lukin <oleksiy.lukin@firstbridge.io>
2019-11-21 17:50:55 +0200

    Better for understanding code

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/shard/observer/ShardObserver.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/shard/observer/ShardObserver.java
@@ -46,36 +46,54 @@ public class ShardObserver {
 //do it async anyway because we have to exit from trim and unlock it       
         tryCreateShardAsync(trimData.getTrimHeight(), trimData.getBlockchainHeight());
     }
-
-    public CompletableFuture<MigrateState> tryCreateShardAsync(int lastTrimBlockHeight, int blockchainHeight) {
-        CompletableFuture<MigrateState> completableFuture = null;
-        HeightConfig currentConfig = blockchainConfig.getCurrentConfig();
+    private boolean isShardingEnabled(HeightConfig currentConfig){
         boolean isShardingOff = propertiesHolder.getBooleanProperty("apl.noshardcreate", false);
         boolean shardingEnabled = currentConfig.isShardingEnabled();
         log.debug("Is sharding enabled ? : '{}' && '{}'", shardingEnabled, !isShardingOff);
+        return shardingEnabled && !isShardingOff;
+    }
+    
+    private boolean isShardAlreadyCreated(){
+        //TODO: check
+        return false;
+    }
+    
+    private boolean isTimeForShard(int lastTrimBlockHeight, int blockchainHeight, HeightConfig currentConfig) {
         int shardingFrequency = 1;
-        if (shardingEnabled && !isShardingOff) {
-            if (!blockchainConfig.isJustUpdated()) {
-                // config didn't change from previous trim scheduling
-                shardingFrequency = currentConfig.getShardingFrequency();
-            } else {
-                // TODO: YL after separating 'shard' and 'trim' logic, we can remove 'isJustUpdated()' usage and checking
-                // config has changed from previous trim scheduling, try to get previous 'shard frequency' value
-                shardingFrequency = blockchainConfig.getPreviousConfig().isPresent()
-                        && blockchainConfig.getPreviousConfig().get().isShardingEnabled() ?
-                        blockchainConfig.getPreviousConfig().get().getShardingFrequency() // previous config
-                        : currentConfig.getShardingFrequency(); // fall back
-            }
-            log.debug("Check shard conditions: ? [{}],  lastTrimBlockHeight = {}, blockchainHeight = {}"
-                            + ", shardingFrequency = {} ({})",
-                    shardingFrequency != 0 ?
-                            lastTrimBlockHeight % shardingFrequency == 0 : "zeroDivision",
-                    lastTrimBlockHeight, blockchainHeight,
-                    shardingFrequency, blockchainConfig.isJustUpdated());
-            
-            //Q. how much blocks we ould be late? 1/4 of frequiency is OK?
-            ModWatcher watch = new ModWatcher(shardingFrequency, shardingFrequency/4);
-            if (lastTrimBlockHeight != 0 && !watch.isTooLate(lastTrimBlockHeight)) {
+        if (!blockchainConfig.isJustUpdated()) {
+            // config didn't change from previous trim scheduling
+            shardingFrequency = currentConfig.getShardingFrequency();
+        } else {
+            // TODO: YL after separating 'shard' and 'trim' logic, we can remove 'isJustUpdated()' usage and checking
+            // config has changed from previous trim scheduling, try to get previous 'shard frequency' value
+            shardingFrequency = blockchainConfig.getPreviousConfig().isPresent()
+                    && blockchainConfig.getPreviousConfig().get().isShardingEnabled()
+                    ? blockchainConfig.getPreviousConfig().get().getShardingFrequency() // previous config
+                    : currentConfig.getShardingFrequency(); // fall back
+        }
+        log.debug("Check shard conditions: ? [{}],  lastTrimBlockHeight = {}, blockchainHeight = {}"
+                + ", shardingFrequency = {} ({})",
+                shardingFrequency != 0
+                        ? lastTrimBlockHeight % shardingFrequency == 0 : "zeroDivision",
+                lastTrimBlockHeight, blockchainHeight,
+                shardingFrequency, blockchainConfig.isJustUpdated());
+        
+        //Q. how much blocks we ould be late? 1/4 of frequiency is OK?
+        //Q. Do we count on some other parameters?
+        long howLateWeCanBe = shardingFrequency / 4;
+        ModWatcher watch = new ModWatcher(shardingFrequency, howLateWeCanBe);
+        boolean res = lastTrimBlockHeight != 0 && !watch.isTooLate(lastTrimBlockHeight);
+        res = res && !isShardAlreadyCreated();
+        return res;
+    }
+    
+    public CompletableFuture<MigrateState> tryCreateShardAsync(int lastTrimBlockHeight, int blockchainHeight) {
+        CompletableFuture<MigrateState> completableFuture = null;
+        HeightConfig currentConfig = blockchainConfig.getCurrentConfig();
+        
+        if (isShardingEnabled(currentConfig)) {
+
+            if (isTimeForShard(lastTrimBlockHeight, blockchainHeight, currentConfig)) {
                 completableFuture = shardService.tryCreateShardAsync(lastTrimBlockHeight, blockchainHeight);
             } else {
                 log.debug("No attempt to create new shard at height '{}' (because lastTrimHeight={}), ({})",
@@ -85,8 +103,6 @@ public class ShardObserver {
             if (blockchainConfig.isJustUpdated()) {
                 blockchainConfig.resetJustUpdated(); // reset flag
             }
-        } else {
-            log.debug("Sharding is disabled on node : {} && {}", shardingEnabled, isShardingOff);
         }
         return completableFuture;
     }
