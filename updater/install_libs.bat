c2f207ec6744df00769168f7ab18cb805f8233c3
Rostyslav Golda <tx_hv@ukr.net>
2019-12-16 11:45:53 +0200

    Windows updater scripts update

--- updater/install_libs.bat
+++ updater/install_libs.bat
@@ -4,7 +4,6 @@ del /S /F /Q %1\bin
 del /S /F /Q %1\sbin
 del /S /F /Q %1\*.jar
 
-
 curl --retry 100 -o %1\libs.zip -k https://s3.amazonaws.com/updates.apollowallet.org/libs/ApolloWallet-%VERSION%-libs.zip
 unzip -o %1\libs.zip -d %1
 
@@ -17,7 +16,7 @@ del /S /F /Q %1\ApolloWallet-%VERSION%-libs*
 mkdir %1\tmpdir
 curl --retry 100 -o %1\tmpdir\%2.zip -k https://s3.amazonaws.com/updates.apollowallet.org/database/%2.zip
 del /S /F /Q %userprofile%\.apl-blockchain\apl-blockchain-db\%2*
-unzip -o %1\tmpdir\%2.zip -D %userprofile%\.apl-blockchain\apl-blockchain-db\
+unzip -o %1\tmpdir\%2.zip -d %userprofile%\.apl-blockchain\apl-blockchain-db\
 del /S /F /Q %1\tmpdir\%2.zip
 
 
