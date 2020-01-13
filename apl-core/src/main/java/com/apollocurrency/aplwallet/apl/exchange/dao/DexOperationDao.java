7d624a896fbcaf56c7a3a6308e5b3c25c16c3b8e
Andrew Boyarsky <andrew.boyarski@gmail.com>
2020-01-13 11:43:07 +0200

    Add dex_operation dao tests, fix sql bugs

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/exchange/dao/DexOperationDao.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/exchange/dao/DexOperationDao.java
@@ -12,27 +12,33 @@ import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 public interface DexOperationDao {
     @Transactional(readOnly = true)
     @RegisterRowMapper(DexOperationMapper.class)
-    @SqlQuery("SELECT * FROM dex_operation WHERE account = :account AND stage = :stage AND eid = :eid ORDER BY time DESC LIMIT 1")
+    @SqlQuery("SELECT * FROM dex_operation WHERE account = :account AND stage = :stage AND eid = :eid ORDER BY ts DESC LIMIT 1")
     @RegisterArgumentFactory(DexOperationStageArgumentFactory.class)
     DexOperation getBy(@Bind("account") String account, @Bind("stage") DexOperation.Stage stage, @Bind("eid") String eid);
 
     @Transactional
-    @SqlUpdate("INSERT INTO dex_operation (account, stage, description, details, time, finished) VALUES (:account, :stage, :description, :details, :time, :finished)")
+    @SqlUpdate("INSERT INTO dex_operation (account, stage, description, details, eid,  ts, finished) VALUES (:account, :stage, :description, :details,:eid, :ts, :finished)")
     @RegisterArgumentFactory(DexOperationStageArgumentFactory.class)
     @GetGeneratedKeys
     long add(@BindBean DexOperation op);
 
     @Transactional
-    @SqlUpdate("UPDATE dex_operation SET account = :account, stage = :stage, eid = :eid, description = :description,  details = :details, time = :time, finished = :finished WHERE db_id = :dbId")
+    @SqlUpdate("UPDATE dex_operation SET account = :account, stage = :stage, eid = :eid, description = :description,  details = :details, ts = :ts, finished = :finished WHERE db_id = :dbId")
     @RegisterArgumentFactory(DexOperationStageArgumentFactory.class)
     int updateByDbId(@BindBean DexOperation op);
 
     @Transactional
-    @SqlUpdate("DELETE FROM dex_operation WHERE time < :toTimestamp")
+    @SqlUpdate("DELETE FROM dex_operation WHERE ts < :toTimestamp")
     int deleteAfterTimestamp(@Bind("toTimestamp") Timestamp toTimestamp);
+
+    @Transactional(readOnly = true)
+    @SqlQuery("SELECT * FROM dex_operation WHERE db_id > :fromDbId ORDER BY db_id LIMIT :limit")
+    @RegisterRowMapper(DexOperationMapper.class)
+    List<DexOperation> getAll(@Bind("fromDbId") long fromDbId, @Bind("limit") int limit);
 
 }
