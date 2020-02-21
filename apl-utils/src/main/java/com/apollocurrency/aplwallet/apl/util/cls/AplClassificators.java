473084a9f7affe8bb21ab9525cd69aa3470bcb7c
Oleksiy Lukin <oleksiy.lukin@firstbrige.io>
2020-02-21 16:08:54 +0200

    Missing classificator data files added, unneded classificators removed

--- apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/cls/AplClassificators.java
+++ apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/cls/AplClassificators.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.apollocurrency.aplwallet.apl.util.cls;
 
 import java.io.IOException;
@@ -14,7 +10,7 @@ import org.slf4j.LoggerFactory;
 /**
  * This is classificators for certificaqte-related and other itmes.
  * Each item have name and Long value. Items could have sub-classifcators
- * TODO: replace hard-coded initializators with init from JSON files
+ * TODO: replace hard-coded initializers with init from JSON files
  * @author alukin@gmail.com
  */
 public class AplClassificators {
@@ -23,10 +19,7 @@ public class AplClassificators {
     public static final String AUTORITY_CLS = "AuthorityCodes";
     public static final String BUSINESS_CLS = "BusinessCodes";
     public static final String OPERATIONS_CLS = "OperationsCodes";
-    public static final String REGION_CLS = "RegionCodes"; // Regions incude several countries
     public static final String SUPL_CLS = "SupplementalCodes";
-    public static final String COUNTRY_CLS = "Countries"; // With states/provinces and cities
-    public static final String CERTPURPOSE_CLS = "CertPurpose";
     
     private static final Logger logger = LoggerFactory.getLogger(AplClassificators.class);
     
@@ -37,10 +30,7 @@ public class AplClassificators {
             clsMap.put(AUTORITY_CLS, BasicClassificator.fromJsonResource(AUTORITY_CLS+".json").setValueComparator(new NumbericValueComparator()));
             clsMap.put(BUSINESS_CLS, BasicClassificator.fromJsonResource(BUSINESS_CLS+".json").setValueComparator(new NumbericValueComparator()));
             clsMap.put(OPERATIONS_CLS, BasicClassificator.fromJsonResource(OPERATIONS_CLS+".json").setValueComparator(new NumbericValueComparator()));
-            clsMap.put(REGION_CLS, BasicClassificator.fromJsonResource(REGION_CLS+".json").setValueComparator(new NumbericValueComparator()));
             clsMap.put(SUPL_CLS, BasicClassificator.fromJsonResource(SUPL_CLS+".json").setValueComparator(new NumbericValueComparator()));
-            clsMap.put(COUNTRY_CLS, BasicClassificator.fromJsonResource(COUNTRY_CLS+".json"));
-            clsMap.put(CERTPURPOSE_CLS, BasicClassificator.fromJsonResource(CERTPURPOSE_CLS+".json").setValueComparator(new NumbericValueComparator()));
         } catch (IOException ex) {
             logger.error("Can not loadclassificator from reources", ex);
         }
