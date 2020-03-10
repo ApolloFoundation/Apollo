ef3f35d372232ab3f50730c99c9c595c54378d67
Andrew Boyarsky <andrew.boyarski@gmail.com>
2020-03-10 14:19:17 +0200

    New multiplatform update attachment structure

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/transaction/messages/update/UpdateV2Attachment.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/transaction/messages/update/UpdateV2Attachment.java
@@ -14,12 +14,13 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
+import org.json.JSONArray;
 import org.json.simple.JSONObject;
-import org.web3j.crypto.Pair;
 
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
-import java.util.EnumSet;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 @EqualsAndHashCode(callSuper = false)
@@ -31,7 +32,7 @@ public class UpdateV2Attachment extends AbstractAttachment {
     private final Set<PlatformPair> platforms = new HashSet<>();
     private final Version releaseVersion;
     private final String cn;
-    private final long serialNumber;
+    private final BigInteger serialNumber;
     private final byte[] signature;
 
     public UpdateV2Attachment(ByteBuffer buffer) throws AplException.NotValidException {
@@ -43,10 +44,12 @@ public class UpdateV2Attachment extends AbstractAttachment {
             for (int i = 0; i < platformsLength; i++) {
                 this.platforms.add(new PlatformPair(Platform.from(buffer.get()), Architecture.from(buffer.get())));
             }
-            this.releaseVersion = new Version(buffer.get(), buffer.getShort(), buffer.getShort());
+            this.releaseVersion = new Version(buffer.getShort(), buffer.getShort(), buffer.getShort());
             this.cn = Convert.readString(buffer, buffer.getShort(), 2048);
-            this.serialNumber = buffer.getLong();
-            byte sigLength = buffer.get();
+            byte[] serialNumberBytes = new byte[buffer.getShort()];
+            buffer.get(serialNumberBytes);
+            this.serialNumber = new BigInteger(serialNumberBytes);
+            short sigLength = buffer.getShort();
             this.signature = new byte[sigLength];
             buffer.get(this.signature);
         } catch (NotValidException ex) {
@@ -56,27 +59,34 @@ public class UpdateV2Attachment extends AbstractAttachment {
 
     public UpdateV2Attachment(JSONObject attachmentData) {
         super(attachmentData);
-        manifestUrl =  Convert.nullToEmpty((String) attachmentData.get("manifestUrl"));
-        updateLevel = Level.from((int)Convert.parseLong(attachmentData.get("level")));
-
-        Platform.from((int)Convert.parseLong(attachmentData.get("platform")));
-        architecture = Architecture.from((int)Convert.parseLong(attachmentData.get("architecture")));
-        releaseVersion = new Version((String)(attachmentData.get("version")));
+        this.manifestUrl =  Convert.nullToEmpty((String) attachmentData.get("manifestUrl"));
+        this.updateLevel = Level.from((int)Convert.parseLong(attachmentData.get("level")));
+        JSONArray platforms = (JSONArray) attachmentData.get("platforms");
+        for (Object platformObj : platforms) {
+            JSONObject platformJsonObj = (JSONObject) platformObj;
+            PlatformPair platformPair = new PlatformPair(Platform.from((int) platformJsonObj.get("platform")), Architecture.from((int) platformJsonObj.get("architecture")));
+            this.platforms.add(platformPair);
+        }
+        this.cn = (String) attachmentData.get("cn");
+        this.signature = Convert.parseHexString((String) attachmentData.get("signature"));
+        this.serialNumber = new BigInteger((String) attachmentData.get("serialNumber"));
+        this.releaseVersion = new Version((String)(attachmentData.get("version")));
 
     }
 
 
-    public UpdateV2Attachment(String manifestUrl, Level updateLevel, Platform platform, Architecture architecture, Version version) {
+    public UpdateV2Attachment(String manifestUrl, Level updateLevel, Version releaseVersion, String cn, BigInteger serialNumber, byte[] signature) {
         this.manifestUrl = manifestUrl;
         this.updateLevel = updateLevel;
-        this.platform = platform;
-        this.architecture = architecture;
-        this.releaseVersion = version;
+        this.releaseVersion = releaseVersion;
+        this.cn = cn;
+        this.serialNumber = serialNumber;
+        this.signature = signature;
     }
 
     @Override
     public int getMySize() {
-        return 2 + manifestUrl.getBytes().length + 1 + 1 + 1 + 5;
+        return 2 + manifestUrl.getBytes().length + 1 + 1 + 2 * this.platforms.size() + 6 + 2 + this.cn.getBytes().length + 2 + this.serialNumber.toByteArray().length + 2 + signature.length;
     }
 
     @Override
@@ -85,20 +95,36 @@ public class UpdateV2Attachment extends AbstractAttachment {
         buffer.putShort((short) manifestUrlBytes.length);
         buffer.put(manifestUrlBytes);
         buffer.put(updateLevel.code);
-        buffer.put(platform.code);
-        buffer.put(architecture.code);
-        buffer.put((byte) releaseVersion.getMajorVersion());
+        buffer.put((byte) platforms.size());
+        for (PlatformPair platformPair : platforms) {
+            buffer.put(platformPair.platform.code);
+            buffer.put(platformPair.architecture.code);
+        }
+        buffer.putShort((short) releaseVersion.getMajorVersion());
         buffer.putShort((short) releaseVersion.getIntermediateVersion());
         buffer.putShort((short) releaseVersion.getMinorVersion());
+        buffer.putShort((short) cn.length());
+        buffer.put(cn.getBytes());
+        byte[] snBytes = serialNumber.toByteArray();
+        buffer.putShort((short) snBytes.length);
+        buffer.put(snBytes);
+        buffer.putShort((short) signature.length);
+        buffer.put(signature);
     }
 
     @Override
     public void putMyJSON(JSONObject attachment) {
         attachment.put("manifestUrl", manifestUrl);
         attachment.put("level", updateLevel.code);
-        attachment.put("platform", platform.code);
-        attachment.put("architecture", architecture.code);
+        JSONArray platformArray = new JSONArray();
+        for (PlatformPair platformPair : this.platforms) {
+            platformArray.put(new JSONObject(Map.of("platform", platformPair.platform.code, "architecture", platformPair.architecture.code)));
+        }
+        attachment.put("platforms", platformArray);
         attachment.put("version", releaseVersion.toString());
+        attachment.put("cn", this.cn);
+        attachment.put("serialNumber", this.serialNumber.toString());
+        attachment.put("signature", Convert.toHexString(this.signature));
     }
 
     @Override
