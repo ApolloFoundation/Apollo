b1972a57e0c1125925e2d048711c940e08f72f91
Yuriy Larin <yuriy.larin@firstbridge.io>
2020-04-15 17:56:02 +0300

    updated

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/rest/NewApiRegistry.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/rest/NewApiRegistry.java
@@ -70,10 +70,10 @@ public class NewApiRegistry {
         apis.put("getTime", "/rest/server/blockchain/time"); //GET
         apis.put("getTotalSupply", "/rest/server/blockchain/supply"); //GET
 
-        apis.put("getBlock", "/rest/block/"); //GET
-        apis.put("getBlockId", "/rest/block/id"); //GET
-        apis.put("getBlocks", "/rest/blocks"); //GET
-        apis.put("getECBlock", "/rest/blocks/ec"); //GET
+//        apis.put("getBlock", "/rest/block/one"); //GET
+//        apis.put("getBlockId", "/rest/block/id"); //GET
+//        apis.put("getBlocks", "/rest/block/list"); //GET
+//        apis.put("getECBlock", "/rest/block/ec"); //GET
 
         //TODO: add new implemented endpoints
     }
