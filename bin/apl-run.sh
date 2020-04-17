70886b06b8a4217cc9670ca0d6ad1950e1f99995
silaev-firstbridge <konstantin.silaev@firstbridge.io>
2020-04-17 12:13:40 +0300

    [APL-1497] Remove extra modules, add BOM

--- bin/apl-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# (C) 2019 Apollo Foundation 
-# Starts Apollo blockchain in foreground
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
- . ${DIR}/apl-common.sh 
-
-${JAVA_CMD} ${JAVA_OPT} -jar ${MAIN_JAR} $@
