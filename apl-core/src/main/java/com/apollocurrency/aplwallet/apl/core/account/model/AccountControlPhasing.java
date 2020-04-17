6a940a8738c348db3e89cd0a6299623f06354d49
Yuriy Larin <yuriy.larin@firstbridge.io>
2020-04-17 21:13:13 +0300

    added new 'account control phasing' entity, dao, service code

---
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/account/model/AccountControlPhasing.java
@@ -0,0 +1,62 @@
+/*
+ *  Copyright © 2018-2020 Apollo Foundation
+ */
+
+package com.apollocurrency.aplwallet.apl.core.account.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.apollocurrency.aplwallet.apl.core.db.DbKey;
+import com.apollocurrency.aplwallet.apl.core.db.DbUtils;
+import com.apollocurrency.aplwallet.apl.core.db.model.VersionedDeletableEntity;
+import com.apollocurrency.aplwallet.apl.core.phasing.model.PhasingParams;
+import com.apollocurrency.aplwallet.apl.crypto.Convert;
+import lombok.Data;
+
+@Data
+public class AccountControlPhasing extends VersionedDeletableEntity {
+
+//    private final DbKey dbKey;
+    private final long accountId;
+    private long maxFees;
+    private short minDuration;
+    private short maxDuration;
+    private PhasingParams phasingParams;
+
+    public AccountControlPhasing(DbKey dbKey, long accountId, PhasingParams params, long maxFees, short minDuration, short maxDuration, int height) {
+        super(null, height);
+        this.accountId = accountId;
+        setDbKey(dbKey);
+        this.phasingParams = params;
+        this.maxFees = maxFees;
+        this.minDuration = minDuration;
+        this.maxDuration = maxDuration;
+    }
+
+    public AccountControlPhasing(long accountId, PhasingParams params, long maxFees, short minDuration, short maxDuration, int height) {
+        super(null, height);
+        this.accountId = accountId;
+//        this.dbKey = null;//AccountRestrictions.phasingControlDbKeyFactory.newKey(this.accountId); // TODO fix it
+        this.phasingParams = params;
+        this.maxFees = maxFees;
+        this.minDuration = minDuration;
+        this.maxDuration = maxDuration;
+    }
+
+    public AccountControlPhasing(ResultSet rs, DbKey dbKey) throws SQLException {
+        super(rs);
+        setDbKey(dbKey);
+        this.accountId = rs.getLong("account_id");
+        Long[] whitelist = DbUtils.getArray(rs, "whitelist", Long[].class);
+        phasingParams = new PhasingParams(rs.getByte("voting_model"),
+            rs.getLong("holding_id"),
+            rs.getLong("quorum"),
+            rs.getLong("min_balance"),
+            rs.getByte("min_balance_model"),
+            whitelist == null ? Convert.EMPTY_LONG : Convert.toArray(whitelist));
+        this.maxFees = rs.getLong("max_fees");
+        this.minDuration = rs.getShort("min_duration");
+        this.maxDuration = rs.getShort("max_duration");
+    }
+}
---
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/account/service/AccountControlPhasingService.java
@@ -0,0 +1,32 @@
+/*
+ *  Copyright © 2018-2020 Apollo Foundation
+ */
+
+package com.apollocurrency.aplwallet.apl.core.account.service;
+
+import java.util.Map;
+
+import com.apollocurrency.aplwallet.apl.core.account.model.Account;
+import com.apollocurrency.aplwallet.apl.core.account.model.AccountControlPhasing;
+import com.apollocurrency.aplwallet.apl.core.app.Transaction;
+import com.apollocurrency.aplwallet.apl.core.db.DbIterator;
+import com.apollocurrency.aplwallet.apl.core.transaction.TransactionType;
+import com.apollocurrency.aplwallet.apl.core.transaction.messages.SetPhasingOnly;
+import com.apollocurrency.aplwallet.apl.util.AplException;
+
+public interface AccountControlPhasingService {
+
+    AccountControlPhasing get(long accountId);
+
+    int getCount();
+
+    DbIterator<AccountControlPhasing> getAll(int from, int to);
+
+    void unset(Account account);
+
+    void set(Account senderAccount, SetPhasingOnly attachment);
+
+    void checkTransaction(Transaction transaction) throws AplException.NotCurrentlyValidException;
+
+    boolean isBlockDuplicate(Transaction transaction, Map<TransactionType, Map<String, Integer>> duplicates);
+}
