12449f09e1d1f6c83d71c5f53516c28e56038499
Oleksiy Lukin <oleksiy.lukin@firstbrige.io>
2020-04-30 11:45:26 +0300

    fix file permissions and Linux build script

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,9 @@
 echo "*********** BUILD Apollo **********"
 echo "----------------------------------------"
 echo "*********** BUILD apl-bom-ext **********"
-cd apl-bom-ext && ./mvnw clean install
+cd apl-bom-ext 
+./mvnw clean install
 
 echo "*********** BUILD apollo-wallet (including apl-bom) **********"
-cd .. && ./mvnw clean install $1
+cd .. 
+./mvnw clean install $1
