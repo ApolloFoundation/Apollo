0604053e077286537e617b6adf05ea8cc4ebcc37
Andrew Boyarsky <andrew.boyarski@gmail.com>
2020-05-05 15:20:16 +0300

    Update ntp test, delete unused deps

--- apl-utils/src/test/java/com/apollocurrency/aplwallet/apl/util/NtpTimeTest.java
+++ apl-utils/src/test/java/com/apollocurrency/aplwallet/apl/util/NtpTimeTest.java
@@ -1,32 +1,38 @@
 package com.apollocurrency.aplwallet.apl.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.jboss.weld.junit5.EnableWeld;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
-@EnableWeld
 class NtpTimeTest {
 
-//    @WeldSetup
-//    public WeldInitiator weld = WeldInitiator.from(NtpTime.class)
-//        .build();
-//
-//    private NtpTime ntpTime;
-//    private long currentTime;
-//    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-//
-//    @BeforeEach
-//    void setUp() {
-//        currentTime = System.currentTimeMillis();
-//    }
-//
-//    @Test
-//    void getTimeTest() {
-//        ntpTime = new NtpTime();
-//        ntpTime.start(); // emulate @PostConstrust
-//        long freshTime = ntpTime.getTime();
-//        assertTrue(freshTime > currentTime);
-//        log.info("now : long = {} / formatted = {}", freshTime, dateFormat.format(freshTime));
-//    }
+    private NtpTime ntpTime;
+    private long currentTime;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+    @BeforeEach
+    void setUp() {
+        currentTime = System.currentTimeMillis();
+        ntpTime = new NtpTime();
+        ntpTime.start(); // emulate @PostConstrust
+    }
+
+    @AfterEach
+    void tearDown() {
+        ntpTime.shutdown();
+    }
+
+    @Test
+    void getTimeTest() {
+        long freshTime = ntpTime.getTime();
+        assertTrue(freshTime > currentTime);
+        log.info("now : long = {} / formatted = {}", freshTime, dateFormat.format(freshTime));
+    }
 
 }
