7e478f669faf7755e2f09a6930b914740e2cadd7
Oleksiy Lukin <oleksiy.lukin@firstbrige.io>
2020-05-12 14:59:56 +0300

    treat file as binary

--- apl-crypto/src/test/java/com/apollocurrency/aplwallet/apl/crypto/TestsCommons.java
+++ apl-crypto/src/test/java/com/apollocurrency/aplwallet/apl/crypto/TestsCommons.java
@@ -13,30 +13,32 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Common code for tests
+ *
  * @author alukin@gmail.com
  */
 public class TestsCommons {
-    protected static final String TST_IN_DIR="testdata/input/";
-    protected static final String TST_OUT_DIR="testdata/out/";
-            
-    protected static final String PLAIN_FILE_TEXT = "lorem_ipsum.txt";
+
+    protected static final String TST_IN_DIR = "testdata/input/";
+    protected static final String TST_OUT_DIR = "testdata/out/";
+
+    protected static final String PLAIN_FILE_TEXT = "lorem_ipsum.bin";
     protected static final byte[] nonce1 = new byte[32]; //(0-31)
     protected static final byte[] nonce2 = new byte[32]; //(32-63)
     protected static final String secretPhraseA = "Red fox jumps over the Lazy dog";
     protected static final String secretPhraseB = "Red dog jumps over the Lazy fox";
     protected static byte[] plain_data;
-    
-    static{
-         String inFile=TST_IN_DIR + PLAIN_FILE_TEXT;
+
+    static {
+        String inFile = TST_IN_DIR + PLAIN_FILE_TEXT;
 
         try {
             ByteBuffer pd = readFromFile(inFile);
             plain_data = pd.array();
             File directory = new File(TST_OUT_DIR);
-            if (! directory.exists()){
+            if (!directory.exists()) {
                 directory.mkdirs();
             }
-    
+
             writeToFile(pd, TST_OUT_DIR + PLAIN_FILE_TEXT);
             for (Integer i = 0; i < 32; i++) {
                 nonce1[i] = i.byteValue();
@@ -44,9 +46,9 @@ public class TestsCommons {
             }
         } catch (IOException ex) {
             fail("Can not read input data file: " + inFile);
-        }       
+        }
     }
-       
+
     public static void writeToFile(ByteBuffer data, String fileName) throws IOException {
         FileChannel out = new FileOutputStream(fileName).getChannel();
         data.rewind();
@@ -55,7 +57,7 @@ public class TestsCommons {
     }
 
     public static ByteBuffer readFromFile(String fileName) throws IOException {
-        
+
         FileChannel fChan;
         Long fSize;
         ByteBuffer mBuf;
@@ -66,5 +68,5 @@ public class TestsCommons {
         fChan.close();
         mBuf.rewind();
         return mBuf;
-    }     
+    }
 }
