12bc0d3ca67c499dcd525790bfff53b1e0c5fd19
Rostyslav Golda <tx_hv@ukr.net>
2020-05-28 09:21:19 +0300

    Fix path

--- bin/apl-run.sh
+++ bin/apl-run.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
  . "${DIR}"/apl-common.sh
 
-${JAVA_CMD} ${JAVA_OPT} -jar ${MAIN_JAR} $@
+"${JAVA_CMD}" ${JAVA_OPT} -jar "${MAIN_JAR}" $@
