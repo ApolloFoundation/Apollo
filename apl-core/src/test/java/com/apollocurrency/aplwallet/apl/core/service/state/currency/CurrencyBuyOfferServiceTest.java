5b819d3e8594f0045e340d952498a088ba786b32
Yuriy Larin <yuriy.larin@firstbridge.io>
2020-06-05 11:49:54 +0300

    added empty uni test

---
+++ apl-core/src/test/java/com/apollocurrency/aplwallet/apl/core/service/state/currency/CurrencyBuyOfferServiceTest.java
@@ -0,0 +1,11 @@
+/*
+ *  Copyright © 2018-2020 Apollo Foundation
+ */
+
+package com.apollocurrency.aplwallet.apl.core.service.state.currency;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyBuyOfferServiceTest {
+
+}
