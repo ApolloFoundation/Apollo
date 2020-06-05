21ac3883eeacf79c7a8c768d53d15144e585acde
Yuriy Larin <yuriy.larin@firstbridge.io>
2020-06-05 16:54:01 +0300

    added 'sell offer' service, unit test

---
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/service/state/currency/impl/CurrencySellOfferServiceImpl.java
@@ -0,0 +1,153 @@
+/*
+ *  Copyright © 2018-2020 Apollo Foundation
+ */
+
+package com.apollocurrency.aplwallet.apl.core.service.state.currency.impl;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.stream.Stream;
+
+import com.apollocurrency.aplwallet.apl.core.app.Transaction;
+import com.apollocurrency.aplwallet.apl.core.converter.rest.IteratorToStreamConverter;
+import com.apollocurrency.aplwallet.apl.core.dao.state.currency.CurrencySellOfferTable;
+import com.apollocurrency.aplwallet.apl.core.db.DbClause;
+import com.apollocurrency.aplwallet.apl.core.db.DbIterator;
+import com.apollocurrency.aplwallet.apl.core.entity.state.account.Account;
+import com.apollocurrency.aplwallet.apl.core.entity.state.currency.CurrencySellOffer;
+import com.apollocurrency.aplwallet.apl.core.monetary.Currency;
+import com.apollocurrency.aplwallet.apl.core.service.state.BlockChainInfoService;
+import com.apollocurrency.aplwallet.apl.core.transaction.messages.MonetarySystemPublishExchangeOffer;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Singleton
+public class CurrencySellOfferServiceImpl {
+
+    private final CurrencySellOfferTable currencySellOfferTable;
+    private final BlockChainInfoService blockChainInfoService;
+    private final IteratorToStreamConverter<CurrencySellOffer> currencyBuyOfferIteratorToStreamConverter;
+
+    @Inject
+    public CurrencySellOfferServiceImpl(CurrencySellOfferTable currencySellOfferTable,
+                                        BlockChainInfoService blockChainInfoService) {
+        this.currencySellOfferTable = currencySellOfferTable;
+        this.blockChainInfoService = blockChainInfoService;
+        currencyBuyOfferIteratorToStreamConverter = new IteratorToStreamConverter<>();
+    }
+
+    /**
+     * for unit tests
+     */
+    public CurrencySellOfferServiceImpl(CurrencySellOfferTable currencySellOfferTable,
+                                        BlockChainInfoService blockChainInfoService,
+                                        IteratorToStreamConverter<CurrencySellOffer> currencyBuyOfferIteratorToStreamConverter) {
+        this.currencySellOfferTable = currencySellOfferTable;
+        this.blockChainInfoService = blockChainInfoService;
+        if (currencyBuyOfferIteratorToStreamConverter != null) {
+            this.currencyBuyOfferIteratorToStreamConverter = currencyBuyOfferIteratorToStreamConverter;
+        } else {
+            this.currencyBuyOfferIteratorToStreamConverter = new IteratorToStreamConverter<>();
+        }
+    }
+
+    public int getCount() {
+        return currencySellOfferTable.getCount();
+    }
+
+    public CurrencySellOffer getOffer(long offerId) {
+        return currencySellOfferTable.get(CurrencySellOfferTable.buyOfferDbKeyFactory.newKey(offerId));
+    }
+
+    public DbIterator<CurrencySellOffer> getAll(int from, int to) {
+        return currencySellOfferTable.getAll(from, to);
+    }
+
+    public Stream<CurrencySellOffer> getAllStream(int from, int to) {
+        return currencyBuyOfferIteratorToStreamConverter.apply(currencySellOfferTable.getAll(from, to));
+    }
+
+    public DbIterator<CurrencySellOffer> getOffers(Currency currency, int from, int to) {
+        return getCurrencyOffers(currency.getId(), false, from, to);
+    }
+
+    public Stream<CurrencySellOffer> getOffersStream(Currency currency, int from, int to) {
+        return getCurrencyOffersStream(currency.getId(), false, from, to);
+    }
+
+    public DbIterator<CurrencySellOffer> getCurrencyOffers(long currencyId, boolean availableOnly, int from, int to) {
+        DbClause dbClause = new DbClause.LongClause("currency_id", currencyId);
+        if (availableOnly) {
+//            dbClause = dbClause.and(availableOnlyDbClause);
+        }
+        return currencySellOfferTable.getManyBy(dbClause, from, to, " ORDER BY rate DESC, creation_height ASC, transaction_height ASC, transaction_index ASC ");
+    }
+
+    public Stream<CurrencySellOffer> getCurrencyOffersStream(long currencyId, boolean availableOnly, int from, int to) {
+        DbClause dbClause = new DbClause.LongClause("currency_id", currencyId);
+        if (availableOnly) {
+//            dbClause = dbClause.and(availableOnlyDbClause);
+        }
+        return currencyBuyOfferIteratorToStreamConverter.apply(
+            currencySellOfferTable.getManyBy(dbClause, from, to,
+                " ORDER BY rate DESC, creation_height ASC, transaction_height ASC, transaction_index ASC ")
+        );
+    }
+
+    public DbIterator<CurrencySellOffer> getAccountOffers(long accountId, boolean availableOnly, int from, int to) {
+        DbClause dbClause = new DbClause.LongClause("account_id", accountId);
+        if (availableOnly) {
+//            dbClause = dbClause.and(availableOnlyDbClause);
+        }
+        return currencySellOfferTable.getManyBy(dbClause, from, to, " ORDER BY rate DESC, creation_height ASC, transaction_height ASC, transaction_index ASC ");
+    }
+
+    public Stream<CurrencySellOffer> getAccountOffersStream(long accountId, boolean availableOnly, int from, int to) {
+        DbClause dbClause = new DbClause.LongClause("account_id", accountId);
+        if (availableOnly) {
+//            dbClause = dbClause.and(availableOnlyDbClause);
+        }
+        return currencyBuyOfferIteratorToStreamConverter.apply(
+            currencySellOfferTable.getManyBy(dbClause, from, to,
+                " ORDER BY rate DESC, creation_height ASC, transaction_height ASC, transaction_index ASC ")
+        );
+    }
+
+    public CurrencySellOffer getOffer(Currency currency, Account account) {
+        return getOffer(currency.getId(), account.getId());
+    }
+
+    public CurrencySellOffer getOffer(final long currencyId, final long accountId) {
+        return currencySellOfferTable.getBy(new DbClause.LongClause("currency_id", currencyId)
+            .and(new DbClause.LongClause("account_id", accountId)));
+    }
+
+    public DbIterator<CurrencySellOffer> getOffers(DbClause dbClause, int from, int to) {
+        return currencySellOfferTable.getManyBy(dbClause, from, to);
+    }
+
+    public Stream<CurrencySellOffer> getOffersStream(DbClause dbClause, int from, int to) {
+        return currencyBuyOfferIteratorToStreamConverter.apply(currencySellOfferTable.getManyBy(dbClause, from, to));
+    }
+
+    public DbIterator<CurrencySellOffer> getOffers(DbClause dbClause, int from, int to, String sort) {
+        return currencySellOfferTable.getManyBy(dbClause, from, to, sort);
+    }
+
+    public Stream<CurrencySellOffer> getOffersStream(DbClause dbClause, int from, int to, String sort) {
+        return currencyBuyOfferIteratorToStreamConverter.apply(currencySellOfferTable.getManyBy(dbClause, from, to, sort));
+    }
+
+    public void addOffer(Transaction transaction, MonetarySystemPublishExchangeOffer attachment) {
+        CurrencySellOffer currencyBuyOffer = new CurrencySellOffer(transaction, attachment, blockChainInfoService.getHeight());
+        currencySellOfferTable.insert(currencyBuyOffer);
+    }
+
+    public void remove(CurrencySellOffer buyOffer) {
+        int height = blockChainInfoService.getHeight();
+        buyOffer.setHeight(height); // important to assign height here!
+        currencySellOfferTable.deleteAtHeight(buyOffer, height);
+    }
+
+
+}
