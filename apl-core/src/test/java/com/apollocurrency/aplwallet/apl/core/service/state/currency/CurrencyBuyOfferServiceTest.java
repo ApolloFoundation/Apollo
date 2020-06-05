9ddf97ac075a7057c7f73361fc02bb1737f57fbb
Yuriy Larin <yuriy.larin@firstbridge.io>
2020-06-05 11:56:24 +0300

    added one method

--- apl-core/src/test/java/com/apollocurrency/aplwallet/apl/core/service/state/currency/CurrencyBuyOfferServiceTest.java
+++ apl-core/src/test/java/com/apollocurrency/aplwallet/apl/core/service/state/currency/CurrencyBuyOfferServiceTest.java
@@ -4,8 +4,81 @@
 
 package com.apollocurrency.aplwallet.apl.core.service.state.currency;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.apollocurrency.aplwallet.apl.core.app.Blockchain;
+import com.apollocurrency.aplwallet.apl.core.app.BlockchainImpl;
+import com.apollocurrency.aplwallet.apl.core.app.BlockchainProcessor;
+import com.apollocurrency.aplwallet.apl.core.app.BlockchainProcessorImpl;
+import com.apollocurrency.aplwallet.apl.core.chainid.BlockchainConfig;
+import com.apollocurrency.aplwallet.apl.core.dao.state.currency.CurrencyBuyOfferTable;
+import com.apollocurrency.aplwallet.apl.core.db.DatabaseManager;
+import com.apollocurrency.aplwallet.apl.core.db.DerivedDbTablesRegistryImpl;
+import com.apollocurrency.aplwallet.apl.core.db.DerivedTablesRegistry;
+import com.apollocurrency.aplwallet.apl.core.db.fulltext.FullTextConfig;
+import com.apollocurrency.aplwallet.apl.core.db.fulltext.FullTextConfigImpl;
+import com.apollocurrency.aplwallet.apl.core.entity.state.currency.CurrencyBuyOffer;
+import com.apollocurrency.aplwallet.apl.core.service.state.BlockChainInfoService;
+import com.apollocurrency.aplwallet.apl.core.service.state.currency.impl.CurrencyBuyOfferServiceImpl;
+import com.apollocurrency.aplwallet.apl.data.CurrencyBuyOfferTestData;
+import com.apollocurrency.aplwallet.apl.extension.DbExtension;
+import com.apollocurrency.aplwallet.apl.util.injectable.PropertiesHolder;
+import org.jboss.weld.junit.MockBean;
+import org.jboss.weld.junit5.EnableWeld;
+import org.jboss.weld.junit5.WeldInitiator;
+import org.jboss.weld.junit5.WeldSetup;
+import org.jdbi.v3.core.Jdbi;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
+import javax.inject.Inject;
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+@EnableWeld
 class CurrencyBuyOfferServiceTest {
+    @RegisterExtension
+    static DbExtension dbExtension = new DbExtension();
+
+    @Inject
+    CurrencyBuyOfferTable table;
+    CurrencyBuyOfferTestData td;
+    @Inject
+    private CurrencyBuyOfferServiceImpl offerService;
+
+    Comparator<CurrencyBuyOffer> currencyBuyOfferComparator = Comparator
+        .comparing(CurrencyBuyOffer::getId)
+        .thenComparing(CurrencyBuyOffer::getAccountId);
+
+    private Blockchain blockchain = mock(BlockchainImpl.class);
+    private BlockchainConfig blockchainConfig = mock(BlockchainConfig.class);
+    private BlockChainInfoService blockChainInfoService = mock(BlockChainInfoService.class);
+    private BlockchainProcessor blockchainProcessor = mock(BlockchainProcessor.class);
+
+    @WeldSetup
+    public WeldInitiator weld = WeldInitiator.from(
+        PropertiesHolder.class, CurrencyBuyOfferTable.class, CurrencyBuyOfferServiceImpl.class
+    )
+        .addBeans(MockBean.of(dbExtension.getDatabaseManager(), DatabaseManager.class))
+        .addBeans(MockBean.of(dbExtension.getDatabaseManager().getJdbi(), Jdbi.class))
+        .addBeans(MockBean.of(blockchainConfig, BlockchainConfig.class))
+        .addBeans(MockBean.of(blockchain, Blockchain.class, BlockchainImpl.class))
+        .addBeans(MockBean.of(blockchainProcessor, BlockchainProcessor.class, BlockchainProcessorImpl.class))
+        .addBeans(MockBean.of(mock(FullTextConfig.class), FullTextConfig.class, FullTextConfigImpl.class))
+        .addBeans(MockBean.of(mock(DerivedTablesRegistry.class), DerivedTablesRegistry.class, DerivedDbTablesRegistryImpl.class))
+        .addBeans(MockBean.of(blockChainInfoService, BlockChainInfoService.class))
+        .build();
+
+    @BeforeEach
+    void setUp() {
+        td = new CurrencyBuyOfferTestData();
+    }
+
+    @Test
+    void test_count() {
+        int result = offerService.getCount();
+        assertEquals(9, result);
+    }
 
 }
