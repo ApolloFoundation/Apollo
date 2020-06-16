8cc1cd97c4b130d2a0cad071ca53f95ccc0b956a
Yuriy Larin <yuriy.larin@firstbridge.io>
2020-06-16 14:18:34 +0300

    added 'currency mint' service, empty unit test

---
+++ apl-core/src/test/java/com/apollocurrency/aplwallet/apl/core/service/state/currency/CurrencyMintServiceTest.java
@@ -0,0 +1,20 @@
+package com.apollocurrency.aplwallet.apl.core.service.state.currency;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CurrencyMintServiceTest {
+
+    @Test
+    void mintCurrency() {
+    }
+
+    @Test
+    void getCounter() {
+    }
+
+    @Test
+    void deleteCurrency() {
+    }
+}
