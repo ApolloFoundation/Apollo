4be9992c249bd1463e147803a1ece8ec918bd987
azinchenko <andrew.zinchenko@gmail.com>
2020-07-06 10:04:56 +0300

    APL-1574 fixed tests;

--- apl-core/src/test/java/com/apollocurrency/aplwallet/apl/core/transaction/ChildAccountTest.java
+++ apl-core/src/test/java/com/apollocurrency/aplwallet/apl/core/transaction/ChildAccountTest.java
@@ -4,14 +4,18 @@
 
 package com.apollocurrency.aplwallet.apl.core.transaction;
 
+import com.apollocurrency.aplwallet.apl.core.app.AplException;
 import com.apollocurrency.aplwallet.apl.core.app.Blockchain;
 import com.apollocurrency.aplwallet.apl.core.app.BlockchainImpl;
+import com.apollocurrency.aplwallet.apl.core.app.EcBlockData;
+import com.apollocurrency.aplwallet.apl.core.app.Transaction;
 import com.apollocurrency.aplwallet.apl.core.app.TransactionProcessor;
 import com.apollocurrency.aplwallet.apl.core.chainid.BlockchainConfig;
 import com.apollocurrency.aplwallet.apl.core.chainid.HeightConfig;
 import com.apollocurrency.aplwallet.apl.core.db.dao.ReferencedTransactionDao;
 import com.apollocurrency.aplwallet.apl.core.entity.state.account.Account;
 import com.apollocurrency.aplwallet.apl.core.entity.state.account.AddressScope;
+import com.apollocurrency.aplwallet.apl.core.model.CreateTransactionRequest;
 import com.apollocurrency.aplwallet.apl.core.rest.TransactionCreator;
 import com.apollocurrency.aplwallet.apl.core.service.appdata.TimeService;
 import com.apollocurrency.aplwallet.apl.core.service.state.PhasingPollService;
@@ -29,25 +33,29 @@ import org.jboss.weld.junit.MockBean;
 import org.jboss.weld.junit5.EnableWeld;
 import org.jboss.weld.junit5.WeldInitiator;
 import org.jboss.weld.junit5.WeldSetup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @EnableWeld
 @ExtendWith(MockitoExtension.class)
-public abstract class ChildAccountTest {
+public class ChildAccountTest {
 
     public static final int ECBLOCK_HEIGHT = 100_000;
     public static final long ECBLOCK_ID = 121L;
 
-    @Mock
-    TransactionValidator validator;
-    @Mock
-    BlockchainConfig blockchainConfig;
     @Mock
     HeightConfig heightConfig;
     @Mock
@@ -57,17 +65,16 @@ public abstract class ChildAccountTest {
     @Mock
     PropertiesHolder propertiesHolder;
     @Mock
-    FeeCalculator calculator;
-    @Mock
     ReferencedTransactionDao referencedTransactionDao;
     @Mock
     PhasingPollService phasingPollService;
-    @Mock
-    AccountControlPhasingService accountControlPhasingService;
 
+    AccountControlPhasingService accountControlPhasingService=mock(AccountControlPhasingService.class);
+    BlockchainConfig blockchainConfig=mock(BlockchainConfig.class);
     Blockchain blockchain = mock(Blockchain.class);
     AccountService accountService=mock(AccountService.class);
     AccountPublicKeyService accountPublicKeyService=mock(AccountPublicKeyService.class);
+    FeeCalculator calculator=mock(FeeCalculator.class);
 
     @WeldSetup
     WeldInitiator weldInitiator = WeldInitiator.from()
@@ -95,7 +102,218 @@ public abstract class ChildAccountTest {
 
     ChildAccountAttachment attachment = new ChildAccountAttachment(AddressScope.IN_FAMILY, 2, List.of(CHILD_PUBLIC_KEY_1, CHILD_PUBLIC_KEY_2));
 
-    TransactionCreator txCreator;
-    TransactionApplier txApplier;
-    TransactionValidator txValidator;
+    TransactionApplier txApplier= new TransactionApplier(blockchainConfig, referencedTransactionDao, accountService, accountPublicKeyService);
+    TransactionValidator txValidator = new TransactionValidator(blockchainConfig, phasingPollService, blockchain, calculator, accountControlPhasingService, accountService);
+    TransactionCreator txCreator= new TransactionCreator(txValidator, propertiesHolder, timeService, calculator, blockchain, processor);
+
+    @BeforeEach
+    void setUp() {
+        child1.setParentId(0L);
+        child1.setMultiSig(false);
+        child2.setParentId(0L);
+        child2.setMultiSig(false);
+
+        EcBlockData ecBlockData = new EcBlockData(ECBLOCK_ID, ECBLOCK_HEIGHT);
+        when(blockchain.getECBlock(300)).thenReturn(ecBlockData);
+        when(accountService.getAccount(senderId)).thenReturn(sender);
+        when(accountService.addOrGetAccount(CHILD_ID_1)).thenReturn(child1);
+        when(accountService.addOrGetAccount(CHILD_ID_2)).thenReturn(child2);
+        when(accountService.getAccount(CHILD_PUBLIC_KEY_1)).thenReturn(child1).thenReturn(null);
+    }
+
+    @Test
+    void applyAttachment() {
+        //GIVEN
+        CreateTransactionRequest request = CreateTransactionRequest.builder()
+            .senderAccount(sender)
+            .deadlineValue("1440")
+            .feeATM(0)
+            .attachment(attachment)
+            .timestamp(300)
+            .keySeed(Crypto.getKeySeed(secretPhrase))
+            .broadcast(false)
+            .build();
+        Transaction tx = txCreator.createTransactionThrowingException(request);
+        assertNotNull(tx);
+
+        //WHEN
+        txApplier.apply(tx);
+
+        //THEN
+        assertEquals(sender.getId(), child1.getParentId());
+        assertEquals(sender.getId(), child2.getParentId());
+
+        assertTrue(child1.isChild());
+        assertTrue(child2.isChild());
+
+        assertTrue(child1.isMultiSig());
+        assertTrue(child2.isMultiSig());
+
+        assertEquals(attachment.getAddressScope(), child1.getAddrScope());
+        assertEquals(attachment.getAddressScope(), child2.getAddrScope());
+
+        //Don't remove, sounds weird, but this snippet doesn't work in batch mode.
+        /*ArgumentCaptor<Account> argument = ArgumentCaptor.forClass(Account.class);
+        verify(accountService, times(2)).update(argument.capture(), eq(false));
+        List<Long> args = argument.getAllValues().stream().map(Account::getId).collect(Collectors.toUnmodifiableList());
+        assertTrue(args.contains(CHILD_ID_1));
+        assertTrue(args.contains(CHILD_ID_2));*/
+
+        verify(accountPublicKeyService).apply(child1, CHILD_PUBLIC_KEY_1);
+        verify(accountPublicKeyService).apply(child2, CHILD_PUBLIC_KEY_2);
+    }
+
+    @Test
+    void validateAttachment() throws AplException.ValidationException {
+        //GIVEN
+        CreateTransactionRequest request = CreateTransactionRequest.builder()
+            .senderAccount(sender)
+            .deadlineValue("1440")
+            .feeATM(0)
+            .attachment(attachment)
+            .timestamp(300)
+            .keySeed(Crypto.getKeySeed(secretPhrase))
+            .broadcast(false)
+            .build();
+        Transaction tx = txCreator.createTransactionThrowingException(request);
+        assertNotNull(tx);
+        when(blockchain.getHeight()).thenReturn(ECBLOCK_HEIGHT+1);
+        when(blockchain.getBlockIdAtHeight(ECBLOCK_HEIGHT)).thenReturn(ECBLOCK_ID);
+        when(blockchainConfig.getCurrentConfig()).thenReturn(heightConfig);
+        when(heightConfig.getMaxBalanceATM()).thenReturn(Long.MAX_VALUE);
+
+        when(heightConfig.getMaxPayloadLength()).thenReturn(255*Constants.MIN_TRANSACTION_SIZE);
+
+        //WHEN
+        txValidator.validate(tx);
+
+        verify(accountControlPhasingService).checkTransaction(tx);
+    }
+
+    @Test
+    void validateAttachment_AmountGTZero() {
+        //GIVEN
+        CreateTransactionRequest request = CreateTransactionRequest.builder()
+            .senderAccount(sender)
+            .deadlineValue("1440")
+            .feeATM(0)
+            .amountATM(1000L)
+            .attachment(attachment)
+            .timestamp(300)
+            .keySeed(Crypto.getKeySeed(secretPhrase))
+            .broadcast(false)
+            .build();
+        Transaction tx = txCreator.createTransactionThrowingException(request);
+
+        assertNotNull(tx);
+        when(blockchain.getHeight()).thenReturn(ECBLOCK_HEIGHT+1);
+        when(blockchain.getBlockIdAtHeight(ECBLOCK_HEIGHT)).thenReturn(ECBLOCK_ID);
+        when(blockchainConfig.getCurrentConfig()).thenReturn(heightConfig);
+        when(heightConfig.getMaxBalanceATM()).thenReturn(Long.MAX_VALUE);
+
+        //WHEN
+        try {
+            txValidator.validate(tx);
+            fail("Unexpected flow.");
+        } catch (AplException.ValidationException e) {
+            //THEN
+            assertTrue(e.getMessage().contains("Transactions of this type must have recipient == 0, amount == 0"), "Unexpected exception message.");
+        }
+    }
+
+    @Test
+    void validateAttachment_withWrongChildAccountCount() throws AplException.ValidationException {
+        //GIVEN
+        int wrongChildCountValue = 1;// 2 is valid
+        CreateTransactionRequest request = CreateTransactionRequest.builder()
+            .senderAccount(sender)
+            .publicKey(Convert.parseHexString(publicKey))
+            .deadlineValue("1440")
+            .feeATM(0L)
+            .amountATM(0L)
+            .attachment(new ChildAccountAttachment(AddressScope.IN_FAMILY, wrongChildCountValue, List.of(CHILD_PUBLIC_KEY_1, CHILD_PUBLIC_KEY_2)))
+            .timestamp(300)
+            .broadcast(false)
+            .build();
+        Transaction tx = txCreator.createTransactionThrowingException(request);
+
+        assertNotNull(tx);
+        when(blockchain.getHeight()).thenReturn(ECBLOCK_HEIGHT+1);
+        when(blockchain.getBlockIdAtHeight(ECBLOCK_HEIGHT)).thenReturn(ECBLOCK_ID);
+        when(blockchainConfig.getCurrentConfig()).thenReturn(heightConfig);
+        when(heightConfig.getMaxBalanceATM()).thenReturn(Long.MAX_VALUE);
+
+
+        //WHEN
+        try {
+            txValidator.validate(tx);
+            fail("Unexpected flow.");
+        } catch (AplException.ValidationException e) {
+            //THEN
+            assertTrue(e.getMessage().contains("Wrong value of the child count, count=1"), "Unexpected exception message.");
+        }
+    }
+
+    @Test
+    void validateAttachment_withParentPublicKey() throws AplException.ValidationException {
+        //GIVEN
+        CreateTransactionRequest request = CreateTransactionRequest.builder()
+            .senderAccount(sender)
+            .publicKey(Convert.parseHexString(publicKey))
+            .deadlineValue("1440")
+            .feeATM(0L)
+            .amountATM(0L)
+            .attachment(new ChildAccountAttachment(AddressScope.IN_FAMILY, 2, List.of(CHILD_PUBLIC_KEY_1, Convert.parseHexString(publicKey))))
+            .timestamp(300)
+            .broadcast(false)
+            .build();
+        Transaction tx = txCreator.createTransactionThrowingException(request);
+
+        assertNotNull(tx);
+        when(blockchain.getHeight()).thenReturn(ECBLOCK_HEIGHT+1);
+        when(blockchain.getBlockIdAtHeight(ECBLOCK_HEIGHT)).thenReturn(ECBLOCK_ID);
+        when(blockchainConfig.getCurrentConfig()).thenReturn(heightConfig);
+        when(heightConfig.getMaxBalanceATM()).thenReturn(Long.MAX_VALUE);
+
+        //WHEN
+        try {
+            txValidator.validate(tx);
+            fail("Unexpected flow.");
+        } catch (AplException.ValidationException e) {
+            //THEN
+            assertTrue(e.getMessage().contains("a child can't simultaneously be a parent"), "Unexpected exception message.");
+        }
+    }
+
+    @Test
+    void validateAttachment_withChildAlreadyExists() {
+        //GIVEN
+        CreateTransactionRequest request = CreateTransactionRequest.builder()
+            .senderAccount(sender)
+            .publicKey(Convert.parseHexString(publicKey))
+            .deadlineValue("1440")
+            .feeATM(0L)
+            .amountATM(0L)
+            .attachment(attachment)
+            .timestamp(300)
+            .broadcast(false)
+            .build();
+        Transaction tx = txCreator.createTransactionThrowingException(request);
+
+        assertNotNull(tx);
+        when(blockchain.getHeight()).thenReturn(ECBLOCK_HEIGHT+1);
+        when(blockchain.getBlockIdAtHeight(ECBLOCK_HEIGHT)).thenReturn(ECBLOCK_ID);
+        when(blockchainConfig.getCurrentConfig()).thenReturn(heightConfig);
+        when(heightConfig.getMaxBalanceATM()).thenReturn(Long.MAX_VALUE);
+
+        //WHEN
+        try {
+            txValidator.validate(tx);
+            fail("Unexpected flow.");
+        } catch (AplException.ValidationException e) {
+            //THEN
+            assertTrue(e.getMessage().contains("Child account already exists"), "Unexpected exception message.");
+        }
+    }
+
 }
