2a08d66dc4ab87099a4ff52a4877a65665842b53
Oleksiy Lukin <oleksiy.lukin@firstbrige.io>
2020-08-05 16:55:33 +0300

    index of nets by chainID added

--- apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/env/dirprovider/DefaultConfigDirProvider.java
+++ apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/env/dirprovider/DefaultConfigDirProvider.java
@@ -1,7 +1,6 @@
 /*
  * Copyright © 2018-2019 Apollo Foundation
  */
-
 package com.apollocurrency.aplwallet.apl.util.env.dirprovider;
 
 import java.nio.file.Paths;
@@ -10,7 +9,28 @@ import java.nio.file.Paths;
  * Config dir provider which provide default config files locations
  */
 public class DefaultConfigDirProvider implements ConfigDirProvider {
-    protected static final String[] CONF_DIRS = {"conf", "conf-tn1", "conf-tn2", "conf-tn3", "conf-tn4"};
+
+    /**
+     * Chain IDs ow known networks from 0 (mainnet) to 3rd testnet }
+     */
+    protected static final String[] CHAIN_IDS = {
+        "b5d7b697-f359-4ce5-a619-fa34b6fb01a5", //main net
+        "a2e9b946-290b-48b6-9985-dc2e5a5860a1", //test net 1
+        "2f2b6149-d29e-41ca-8c0d-f3343f5540c6", //test net 2
+        "3fecf3bd-86a3-436b-a1d6-41eefc0bd1c6" //test net 3
+    };
+
+    /**
+     * Deprecated names of configuration directories for known networks. Should
+     * be replaced later by chainID
+     */
+    protected static final String[] CONF_DIRS = {
+        "conf", //main net
+        "conf-tn1", //test net 1
+        "conf-tn2", //test net 2
+        "conf-tn3" //test net 3
+    };
+
     protected String applicationName;
     protected boolean isService;
     protected int netIndex;
@@ -19,8 +39,8 @@ public class DefaultConfigDirProvider implements ConfigDirProvider {
      * Constructs config dir provider
      *
      * @param applicationName name of application's parameter dir
-     * @param isService       service mode or user mode
-     * @param netIdx          index of network. 0 means main net, 1,2,3 - testnets 1,2,3
+     * @param isService service mode or user mode
+     * @param netIdx index of network. 0 means main net, 1,2,3 - testnets 1,2,3
      */
     public DefaultConfigDirProvider(String applicationName, boolean isService, int netIdx) {
         if (applicationName == null || applicationName.trim().isEmpty()) {
@@ -60,10 +80,10 @@ public class DefaultConfigDirProvider implements ConfigDirProvider {
 
     @Override
     public String getConfigDirectory() {
-        String res =
-            isService
-                ? getSysConfigDirectory()
-                : getUserConfigDirectory();
+        String res
+                = isService
+                        ? getSysConfigDirectory()
+                        : getUserConfigDirectory();
         return res;
 
     }
