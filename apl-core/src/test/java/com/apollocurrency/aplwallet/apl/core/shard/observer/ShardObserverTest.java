22e0ea5fd48ad70d266e72817a9f0536ad57d868
Yuriy Larin <yuriy.larin@firstbridge.io>
2020-09-10 09:22:31 +0300

    added lock to calculation logic

--- apl-core/src/test/java/com/apollocurrency/aplwallet/apl/core/shard/observer/ShardObserverTest.java
+++ apl-core/src/test/java/com/apollocurrency/aplwallet/apl/core/shard/observer/ShardObserverTest.java
@@ -46,6 +46,7 @@ import static org.mockito.Mockito.withSettings;
 public class ShardObserverTest {
     private static final String CONFIG_NAME = "another-list-for-shar-observer-config.json";
     private static final String CONFIG_NAME_2 = "tn3-shard-observer-config.json";
+    private static final String CONFIG_NAME_3 = "mainnet-config.json";
     private ChainsConfigLoader chainsConfigLoader;
     private Map<UUID, Chain> loadedChains;
     private Chain chain;
@@ -162,7 +163,8 @@ public class ShardObserverTest {
 
     @ParameterizedTest
     @MethodSource("supplyTestDataTN3")
-    void testStartShardingByTN3(int latestShardHeight, int currentBlockHeight,
+    void testStartShardingByTN3(int latestShardHeight,
+                                int currentBlockHeight,
                                 int targetShardHeight,
                                 boolean isShouldBeCalled,
                                 int randomMockValue) {
@@ -192,7 +194,7 @@ public class ShardObserverTest {
             // 2. currentHeightBlockPushed - simulate block to be pushed at that height
             // 3. newShardHeight - we expect shard to be created at that height !!
             // 4. isShardingCalled - check if sharding was really executed
-            // 5. isConfigJustUpdate - simulate HeightConfig is updated from previous to next height
+            // 5. mocked random Value - simulate random divergence
             arguments(0, 100, 0, false, 143),
             arguments(0, 4144, 2000, true, 143),
             arguments(6000, 10144, 8000, true, 143),
@@ -205,5 +207,43 @@ public class ShardObserverTest {
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("supplyTestDataMainNet")
+    void testStartShardingByMainNet(int latestShardHeight, int currentBlockHeight,
+                                int targetShardHeight,
+                                boolean isShouldBeCalled,
+                                int randomMockValue) {
+        // prepare components
+        prepareAndInit(CONFIG_NAME_3, 21000, randomMockValue);
+        // prepare tes data
+        Shard shard = mock(Shard.class);
+        doReturn(latestShardHeight).when(shard).getShardHeight();
+        doReturn(shard).when(shardService).getLastShard();
+        Block block = td.BLOCK_1;
+        block.setHeight(currentBlockHeight);
+        blockchainConfigUpdater.onBlockPopped(block); // simulate setting 'current' and 'previous' config
+
+        shardObserver.onBlockPushed(block);
+
+        if (isShouldBeCalled) {
+            verify(shardService).tryCreateShardAsync(targetShardHeight, currentBlockHeight);
+        } else {
+            verify(shardService, never()).tryCreateShardAsync(anyInt(), anyInt());
+        }
+    }
+
+    static Stream<Arguments> supplyTestDataMainNet() {
+        return Stream.of(
+            // arguments by order:
+            // 1. lastShardHeight - simulate previously created shard (height)
+            // 2. currentHeightBlockPushed - simulate block to be pushed at that height
+            // 3. newShardHeight - we expect shard to be created at that height !!
+            // 4. isShardingCalled - check if sharding was really executed
+            // 5. mocked random Value - simulate random divergence
+            arguments(0, 2247000, 0, false, 323),
+            arguments(0, 2271324, 2250000, true, 323),
+            arguments(0, 2271217, 2250000, true, 216)
+        );
+    }
 
 }
