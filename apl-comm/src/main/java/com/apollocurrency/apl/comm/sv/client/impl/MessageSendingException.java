e850bf6f2cfab7cf3b7dd16ec0f737ad9695c38d
Oleksiy Lukin <oleksiy.lukin@firstbrige.io>
2020-09-23 13:11:18 +0300

    Object communication library now is separate project

--- apl-comm/src/main/java/com/apollocurrency/apl/comm/sv/client/impl/MessageSendingException.java
+++
@@ -1,7 +0,0 @@
-package com.apollocurrency.apl.comm.sv.client.impl;
-
-public class MessageSendingException extends RuntimeException {
-    public MessageSendingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
