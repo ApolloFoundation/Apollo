12d1c2400d1224a06d312436e6e0264a882a966a
Rostyslav Golda <tx_hv@ukr.net>
2020-12-02 09:25:48 +0200

    Fixed windows update script

--- updater/install_libs.bat
+++ updater/install_libs.bat
@@ -4,16 +4,16 @@ del /S /F /Q %1\bin
 del /S /F /Q %1\sbin
 del /S /F /Q %1\*.jar
 
-curl --retry 100 -o %1\libs.zip -k https://s3.amazonaws.com/updates.apollowallet.org/libs/ApolloWallet-%VERSION%-libs.zip
-unzip -o %1\libs.zip -d %1
+rem curl --retry 100 -o %1\libs.zip -k https://s3.amazonaws.com/updates.apollowallet.org/libs/ApolloWallet-%VERSION%-libs.zip
+rem unzip -o %1\libs.zip -d %1
 
-del /S /F /Q %1\lib
-rmdir /S /Q %1\lib
-mkdir %1\lib
+rem del /S /F /Q %1\lib
+rem rmdir /S /Q %1\lib
+rem mkdir %1\lib
 
-move /Y %WALLETDIR:~0,-1%\ApolloWallet-%VERSION%-libs\*" %WALLETDIR:~0,-1%\lib"
-del /S /F /Q %1\ApolloWallet-%VERSION%-libs*
-mkdir %1\tmpdir
+rem move /Y %WALLETDIR:~0,-1%\ApolloWallet-%VERSION%-libs\*" %WALLETDIR:~0,-1%\lib"
+rem del /S /F /Q %1\ApolloWallet-%VERSION%-libs*
+rem mkdir %1\tmpdir
 
 rem curl --retry 100 -o %1\tmpdir\%2.zip -k https://s3.amazonaws.com/updates.apollowallet.org/database/%2-2020-q1.zip
 rem del /S /F /Q %userprofile%\.apl-blockchain\apl-blockchain-db\%2
