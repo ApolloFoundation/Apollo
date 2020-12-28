12cf207b31b23b3d96aeddc10d21eb0e6c7bc94a
Rostyslav Golda <tx_hv@ukr.net>
2020-12-28 09:22:50 +0200

    APL-1769: Windows update script corrected

---
+++ updater/update.bat
@@ -0,0 +1,31 @@
+set /p VERSION=<VERSION
+set WALLETDIR=%1
+set TARGETDIR=%2
+del /S /F /Q %1\bin
+del /S /F /Q %1\sbin
+del /S /F /Q %1\*.jar
+del /S /F /Q %1\guilib
+del /S /F /Q %1\html-stub
+del /S /F /Q %1\lib
+del /S /F /Q %1\sbin
+del /S /F /Q %1\updater
+del /S /F /Q %1\webui
+del /S /F /Q %1\%1\3RD-PARTY-LICENSES.txt
+del /S /F /Q %1\VERSION*
+del /S /F /Q %1\LICENSE*
+del /S /F /Q %1\META-INF
+del /S /F /Q %1\update*
+
+robocopy %1 %1\.. /S
+robocopy %2 %1\.. /S
+
+del /S /F /Q %1
+rmdir /S /Q %1
+
+
+
+
+
+
+
+
