4248058a284300ba8c3354690e879d588a38a5d1
Yuriy Larin <yuriy.larin@firstbridge.io>
2021-01-21 17:17:26 +0200

    update main scripts

--- apl-db-updater/src/main/resources/db/migration/shard/V1_0__Initialize_tables.sql
+++ apl-db-updater/src/main/resources/db/migration/shard/V1_0__Initialize_tables.sql
@@ -1,63 +1,69 @@
-CREATE TABLE IF NOT EXISTS block
+CREATE TABLE IF NOT EXISTS 'block'
 (
-    db_id                 BIGINT UNSIGNED NOT NULL,
-    id                    BIGINT          NOT NULL,
-    version               INT             NOT NULL,
-    `TIMESTAMP`           INT             NOT NULL,
-    previous_block_id     BIGINT,
-    total_amount          BIGINT          NOT NULL,
-    total_fee             BIGINT          NOT NULL,
-    payload_length        INT             NOT NULL,
-    previous_block_hash   BINARY(32),
-    cumulative_difficulty BLOB            NOT NULL,
-    base_target           BIGINT          NOT NULL,
-    next_block_id         BIGINT,
-    height                INT             NOT NULL,
-    generation_signature  BINARY(32)      NOT NULL,
-    block_signature       BINARY(64),
-    payload_hash          BINARY(32)      NOT NULL,
-    generator_id          BIGINT          NOT NULL,
-    timeout               INT             NOT NULL DEFAULT 0
-) ENGINE = ROCKSDB;
+    `db_id`                 bigint(20) unsigned NOT NULL,
+    `id`                    bigint(20)          NOT NULL,
+    `version`               int(11)             NOT NULL,
+    `TIMESTAMP`             int(11)             NOT NULL,
+    `previous_block_id`     bigint(20)          DEFAULT NULL,
+    `total_amount`          bigint(20)          NOT NULL,
+    `total_fee`             bigint(20)          NOT NULL,
+    `payload_length`        int(11)             NOT NULL,
+    `previous_block_hash`   binary(32)          DEFAULT NULL,
+    `cumulative_difficulty` blob                NOT NULL,
+    `base_target`           bigint(20)          NOT NULL,
+    `next_block_id`         bigint(20)          DEFAULT NULL,
+    `height`                int(11)             NOT NULL,
+    `generation_signature`  binary(32)          NOT NULL,
+    `block_signature`       binary(64)          NOT NULL,
+    `payload_hash`          binary(32)          NOT NULL,
+    `generator_id`          bigint(20)          NOT NULL,
+    `timeout`               int(11)             NOT NULL DEFAULT 0,
+) ENGINE = ROCKSDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS transaction
+CREATE TABLE IF NOT EXISTS 'transaction'
 (
-    db_id                            BIGINT UNSIGNED NOT NULL,
-    id                               BIGINT          NOT NULL,
-    deadline                         SMALLINT        NOT NULL,
-    recipient_id                     BIGINT,
-    transaction_index                SMALLINT        NOT NULL,
-    amount                           BIGINT          NOT NULL,
-    fee                              BIGINT          NOT NULL,
-    full_hash                        BINARY(32)      NOT NULL,
-    height                           INT             NOT NULL,
-    block_id                         BIGINT          NOT NULL,
-    signature                        BINARY(64)      NOT NULL,
-    `TIMESTAMP`                      INT             NOT NULL,
-    type                             TINYINT         NOT NULL,
-    subtype                          TINYINT         NOT NULL,
-    sender_id                        BIGINT          NOT NULL,
-    sender_public_key                BINARY(32),
-    block_timestamp                  INT             NOT NULL,
-    referenced_transaction_full_hash BINARY(32),
-    phased                           BOOLEAN         NOT NULL DEFAULT FALSE,
-    attachment_bytes                 BLOB,
-    version                          TINYINT         NOT NULL,
-    has_message                      BOOLEAN         NOT NULL DEFAULT FALSE,
-    has_encrypted_message            BOOLEAN         NOT NULL DEFAULT FALSE,
-    has_public_key_announcement      BOOLEAN         NOT NULL DEFAULT FALSE,
-    ec_block_height                  INT                      DEFAULT NULL,
-    ec_block_id                      BIGINT                   DEFAULT NULL,
-    has_encrypttoself_message        BOOLEAN         NOT NULL DEFAULT FALSE,
-    has_prunable_message             BOOLEAN         NOT NULL DEFAULT FALSE,
-    has_prunable_encrypted_message   BOOLEAN         NOT NULL DEFAULT FALSE,
-    has_prunable_attachment          BOOLEAN         NOT NULL DEFAULT FALSE
-) ENGINE = ROCKSDB;
+    `db_id`                            bigint(20) unsigned NOT NULL,
+    `id`                               bigint(20)          NOT NULL,
+    `deadline`                         smallint(6)         NOT NULL,
+    `recipient_id`                     bigint(20)          DEFAULT NULL,
+    `transaction_index`                smallint(6)         NOT NULL,
+    `amount`                           bigint(20)          NOT NULL,
+    `fee`                              bigint(20)          NOT NULL,
+    `full_hash`                        binary(32)          NOT NULL,
+    `height`                           int(11)             NOT NULL,
+    `block_id`                         bigint(20)          NOT NULL,
+    `signature`                        blob                DEFAULT NULL,
+    `TIMESTAMP`                        int(11)             NOT NULL,
+    `type`                             tinyint(4)          NOT NULL,
+    `subtype`                          tinyint(4)          NOT NULL,
+    `sender_id`                        bigint(20)          NOT NULL,
+    `sender_public_key`                binary(32)          DEFAULT NULL,
+    `block_timestamp`                  int(11)             NOT NULL,
+    `referenced_transaction_full_hash` binary(32)          DEFAULT NULL,
+    `phased`                           tinyint(1)          NOT NULL DEFAULT 0,
+    `attachment_bytes`                 blob                DEFAULT NULL,
+    `version`                          tinyint(4)          NOT NULL,
+    `has_message`                      tinyint(1)          NOT NULL DEFAULT 0,
+    `has_encrypted_message`            tinyint(1)          NOT NULL DEFAULT 0,
+    `has_public_key_announcement`      tinyint(1)          NOT NULL DEFAULT 0,
+    `ec_block_height`                  int(11)             DEFAULT NULL,
+    `ec_block_id`                      bigint(20)          DEFAULT NULL,
+    `has_encrypttoself_message`        tinyint(1)          NOT NULL DEFAULT 0,
+    `has_prunable_message`             tinyint(1)          NOT NULL DEFAULT 0,
+    `has_prunable_encrypted_message`   tinyint(1)          NOT NULL DEFAULT 0,
+    `has_prunable_attachment`          tinyint(1)          NOT NULL DEFAULT 0,
+) ENGINE = ROCKSDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS option
 (
     name    VARCHAR(100) not null,
     `VALUE` VARCHAR(150)
-) ENGINE = ROCKSDB;
+) ENGINE = ROCKSDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
 CREATE UNIQUE INDEX IF NOT EXISTS option_name_value_idx ON option (name, `VALUE`);
