baa6dd21f55344e3f2578adc216e9eb201f43a82
Andrew Boyarsky <andrew.boyarski@gmail.com>
2021-03-03 14:52:11 +0200

    Fix TrimObserver default delay

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/app/observer/TrimConfig.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/app/observer/TrimConfig.java
@@ -12,14 +12,21 @@ import javax.inject.Singleton;
 
 @Singleton
 public class TrimConfig {
+    public static final int DEFAULT_TRIM_DELAY = 2000;
     private final int trimFrequency;
     private final int trimDelay;
+    private final int defaultTrimDelay;
 
     @Inject
     public TrimConfig(@Property(value = "apl.trimProcessingDelay", defaultValue = "500") int trimDelay,
                       @Property(value = "apl.trimFrequency", defaultValue = "1000") int trimFrequency) {
+        this(trimDelay, trimFrequency, DEFAULT_TRIM_DELAY);
+    }
+
+    public TrimConfig(int trimDelay, int trimFrequency, int defaultTrimDelay) {
         this.trimFrequency = trimFrequency;
         this.trimDelay = trimDelay;
+        this.defaultTrimDelay = defaultTrimDelay;
     }
 
     public int getTrimFrequency() {
@@ -28,5 +35,9 @@ public class TrimConfig {
 
     public int getTrimDelay() {
         return trimDelay;
+    }
+
+    public int getDefaultTrimDelay() {
+        return defaultTrimDelay;
     }
 }
