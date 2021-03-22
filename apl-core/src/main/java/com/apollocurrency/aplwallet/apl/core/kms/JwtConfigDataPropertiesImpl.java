8927726550103fa45da1a1b981c4b7278ab28ee1
Yuriy Larin <yuriy.larin@firstbridge.io>
2021-03-22 18:27:22 +0200

    added JWT config properties and config data component

---
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/kms/JwtConfigDataPropertiesImpl.java
@@ -0,0 +1,40 @@
+package com.apollocurrency.aplwallet.apl.core.kms;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import com.apollocurrency.aplwallet.apl.util.cdi.config.Property;
+import io.firstbridge.kms.security.JwtConfigData;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Properties to be loaded from 'apl-blockchain.properties' file
+ */
+@Singleton
+@Setter
+@Getter
+@ToString
+public class JwtConfigDataPropertiesImpl implements JwtConfigData {
+
+    @Inject
+    public JwtConfigDataPropertiesImpl(@Property(name = "apl.jwt.issuer", defaultValue = "") String issuer,
+                                       @Property(name = "apl.jwt.secret", defaultValue = "") String secret,
+                                       @Property(name = "apl.jwt.access.expiration.time", defaultValue = "") String accessTokenExpirationTime,
+                                       @Property(name = "apl.jwt.admin.access.expiration.time", defaultValue = "") String accessAdminTokenExpirationTime,
+                                       @Property(name = "apl.jwt.refresh.expiration.time", defaultValue = "") String refreshTokenExpirationTime) {
+        this.issuer = issuer;
+        this.secret = secret;
+        this.accessTokenExpirationTime = accessTokenExpirationTime;
+        this.accessAdminTokenExpirationTime = accessAdminTokenExpirationTime;
+        this.refreshTokenExpirationTime = refreshTokenExpirationTime;
+    }
+
+    private String issuer;
+    private String secret;
+    private String accessTokenExpirationTime;
+    private String accessAdminTokenExpirationTime;
+    private String refreshTokenExpirationTime;
+
+}
