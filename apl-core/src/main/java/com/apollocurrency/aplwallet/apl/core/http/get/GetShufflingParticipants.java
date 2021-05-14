c44cff7ceeab058d3970da4c67717e1118bd2485
Andrew Boyarsky <andrew.boyarski@gmail.com>
2021-05-14 19:41:36 +0300

    Add test for all shuffling tables (derived from the APL-750 branch), fix null array of array bytes deserialization, replace DbIterator service/dao methods by the List methods

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/http/get/GetShufflingParticipants.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/http/get/GetShufflingParticipants.java
@@ -20,7 +20,6 @@
 
 package com.apollocurrency.aplwallet.apl.core.http.get;
 
-import com.apollocurrency.aplwallet.apl.core.db.DbIterator;
 import com.apollocurrency.aplwallet.apl.core.entity.state.shuffling.ShufflingParticipant;
 import com.apollocurrency.aplwallet.apl.core.http.APITag;
 import com.apollocurrency.aplwallet.apl.core.http.AbstractAPIRequestHandler;
@@ -33,6 +32,7 @@ import org.json.simple.JSONStreamAware;
 
 import javax.enterprise.inject.Vetoed;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Vetoed
 public final class GetShufflingParticipants extends AbstractAPIRequestHandler {
@@ -47,10 +47,9 @@ public final class GetShufflingParticipants extends AbstractAPIRequestHandler {
         JSONObject response = new JSONObject();
         JSONArray participantsJSONArray = new JSONArray();
         response.put("participants", participantsJSONArray);
-        try (DbIterator<ShufflingParticipant> participants = shufflingService.getParticipants(shufflingId)) {
-            for (ShufflingParticipant participant : participants) {
-                participantsJSONArray.add(JSONData.participant(participant));
-            }
+        List<ShufflingParticipant> participants = shufflingService.getParticipants(shufflingId);
+        for (ShufflingParticipant participant : participants) {
+            participantsJSONArray.add(JSONData.participant(participant));
         }
         return response;
     }
