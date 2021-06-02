ff802fd272dfa810797926600af70fb22c6c6414
Yuriy Larin <yuriy.larin@firstbridge.io>
2021-06-02 14:57:11 +0300

    added config parameter

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/kms/config/RemoteKmsConfigImpl.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/kms/config/RemoteKmsConfigImpl.java
@@ -24,18 +24,21 @@ public class RemoteKmsConfigImpl implements RemoteKmsConfig {
     private String address;
     private int grpcPort;
     private int httpPort;
+    private String httpSchema;
 
     @Inject
     public RemoteKmsConfigImpl(
         @Property(name = "kms.main.remote.server.mode.on", defaultValue = "false") boolean remoteServerModeOn,
         @Property(name = "kms.main.remote.server.address", defaultValue = "") String address,
         @Property(name = "kms.main.remote.server.grpc.port", defaultValue = "-1") int grpcPort,
-        @Property(name = "kms.main.remote.server.http.port", defaultValue = "-1") int httpPort
+        @Property(name = "kms.main.remote.server.http.port", defaultValue = "-1") int httpPort,
+        @Property(name = "kms.main.remote.server.http.schema", defaultValue = "") String httpSchema
     ) {
         this.remoteServerModeOn = remoteServerModeOn;
         this.address = address;
         this.grpcPort = grpcPort;
         this.httpPort = httpPort;
+        this.httpSchema = httpSchema;
     }
 
     public void setRemoteServerModeOn(boolean remoteServerModeOn) {
@@ -55,4 +58,10 @@ public class RemoteKmsConfigImpl implements RemoteKmsConfig {
     public WorkMode getRemoteServerModeOn() {
         return remoteServerModeOn ? WorkMode.REMOTE_SERVER : WorkMode.LOCAL_CODE;
     }
+
+    @Override
+    public String getHttpSchema() {
+        return this.httpSchema;
+    }
+
 }
