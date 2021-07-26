80d1eaffc97fed3a602eed74bdb4f95d5415b92e
Andrew Boyarsky <andrew.boyarski@gmail.com>
2021-07-26 18:14:46 +0300

    Add additional java docs for TransactionType validation methods and FeeFactory

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/transaction/TransactionType.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/transaction/TransactionType.java
@@ -206,7 +206,7 @@ public abstract class TransactionType {
      * balance, currency/asset balance, etc) to be included in a one block without conflicts (e.g double spending)
      * <p>This capability consist of checking that account has enough funds to pay transaction amount and fee and freezing tx's entire amount to guarantee
      * no double spending from other transactions, pretending on the same sender's funds. Transaction types implement
-     * the same freeze/unconfirmed state persistence for its specific attachments (if necessary)  </p>
+     * the same freeze/unconfirmed state persistence for its specific attachments (if necessary) {@link TransactionType#applyAttachmentUnconfirmed(Transaction, Account)}  </p>
      * <p>Represent a first step of a transaction execution</p>
      * <p>Restore modified data, if double spending occurred</p>
      * @return false if double spending, otherwise true
@@ -237,10 +237,14 @@ public abstract class TransactionType {
     protected abstract boolean applyAttachmentUnconfirmed(Transaction transaction, Account senderAccount);
 
     /**
-     * Execute transaction applying all the changes incl
-     * @param transaction
-     * @param senderAccount
-     * @param recipientAccount
+     * Execute transaction transferring transaction amount from sender's account to the recipient if any and perform tx type
+     * specific action by the method {@link TransactionType#applyAttachment(Transaction, Account, Account)}
+     * <p>Must not be overridden</p>
+     * @param transaction transaction to execute
+     * @param senderAccount transaction's sender
+     * @param recipientAccount transaction's recipient (may be null)
+     * @throws com.apollocurrency.aplwallet.apl.core.exception.AplTransactionExecutionException when transaction
+     * supports failing during execution {@link TransactionType#canFailDuringExecution() = true}
      */
     public void apply(Transaction transaction, Account senderAccount, Account recipientAccount) {
         long amount = transaction.getAmountATM();
@@ -261,6 +265,16 @@ public abstract class TransactionType {
         applyAttachment(transaction, senderAccount, recipientAccount);
     }
 
+    /**
+     * Compensate all the changes done by the transaction execution from the method {@link TransactionType#apply(Transaction, Account, Account)}
+     * <p>Typically should be invoked for reverting transaction, which failed during execution</p>
+     * <p>Transaction type specific 'undo' procedures should be implemented by the {@link TransactionType#undoApplyAttachment(Transaction, Account, Account)}</p>
+     * <p>For transaction types, which support failing during execution, in-memory action log is recommended to don't rollback partially executed actions</p>
+     * <p>Must not be overridden</p>
+     * @param transaction transaction to undo
+     * @param senderAccount transaction's sender
+     * @param recipientAccount transaction's recipient
+     */
     public void undoApply(Transaction transaction, Account senderAccount, Account recipientAccount) {
         long amount = transaction.getAmountATM();
         long transactionId = transaction.getId();
@@ -280,8 +294,24 @@ public abstract class TransactionType {
         undoApplyAttachment(transaction, senderAccount, recipientAccount);
     }
 
+    /**
+     * Execute transaction type specific actions depending on transaction attachment and other data
+     * <p>Not intended to be called directly, since this method will be called from {@link TransactionType#apply(Transaction, Account, Account)}</p>
+     *
+     * @param transaction transaction, which should be executed specifically for the transactio type
+     * @param senderAccount transaction's sender
+     * @param recipientAccount transaction's recipient (may be null)
+     * @throws com.apollocurrency.aplwallet.apl.core.exception.AplTransactionExecutionException when transaction type
+     * supports failing during execution
+     */
     public abstract void applyAttachment(Transaction transaction, Account senderAccount, Account recipientAccount);
 
+    /**
+     * Compensate all the changes done by the {@link TransactionType#applyUnconfirmed(Transaction, Account)}
+     * <p>Transaction type specific compensation actions should be performed by implementation of the {@link TransactionType#undoAttachmentUnconfirmed(Transaction, Account)} method</p>
+     * @param transaction transaction to undo freeze/validation changes
+     * @param senderAccount transaction's sender
+     */
     @TransactionFee(FeeMarker.UNDO_UNCONFIRMED_BALANCE)
     public final void undoUnconfirmed(Transaction transaction, Account senderAccount) {
         undoAttachmentUnconfirmed(transaction, senderAccount);
@@ -293,12 +323,19 @@ public abstract class TransactionType {
         }
     }
 
+    /**
+     * Compensate transaction type specific freeze/validation changes done by the {@link TransactionType#applyAttachmentUnconfirmed(Transaction, Account)}.
+     * <p>Typically should not be called directly, {@link TransactionType#undoUnconfirmed(Transaction, Account)} should be used instead</p>
+     * @param transaction transaction, which freeze/validation/unconfirmed changes should be reverted for specific tx type
+     * @param senderAccount transaction's sender
+     */
     @TransactionFee(FeeMarker.UNDO_UNCONFIRMED_BALANCE)
     public abstract void undoAttachmentUnconfirmed(Transaction transaction, Account senderAccount);
 
     /**
      * Check whether transaction is a duplicate under duplicates map, which should be updated (in most cases)
      * during check using principle defined by the {@link TransactionType#isDuplicate(TransactionTypes.TransactionTypeSpec, String, Map, int)}
+     * <p>Will not be called for phasing transaction on transaction's acceptance height, but will be called on transaction's execution height</p>
      * @param transaction transaction, that should be checked for duplicate
      * @param duplicates global duplicates map, which is shared among the all transactions which should be checked for duplicates
      * @return true, when transaction is a duplicate, otherwise false
@@ -307,21 +344,48 @@ public abstract class TransactionType {
         return false;
     }
 
-    // isBlockDuplicate and isDuplicate share the same duplicates map, but isBlockDuplicate check is done first
+    /**
+     * Typically do the same as {@link TransactionType#isDuplicate}  and share the same duplicates map, but isBlockDuplicate check
+     * is done first on transaction's acceptance height (when transaction included into a blockchain).
+     * <p>Will be called for phasing and standard transaction on transaction's acceptance height only</p>
+     *  @param transaction transaction, that should be checked for duplicates
+     *  @param duplicates global duplicates map, which is shared among the all transactions which should be checked for duplicates
+     *  @return true, when transaction is a duplicate, otherwise false
+     */
     public boolean isBlockDuplicate(Transaction transaction, Map<TransactionTypes.TransactionTypeSpec, Map<String, Integer>> duplicates) {
         return false;
     }
 
+    /**
+     * Verify duplicates on mempool level, currently is not used and should not be implemented
+     * @param transaction transaction to check
+     * @param duplicates global duplicates map
+     * @return false only
+     */
+    @Deprecated
     public boolean isUnconfirmedDuplicate(Transaction transaction, Map<TransactionTypes.TransactionTypeSpec, Map<String, Integer>> duplicates) {
         return false;
     }
 
+    /**
+     * Check, whether transaction is pruned (data was expired and cleared) or not
+     * @param transactionId id of the transaction to check
+     * @return false for not pruned transaction, otherwise true
+     */
     public boolean isPruned(long transactionId) {
         return false;
     }
 
+    /**
+     * Specify, whether transaction can have recipient or not
+     * @return true, when recipient is allowed, otherwise - false
+     */
     public abstract boolean canHaveRecipient();
 
+    /**
+     * Almost the same as {@link TransactionType#canHaveRecipient()} but more strict, primarily should be used for validation
+     * @return true, when transaction should be with a recipient field, otherwise - false
+     */
     public boolean mustHaveRecipient() {
         return canHaveRecipient();
     }
@@ -332,10 +396,22 @@ public abstract class TransactionType {
     @Deprecated
     public abstract boolean isPhasingSafe();
 
+    /**
+     * Specify, that transaction of the specific type may be phased (delayed until some event occurred)
+     * @return true, when phasig is allowed, otherwise false
+     */
     public boolean isPhasable() {
         return true;
     }
 
+    /**
+     * Specify current fee for the transaction of a specific type
+     * <p>Fee may be created using {@link TransactionType.FeeFactory} class, allowing flexible fee configs via chains.json
+     * and {@link com.apollocurrency.aplwallet.apl.util.env.config.TransactionFeeSettings}</p>
+     * <p>Default fee is whole one unit</p>
+     * @param transaction transaction, to get fee for
+     * @return fee object, which allows fee calculation for transactions of this type
+     */
     @TransactionFee(FeeMarker.BASE_FEE)
     public Fee getBaselineFee(Transaction transaction) {
         return getFeeFactory().createFixed(BigDecimal.ONE);
@@ -366,19 +442,47 @@ public abstract class TransactionType {
         return Integer.MAX_VALUE;
     }
 
+    /**
+     * Return array of splitted transaction fee into 3 parts to award to the last 3 forgers to enable more uniform fee
+     * distribution.
+     * <p>Should be avoided from usage in new transaction types, because it leads to the complicated and ambiguous logic</p>
+     * @param transaction transaction, which fee should be splitted
+     * @return array of 3 length, with transaction fee splitted
+     */
+    @Deprecated
     @TransactionFee(FeeMarker.FEE)
     public long[] getBackFees(Transaction transaction) {
         return Convert.EMPTY_LONG;
     }
 
+    /**
+     * Specify unique transaction type's name
+     * @return unique tx type's name
+     */
     public abstract String getName();
 
+    /**
+     * @return string representation of the transaction type including its spec and name
+     */
     @Override
     public final String toString() {
         return getName() + " " + getSpec();
     }
 
+    /**
+     * Handy class to create transaction fee objects along with the custom transaction fee configs, loaded from the chains.json.
+     * Fee factory offers a defaults support also
+     */
     public class FeeFactory {
+        /**
+         * Create fee object for size-based fee calculation using specific size function and unit size to count
+         * <p>Supports chains.json fee configuration, when specified, if not given default values will be used</p>
+         * @param defaultConstantAPL default constant fee value in APL to use for fee calculation, when fee config from chain.json is absent
+         * @param defaultSizedBasedFeeAPL default size-based fee value in APL to use for fee calculation, when fee config from chains.json is absent
+         * @param sizeFunction function, which calculates size on transaction and its appendage
+         * @param unitSize size of the transaction data, which should be treated as one unit, every unit will be accounted by the size-based fee value
+         * @return size-based fee object to calculate fee for the transaction
+         */
         public Fee createSizeBased(BigDecimal defaultConstantAPL, BigDecimal defaultSizedBasedFeeAPL, BiFunction<Transaction, Appendix, Integer> sizeFunction, int unitSize) {
             BlockchainConfig blockchainConfig = getBlockchainConfig();
             HeightConfig heightConfig = blockchainConfig.getCurrentConfig();
@@ -393,26 +497,53 @@ public abstract class TransactionType {
             };
         }
 
+        /**
+         * Same as {@link TransactionType.FeeFactory#createSizeBased(BigDecimal, BigDecimal, BiFunction, int)} but unitSize of
+         * 32 is supplied (default for most transaction types)
+         */
         public Fee createSizeBased(BigDecimal defaultConstantAPL, BigDecimal defaultSizedBasedFeeAPL, BiFunction<Transaction, Appendix, Integer> sizeFunction) {
             return createSizeBased(defaultConstantAPL, defaultSizedBasedFeeAPL, sizeFunction, 32);
         }
 
+        /**
+         * Create fixed fee object using configured fee for tx of a certain type in chains.json or using default supplied value in case of absent fee config
+         * @param defaultConstantAPL default fee value in APL to use for fee calculation, when fee config from chain.json is absent
+         * @return fixed fee object for the transaction type
+         */
         public Fee createFixed(BigDecimal defaultConstantAPL) {
             long oneAPL = blockchainConfig.getOneAPL();
             long constantFeeATM = blockchainConfig.getCurrentConfig().getBaseFee(getSpec(), defaultConstantAPL).multiply(BigDecimal.valueOf(oneAPL)).longValueExact();
             return new Fee.ConstantFee(constantFeeATM);
         }
 
+        /**
+         * Create custom fee object, which will calculate fee by the supplied function
+         * @param feeCalc custom fee calculation function
+         * @return fee object for a custom fee calculation
+         */
         public Fee createCustom(BiFunction<Transaction, Appendix, Long> feeCalc) {
             return (feeCalc::apply);
         }
 
     }
 
+    /**
+     * Perform transaction type's specific validation against a state, when phased transaction should be executed.
+     * <p>Typically should not be called directly, use {@link TransactionType#validateStateDependentAtFinish(Transaction)}</p>
+     * <p>In most cases should not be overridden</p>
+     * @param transaction phased transaction to validate at execution's height
+     * @throws AplException.ValidationException when transaction is not valid (avoid throwing it)
+     * @throws com.apollocurrency.aplwallet.apl.core.exception.AplTransactionValidationException when transaction is not valid for some meaningful reason (preferred way)
+     */
     protected void doStateDependentValidationAtFinish(Transaction transaction) throws AplException.ValidationException {
         doStateDependentValidation(transaction);
     }
 
+    /**
+     * Perform transaction type's
+     * @param transaction
+     * @throws AplException.ValidationException
+     */
     protected abstract void doStateIndependentValidation(Transaction transaction) throws AplException.ValidationException;
 
     protected abstract void doStateDependentValidation(Transaction transaction) throws AplException.ValidationException;
