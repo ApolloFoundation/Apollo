a31b8d12af6dccd87ede727673d0762b37968306
azinchenko <andrew.zinchenko@gmail.com>
2021-07-26 18:29:17 +0300

    [AD-290] bump version to 1.48.12; added src field in the response of the rest method /rest/v2/smc/{address};

--- apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/rest/v2/impl/SmcApiServiceImpl.java
+++ apl-core/src/main/java/com/apollocurrency/aplwallet/apl/core/rest/v2/impl/SmcApiServiceImpl.java
@@ -505,7 +505,7 @@ public class SmcApiServiceImpl implements SmcApiService {
     }
 
     @Override
-    public Response getSmcByAddress(String addressStr, SecurityContext securityContext) throws NotFoundException {
+    public Response getSmcByAddress(String addressStr, Boolean includeAsrCode, SecurityContext securityContext) throws NotFoundException {
         ResponseBuilderV2 builder = ResponseBuilderV2.startTiming();
 
         AplAddress address = new AplAddress(Convert.parseAccountId(addressStr));
