c9f1af318048fb0cf7a1bdd87c3999ab60da8354
Alexander Hrihoriev <a.hrihoriev@firstbridge.io>
2021-08-16 10:51:58 +0300

    Renamed main class. Added file headers

---
+++ apl-common/src/main/java/com/apollocurrency/aplwallet/apl/common/ApolloCommon.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright © 2021 Apollo Foundation
+ */
+
+package com.apollocurrency.aplwallet.apl.common;
+
+public class ApolloCommon {
+}
--- apl-common/src/main/java/com/apollocurrency/aplwallet/apl/common/Common.java
+++
@@ -1,1 +0,0 @@
-package com.apollocurrency.aplwallet.apl.common;
