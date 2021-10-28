13c2331fda8c65d333e4887955617b8ce591bba5
Andrew Boyarsky <andrew.boyarski@gmail.com>
2021-10-28 10:47:59 +0300

    Add explicit sort order for dex contract/order fetch

--- apl-core/src/test/java/com/apollocurrency/aplwallet/apl/exchange/dao/DexContractTableTest.java
+++ apl-core/src/test/java/com/apollocurrency/aplwallet/apl/exchange/dao/DexContractTableTest.java
@@ -4,55 +4,20 @@
 
 package com.apollocurrency.aplwallet.apl.exchange.dao;
 
-import com.apollocurrency.aplwallet.apl.core.service.blockchain.TransactionBuilderFactory;
-import com.apollocurrency.aplwallet.apl.core.cache.NullCacheProducerForTests;
-import com.apollocurrency.aplwallet.apl.core.chainid.BlockchainConfig;
-import com.apollocurrency.aplwallet.apl.core.config.DaoConfig;
-import com.apollocurrency.aplwallet.apl.core.config.NtpTimeConfig;
-import com.apollocurrency.aplwallet.apl.core.converter.db.BlockEntityRowMapper;
-import com.apollocurrency.aplwallet.apl.core.converter.db.BlockEntityToModelConverter;
-import com.apollocurrency.aplwallet.apl.core.converter.db.BlockModelToEntityConverter;
-import com.apollocurrency.aplwallet.apl.core.converter.db.PrunableTxRowMapper;
-import com.apollocurrency.aplwallet.apl.core.converter.db.TransactionEntityRowMapper;
-import com.apollocurrency.aplwallet.apl.core.converter.db.TransactionEntityToModelConverter;
-import com.apollocurrency.aplwallet.apl.core.converter.db.TransactionModelToEntityConverter;
-import com.apollocurrency.aplwallet.apl.core.converter.db.TxReceiptRowMapper;
 import com.apollocurrency.aplwallet.apl.core.dao.DbContainerBaseTest;
-import com.apollocurrency.aplwallet.apl.core.dao.blockchain.BlockDaoImpl;
-import com.apollocurrency.aplwallet.apl.core.dao.blockchain.TransactionDaoImpl;
-import com.apollocurrency.aplwallet.apl.core.db.DatabaseManager;
-import com.apollocurrency.aplwallet.apl.util.db.DbIterator;
-import com.apollocurrency.aplwallet.apl.core.config.JdbiConfiguration;
 import com.apollocurrency.aplwallet.apl.core.model.dex.ExchangeContract;
-import com.apollocurrency.aplwallet.apl.core.service.appdata.TimeService;
-import com.apollocurrency.aplwallet.apl.core.service.appdata.impl.TimeServiceImpl;
-import com.apollocurrency.aplwallet.apl.core.service.blockchain.BlockchainImpl;
-import com.apollocurrency.aplwallet.apl.core.service.blockchain.TransactionServiceImpl;
-import com.apollocurrency.aplwallet.apl.core.service.fulltext.FullTextConfigImpl;
-import com.apollocurrency.aplwallet.apl.core.service.state.DerivedDbTablesRegistryImpl;
-import com.apollocurrency.aplwallet.apl.core.service.state.account.PublicKeyDao;
-import com.apollocurrency.aplwallet.apl.core.shard.BlockIndexServiceImpl;
-import com.apollocurrency.aplwallet.apl.core.shard.ShardDbExplorerImpl;
-import com.apollocurrency.aplwallet.apl.core.transaction.TransactionTypeFactory;
-import com.apollocurrency.aplwallet.apl.core.transaction.messages.PrunableLoadingService;
 import com.apollocurrency.aplwallet.apl.core.utils.CollectionUtil;
 import com.apollocurrency.aplwallet.apl.data.DexTestData;
-import com.apollocurrency.aplwallet.apl.data.TransactionTestData;
 import com.apollocurrency.aplwallet.apl.extension.DbExtension;
 import com.apollocurrency.aplwallet.apl.testutil.DbUtils;
-import com.apollocurrency.aplwallet.apl.util.cdi.transaction.JdbiHandleFactory;
-import com.apollocurrency.aplwallet.apl.util.injectable.PropertiesHolder;
+import com.apollocurrency.aplwallet.apl.util.db.DbIterator;
 import lombok.extern.slf4j.Slf4j;
-import org.jboss.weld.junit.MockBean;
-import org.jboss.weld.junit5.EnableWeld;
-import org.jboss.weld.junit5.WeldInitiator;
-import org.jboss.weld.junit5.WeldSetup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import javax.inject.Inject;
+import javax.enterprise.event.Event;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -60,59 +25,28 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 
 @Slf4j
-
 @Tag("slow")
-@EnableWeld
 public class DexContractTableTest extends DbContainerBaseTest {
 
     @RegisterExtension
     static DbExtension extension = new DbExtension(mariaDBContainer);
-    PropertiesHolder propertiesHolder = mock(PropertiesHolder.class);
-    NtpTimeConfig ntpTimeConfig = new NtpTimeConfig();
-    TimeService timeService = new TimeServiceImpl(ntpTimeConfig.time());
-    TransactionTestData td = new TransactionTestData();
 
-    @WeldSetup
-    public WeldInitiator weld = WeldInitiator.from(
-        BlockchainImpl.class, DaoConfig.class,
-        JdbiHandleFactory.class,
-        FullTextConfigImpl.class,
-        DexContractTable.class,
-        TransactionServiceImpl.class, ShardDbExplorerImpl.class,
-        TransactionEntityRowMapper.class, TransactionEntityRowMapper.class, TxReceiptRowMapper.class, PrunableTxRowMapper.class,
-        TransactionModelToEntityConverter.class, TransactionEntityToModelConverter.class,
-        TransactionBuilderFactory.class,
-        DerivedDbTablesRegistryImpl.class,
-        BlockDaoImpl.class,
-        BlockEntityRowMapper.class, BlockEntityToModelConverter.class, BlockModelToEntityConverter.class,
-        TransactionDaoImpl.class,
-        BlockIndexServiceImpl.class, NullCacheProducerForTests.class, JdbiHandleFactory.class, JdbiConfiguration.class)
-        .addBeans(MockBean.of(extension.getDatabaseManager(), DatabaseManager.class))
-        .addBeans(MockBean.of(propertiesHolder, PropertiesHolder.class))
-        .addBeans(MockBean.of(ntpTimeConfig, NtpTimeConfig.class))
-        .addBeans(MockBean.of(timeService, TimeService.class))
-        .addBeans(MockBean.of(mock(PrunableLoadingService.class), PrunableLoadingService.class))
-        .addBeans(MockBean.of(td.getTransactionTypeFactory(), TransactionTypeFactory.class))
-        .addBeans(MockBean.of(mock(PublicKeyDao.class), PublicKeyDao.class))
-        .addBeans(MockBean.of(mock(BlockchainConfig.class), BlockchainConfig.class))
-        .build();
-    @Inject
-    DexContractTable table;
-    DexTestData dtd;
+    DexContractTable table = new DexContractTable(extension.getDatabaseManager(), mock(Event.class));
+    DexTestData td;
 
     @BeforeEach
     void setUp() {
-        dtd = new DexTestData();
+        td = new DexTestData();
     }
 
     @Test
     void testInsert() {
         DbUtils.inTransaction(extension, (con) -> {
-            table.insert(dtd.NEW_EXCHANGE_CONTRACT_16);
+            table.insert(td.NEW_EXCHANGE_CONTRACT_16);
         });
-        ExchangeContract result = table.getById(dtd.NEW_EXCHANGE_CONTRACT_16.getId());
+        ExchangeContract result = table.getById(td.NEW_EXCHANGE_CONTRACT_16.getId());
         assertNotNull(result);
-        assertEquals(dtd.NEW_EXCHANGE_CONTRACT_16.getId(), result.getId());
+        assertEquals(td.NEW_EXCHANGE_CONTRACT_16.getId(), result.getId());
     }
 
     @Test
@@ -120,6 +54,13 @@ public class DexContractTableTest extends DbContainerBaseTest {
         DbIterator<ExchangeContract> iterator = table.getAll(0, 10);
         List<ExchangeContract> result = CollectionUtil.toList(iterator);
         assertEquals(10, result.size());
+    }
+
+    @Test
+    void t() {
+        List<ExchangeContract> allByCounterOrder = table.getAllByCounterOrder(-6968465014361285240L);
+
+        assertEquals(List.of(td.EXCHANGE_CONTRACT_11, td.EXCHANGE_CONTRACT_12, td.EXCHANGE_CONTRACT_13, td.EXCHANGE_CONTRACT_14, td.EXCHANGE_CONTRACT_15), allByCounterOrder);
     }
 
 }
