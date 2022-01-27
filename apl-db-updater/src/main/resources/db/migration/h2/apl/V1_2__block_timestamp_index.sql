01102ffcd5fc94056ad1f4fb0742f0fd1702cdb5
Andrew Boyarsky <andrew.boyarski@gmail.com>
2022-01-27 17:34:27 +0200

    Revert "Apollo BLockchain 1.50.34 Release (H2 block index fix)"

--- apl-db-updater/src/main/resources/db/migration/h2/apl/V1_2__block_timestamp_index.sql
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2022. Apollo Foundation.
- */
--- Recreate block_timestamp_idx with DESC sort to ensure
--- fast last block lookup required for popOff operation
-DROP INDEX  block_timestamp_idx;
-CREATE UNIQUE INDEX block_timestamp_idx ON block (`timestamp` DESC) ;
