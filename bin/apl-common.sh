a7a8af9a516450167c7ffdf1d9ce364197ed90ba
Rostyslav Golda <tx_hv@ukr.net>
2023-02-20 10:30:18 +0200

    Increased JAVA_HOME prioryty in front of simple java command for *ix

--- bin/apl-common.sh
+++ bin/apl-common.sh
@@ -53,14 +53,13 @@ echo "${ECHO_PREFIX} Apollo main jar path: ${MAIN_JAR}"
 if [ -x "${APL_TOP_DIR}"/../jre/bin/java ]; then
     JAVA_CMD="${APL_TOP_DIR}"/../jre/bin/java
 else
-  if [[ -n $(type -p java) ]]
-  then
-    JAVA_CMD=java
-  elif [[ (-n "$JAVA_HOME") && (-x "$JAVA_HOME/bin/java") ]]
+  if [[ (-n "$JAVA_HOME") && (-x "$JAVA_HOME/bin/java") ]]
   then
     JAVA_CMD="$JAVA_HOME/bin/java"
-  fi
+  elif [[ -n $(type -p java) ]]
+  then
     JAVA_CMD=java
+  fi
 fi
 WJAVACMD=$(which $JAVA_CMD)
 JAVA_BASE=$(dirname "${WJAVACMD}")
